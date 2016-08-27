9a482bbd09e96252822a91a19302fc8616f1c32b
Min RK <benjaminrk@gmail.com>
2016-08-27 16:44:09 +0200

    use start_ipython utility function in scheduler.start_ipython
    removes unnecessary duplicate code

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -2615,53 +2615,18 @@ class Scheduler(Server):
                 out.update({ww for ww in self.ncores if w in ww}) # TODO: quadratic
         return list(out)
 
-    def _start_ipython(self):
-        from IPython import get_ipython
-        if get_ipython() is not None:
-            raise RuntimeError("Cannot start IPython, it's already running.")
-
-        from zmq.eventloop.ioloop import ZMQIOLoop
-        from ipykernel.kernelapp import IPKernelApp
-        # save the global IOLoop instance
-        # since IPython relies on it, but we are going to put it in a thread.
-        save_inst = IOLoop.instance()
-        IOLoop.clear_instance()
-        zmq_loop = ZMQIOLoop()
-        zmq_loop.install()
-
-        # start IPython, disabling its signal handlers that won't work due to running in a thread:
-        app = self._ipython_kernel = IPKernelApp.instance(log=logger)
-        # Don't connect to the history database
-        app.config.HistoryManager.hist_file = ':memory:'
-        # listen on all interfaces, so remote clients can connect:
-        app.ip = self.ip
-        app.init_signal = lambda : None
-        app.initialize([])
-        app.kernel.pre_handler_hook = lambda : None
-        app.kernel.post_handler_hook = lambda : None
-        app.kernel.start()
-
-        # save self in the IPython namespace as 'scheduler'
-        app.kernel.shell.user_ns['scheduler'] = self
-        app.kernel.shell.user_ns['s'] = self
-
-        # start IPython's IOLoop in a thread
-        from threading import Thread
-        zmq_loop_thread = Thread(target=zmq_loop.start)
-        zmq_loop_thread.start()
-
-        # put the global IOLoop instance back:
-        IOLoop.clear_instance()
-        save_inst.install()
-        return app
-
     def start_ipython(self, stream=None):
         """Start an IPython kernel
 
         Returns Jupyter connection info dictionary.
         """
+        from ._ipython_utils import start_ipython
         if self._ipython_kernel is None:
-            self._ipython_kernel = self._start_ipython()
+            self._ipython_kernel = start_ipython(
+                ip=self.ip,
+                ns={'scheduler': self},
+                log=logger,
+            )
         return self._ipython_kernel.get_connection_info()
 
 
