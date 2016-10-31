aa8da8acaa3d0e79e7a556258a8e6ae689b4141f
Matthew Rocklin <mrocklin@gmail.com>
2016-10-31 19:28:42 -0400

    Construct messages dict all at once

--- distributed/bokeh/__init__.py
+++ distributed/bokeh/__init__.py
@@ -1,33 +1,34 @@
 from collections import deque
 from time import time
 
-messages = {}
-
 n = 60
-messages['workers'] = {'interval': 1000,
-                       'deque': deque(maxlen=n),
-                       'times': deque(maxlen=n),
-                       'index': deque(maxlen=n),
-                       'plot-data': {'time': deque(maxlen=n),
-                                     'cpu': deque(maxlen=n),
-                                     'memory_percent': deque(maxlen=n),
-                                     'network-send': deque(maxlen=n),
-                                     'network-recv': deque(maxlen=n)}}
 
-messages['tasks'] = {'interval': 150,
-                     'deque': deque(maxlen=100),
-                     'times': deque(maxlen=100)}
+messages = {
+    'workers': {'interval': 1000,
+                'deque': deque(maxlen=n),
+                'times': deque(maxlen=n),
+                'index': deque(maxlen=n),
+                'plot-data': {'time': deque(maxlen=n),
+                              'cpu': deque(maxlen=n),
+                              'memory_percent': deque(maxlen=n),
+                              'network-send': deque(maxlen=n),
+                              'network-recv': deque(maxlen=n)}},
 
-messages['progress'] = {}
+    'tasks': {'interval': 150,
+              'deque': deque(maxlen=100),
+              'times': deque(maxlen=100)},
 
-messages['processing'] = {'stacks': {}, 'processing': {},
-                          'memory': 0, 'waiting': 0, 'ready': 0}
+    'progress': {},
 
-messages['task-events'] = {'interval': 200,
-                           'deque': deque(maxlen=n),
-                           'times': deque(maxlen=n),
-                           'index': deque(maxlen=n),
-                           'rectangles':{name: deque(maxlen=n) for name in
-                                        'start duration key name color worker worker_thread y alpha'.split()},
-                           'workers': dict(),
-                           'last_seen': [time()]}
+    'processing': {'stacks': {}, 'processing': {},
+                   'memory': 0, 'waiting': 0, 'ready': 0},
+
+    'task-events': {'interval': 200,
+                    'deque': deque(maxlen=n),
+                    'times': deque(maxlen=n),
+                    'index': deque(maxlen=n),
+                    'rectangles':{name: deque(maxlen=n) for name in
+                                 'start duration key name color worker worker_thread y alpha'.split()},
+                    'workers': dict(),
+                    'last_seen': [time()]}
+}
