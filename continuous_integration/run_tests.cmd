aaa187d5f3c050ef7acd51e6321491c13eec7796
Antoine Pitrou <antoine@python.org>
2016-11-06 15:16:32 +0100

    Up AppVeyor test timeout a bit

--- continuous_integration/run_tests.cmd
+++ continuous_integration/run_tests.cmd
@@ -4,6 +4,6 @@ call activate %CONDA_ENV%
 
 set PYTHONFAULTHANDLER=1
 
-set PYTEST=py.test --tb=native --timeout=60
+set PYTEST=py.test --tb=native --timeout=120
 
 %PYTEST% -v -m "not avoid_travis" --junit-xml="%JUNIT_OUT%" distributed
