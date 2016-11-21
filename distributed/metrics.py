b171716a362542f1558a87741e008ab308f3e803
Antoine Pitrou <antoine@python.org>
2016-11-21 23:31:10 +0100

    Move psutil check outside

--- distributed/metrics.py
+++ distributed/metrics.py
@@ -1,25 +1,31 @@
 from __future__ import print_function, division, absolute_import
 
 import collections
+from functools import wraps
 
 
 _empty_namedtuple = collections.namedtuple("_empty_namedtuple", ())
 
 
-def _run_psutil(method_name, default):
+def _psutil_runner(method_name, default=_empty_namedtuple):
     try:
         import psutil
     except ImportError:
         return default
-    try:
-        return getattr(psutil, method_name)()
-    except RuntimeError:
-        # This can happen on some systems (e.g. no physical disk in worker)
-        return default
+
+    meth = getattr(psutil, method_name)
+    @wraps(meth)
+    def wrapper():
+        try:
+            return meth()
+        except RuntimeError:
+            # This can happen on some systems (e.g. no physical disk in worker)
+            return default()
+
+    return wrapper
 
 
-def disk_io_counters():
-    return _run_psutil("disk_io_counters", _empty_namedtuple())
+disk_io_counters = _psutil_runner("disk_io_counters")
 
-def net_io_counters():
-    return _run_psutil("net_io_counters", _empty_namedtuple())
+net_io_counters = _psutil_runner("net_io_counters")
+
