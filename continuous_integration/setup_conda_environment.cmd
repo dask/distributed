07255f657e4cfa82be36a950ef9996441254fa8b
Antoine Pitrou <antoine@python.org>
2016-11-21 21:26:42 +0100

    Fix early exit

--- continuous_integration/setup_conda_environment.cmd
+++ continuous_integration/setup_conda_environment.cmd
@@ -9,17 +9,17 @@ set PIP_INSTALL=pip install -q
 @rem Deactivate any environment
 call deactivate
 @rem Display root environment (for debugging)
-conda list
+%CONDA% list
 @rem Clean up any left-over from a previous build
-conda remove --all -q -y -n %CONDA_ENV%
+%CONDA% remove --all -q -y -n %CONDA_ENV%
 
 @rem Create test environment
 @rem (note: no cytoolz as it seems to prevent faulthandler tracebacks on crash)
-conda create -n %CONDA_ENV% -q -y python=%PYTHON% pytest toolz dill futures dask ipywidgets psutil bokeh requests joblib mock ipykernel jupyter_client tblib msgpack-python cloudpickle locket click zict lz4
+%CONDA% create -n %CONDA_ENV% -q -y python=%PYTHON% pytest toolz dill futures dask ipywidgets psutil bokeh requests joblib mock ipykernel jupyter_client tblib msgpack-python cloudpickle locket click zict lz4
 
 call activate %CONDA_ENV%
 
-conda uninstall -q -y --force dask joblib
+%CONDA% uninstall -q -y --force dask joblib
 %PIP_INSTALL% git+https://github.com/dask/dask --upgrade
 %PIP_INSTALL% git+https://github.com/joblib/joblib.git --upgrade
 
