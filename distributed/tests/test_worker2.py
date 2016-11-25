94acb4dc939ab57b1b9ad65db483024bc9f8af51
Matthew Rocklin <mrocklin@gmail.com>
2016-11-25 10:07:54 -0500

    clean up testing

--- distributed/tests/test_worker2.py
+++ distributed/tests/test_worker2.py
@@ -1,31 +1,29 @@
 
 from operator import add
 
-from distributed.utils_test import gen_cluster, inc
+from distributed.utils_test import gen_cluster, inc, slowinc
 from distributed.worker2 import Worker2
 
 
-@gen_cluster(client=True, ncores=[])
-def test_submit(c, s):
-    w = Worker2(s.ip, s.port, ncores=2, loop=s.loop)
-    yield w._start()
-
+@gen_cluster(client=True, Worker=Worker2)
+def test_submit(c, s, a, b):
+    assert isinstance(a, Worker2)
     future = c.submit(inc, 1)
     result = yield future._result()
     assert result == 2
 
-    yield w._close()
 
-
-@gen_cluster(client=True)
+@gen_cluster(client=True, Worker=Worker2)
 def test_inter_worker_communication(c, s, a, b):
-    [x, y] = yield c._scatter([1, 2])
+    [x, y] = yield c._scatter([1, 2], workers=a.address)
 
-    w = Worker2(s.ip, s.port, ncores=2, loop=s.loop)
-    yield w._start()
-
-    future = c.submit(add, x, y, workers=w.address)
+    future = c.submit(add, x, y, workers=b.address)
     result = yield future._result()
     assert result == 3
 
-    yield w._close()
+
+@gen_cluster(client=True, Worker=Worker2)
+def test_map(c, s, a, b):
+    futures = c.map(slowinc, range(20), delay=0.01)
+    result = yield c._gather(futures)
+    assert result == list(range(1, 21))
