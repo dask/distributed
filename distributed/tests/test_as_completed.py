ceafc5583589fe9824285a0b93a3a2521f478109
Matthew Rocklin <mrocklin@gmail.com>
2017-01-24 17:07:04 -0500

    Add AsCompleted class

--- distributed/tests/test_as_completed.py
+++ distributed/tests/test_as_completed.py
@@ -1,9 +1,12 @@
 from collections import Iterator
+from operator import add
+import random
 
 import pytest
 
 from distributed import Client
-from distributed.client import _as_completed, as_completed, _first_completed
+from distributed.client import (_as_completed, as_completed, _first_completed,
+        AsCompleted)
 from distributed.utils_test import gen_cluster, inc, dec, loop, cluster
 from distributed.compatibility import Queue
 
@@ -44,3 +47,34 @@ def test_as_completed_with_non_futures(loop):
             with pytest.raises(TypeError):
                 list(as_completed([1, 2, 3]))
 
+
+def test_as_completed_add(loop):
+    with cluster() as (s, [a, b]):
+        with Client(('127.0.0.1', s['port']), loop=loop) as c:
+            total = 0
+            futures = c.map(inc, range(20))
+            ac = AsCompleted(futures)
+            for future in ac:
+                total += future.result()
+                if random.random() < 0.5:
+                    future = c.submit(add, future, 10)
+                    ac.add(future)
+            assert total > sum(map(inc, range(10)))
+
+
+def test_as_completed_repeats(loop):
+    with cluster() as (s, [a, b]):
+        with Client(('127.0.0.1', s['port']), loop=loop) as c:
+            ac = AsCompleted()
+            x = c.submit(inc, 1)
+            ac.add(x)
+            ac.add(x)
+
+            assert next(ac) is x
+            assert next(ac) is x
+
+            with pytest.raises(StopIteration):
+                next(ac)
+
+            ac.add(x)
+            assert next(ac) is x
