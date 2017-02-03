59c447faa6d356884e2660ee742c42e6c7673093
Matthew Rocklin <mrocklin@gmail.com>
2017-02-03 13:19:35 -0500

    remove worker axis labels

--- distributed/bokeh/components.py
+++ distributed/bokeh/components.py
@@ -81,6 +81,8 @@ class TaskStream(DashboardComponent):
         self.root.add_layout(LinearAxis(axis_label="Worker Core", ticker=ticker), "left")
         self.root.add_layout(Grid(dimension=1, grid_line_alpha=0.4, ticker=ticker))
 
+        self.root.yaxis.major_label_text_alpha = 0
+
         hover = HoverTool(
             point_policy="follow_mouse",
             tooltips="""
