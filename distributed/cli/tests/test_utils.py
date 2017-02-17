77073fddb91fb741d4febc86033cfc157f7832c9
Matthew Rocklin <mrocklin@gmail.com>
2017-02-17 12:56:02 -0500

    remove extra test file

--- distributed/cli/tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-from distributed.cli.utils import uri_from_host_port
-
-def test_uri_from_host_port():
-    addr = uri_from_host_port('127.0.0.1', 0, 8786)
-    port = addr.split(':')[-1]
-    assert port
-    assert port == '0'
