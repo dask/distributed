16ae3387430bfcda474de304674748bb7eafa0ab
Matthew Rocklin <mrocklin@gmail.com>
2017-03-03 08:18:45 -0500

    Pandas serialization

--- distributed/protocol/tests/test_pandas.py
+++ distributed/protocol/tests/test_pandas.py
@@ -2,6 +2,7 @@ from __future__ import print_function, division, absolute_import
 
 from zlib import crc32
 
+import numpy as np
 import pandas as pd
 import pandas.util.testing as tm
 import pytest
@@ -32,13 +33,16 @@ dfs = [
     pd.DataFrame({'x': [b'a', b'b', b'c']}),
     pd.DataFrame({'x': pd.Categorical(['a', 'b', 'a'], ordered=True)}),
     pd.DataFrame({'x': pd.Categorical(['a', 'b', 'a'], ordered=False)}),
+    pd.Series(np.arange(10000000)),
+    pd.DataFrame({'x': np.arange(10000000)}),
     tm.makeCategoricalIndex(),
     tm.makeCustomDataframe(5, 3),
     tm.makeDataFrame(),
     tm.makeDateIndex(),
     tm.makeMissingDataframe(),
     tm.makeMixedDataFrame(),
-    tm.makeObjectSeries(),
+    pytest.mark.xfail(tm.makeObjectSeries(),
+                      reason='date to timestamp conversion'),
     tm.makePeriodFrame(),
     tm.makeRangeIndex(),
     tm.makeTimeDataFrame(),
@@ -48,10 +52,28 @@ dfs = [
 
 
 @pytest.mark.parametrize('df', dfs)
-def test_dumps_serialize_numpy(df):
+def test_serialize_pandas(df):
     header, frames = serialize(df)
     if 'compression' in header:
         frames = decompress(header, frames)
     df2 = deserialize(header, frames)
 
-    assert_eq(df, df2)
+    if isinstance(df, pd.DataFrame):
+        tm.assert_frame_equal(df, df2)
+    elif isinstance(df, pd.Series):
+        tm.assert_series_equal(df, df2)
+    else:
+        assert_eq(df, df2)
+
+
+@pytest.mark.parametrize('df', dfs)
+def test_dumps_pandas(df):
+    frames = dumps({'x': to_serialize(df)})
+    df2 = loads(frames)['x']
+
+    if isinstance(df, pd.DataFrame):
+        tm.assert_frame_equal(df, df2)
+    elif isinstance(df, pd.Series):
+        tm.assert_series_equal(df, df2)
+    else:
+        assert_eq(df, df2)
