c41d1f0d99cc36da66656f573368eda0e095b5d7
Matthew Rocklin <mrocklin@gmail.com>
2017-03-19 14:26:00 -0400

    test windows error

--- distributed/tests/py3_test_client.py
+++ distributed/tests/py3_test_client.py
@@ -3,21 +3,6 @@ import pytest
 from distributed.utils_test import gen_cluster, inc, div
 from distributed import as_completed
 
-@gen_cluster(client=True)
-def test_as_completed_async_for(c, s, a, b):
-    futures = c.map(inc, range(10))
-    ac = as_completed(futures)
-    results = []
-
-    async def f():
-        async for future in ac:
-            result = await future._result()
-            results.append(result)
-
-    yield f()
-
-    assert set(results) == set(range(1, 11))
-
 
 @gen_cluster(client=True)
 def test_await_future(c, s, a, b):
@@ -37,3 +22,18 @@ def test_await_future(c, s, a, b):
 
     yield f()
 
+
+@gen_cluster(client=True)
+def test_as_completed_async_for(c, s, a, b):
+    futures = c.map(inc, range(10))
+    ac = as_completed(futures)
+    results = []
+
+    async def f():
+        async for future in ac:
+            result = await future
+            results.append(result)
+
+    yield f()
+
+    assert set(results) == set(range(1, 11))
