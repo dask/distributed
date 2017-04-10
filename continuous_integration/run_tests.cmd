100d49853b12d230b6fef7a732a92cab993ceced
Antoine Pitrou <antoine@python.org>
2017-04-10 12:45:14 +0200

    Try to fix AppVeyor builds

--- continuous_integration/run_tests.cmd
+++ continuous_integration/run_tests.cmd
@@ -6,4 +6,7 @@ set PYTHONFAULTHANDLER=1
 
 set PYTEST=py.test --tb=native --timeout=120 -r s
 
+where python
+where py.test
+
 %PYTEST% -v -m "not avoid_travis" --junit-xml="%JUNIT_OUT%" distributed
