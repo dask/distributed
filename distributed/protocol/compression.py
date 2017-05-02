aa4472bead9f05ab6f9e4910d775ed4fb2c85163
Jens Nie <jneines@web.de>
2017-05-02 09:51:13 +0200

    Update compression.py
    Added support to distinguish between older and new versions of lz4, as we cannot expect to have the latest lz4 packages available in older python environments.

--- distributed/protocol/compression.py
+++ distributed/protocol/compression.py
@@ -51,15 +51,24 @@ with ignoring(ImportError):
 with ignoring(ImportError):
     import lz4
 
+    try:
+        # try using the new lz4 API
+        lz4_compress=lz4.block.compress
+        lz4_decompress=lz4.block.decompress
+    except AttributeError as err:
+        # fall back to old one
+        lz4_compress=lz4.LZ4_compress
+        lz4_decompress=lz4.LZ4_uncompress
+
     def _fixed_lz4_decompress(data):
-        # lz4.block.decompress() doesn't accept memoryviews
+        # helper to bypass missing memoryview support in lz4
         if isinstance(data, memoryview):
             data = data.tobytes()
-        return lz4.block.decompress(data)
+        return lz4_decompress(data)
 
-    compressions['lz4'] = {'compress': lz4.block.compress,
+    compressions['lz4'] = {'compress': lz4_compress,
                            'decompress': _fixed_lz4_decompress}
-    default_compression = 'lz4'
+     default_compression = 'lz4'
 
 with ignoring(ImportError):
     import blosc
