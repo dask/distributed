25934b893593c90bb548e0775e5fdff7939527a8
Matthew Rocklin <mrocklin@gmail.com>
2017-06-04 20:15:38 -0400

    copy singleton scatter code from client to worker_client

--- distributed/worker_client.py
+++ distributed/worker_client.py
@@ -3,7 +3,6 @@ from __future__ import print_function, division, absolute_import
 from contextlib import contextmanager
 from datetime import timedelta
 from toolz import keymap, valmap, merge
-import uuid
 
 from dask.base import tokenize
 from tornado import gen
@@ -100,38 +99,40 @@ class WorkerClient(Client):
                 d = yield self._scatter(keymap(tokey, data), workers, broadcast)
                 raise gen.Return({k: d[tokey(k)] for k in data})
 
-            if isinstance(data, (list, tuple, set, frozenset)):
-                keys = []
-                for x in data:
-                    try:
-                        keys.append(tokenize(x))
-                    except:
-                        keys.append(str(uuid.uuid1()))
-                data2 = dict(zip(keys, data))
-            elif isinstance(data, dict):
-                keys = set(data)
-                data2 = data
-            else:
-                raise TypeError("Don't know how to scatter %s" % type(data))
+            if isinstance(data, type(range(0))):
+                data = list(data)
+            input_type = type(data)
+            names = False
+            unpack = False
+            if isinstance(data, (set, frozenset)):
+                data = list(data)
+            if not isinstance(data, (dict, list, tuple, set, frozenset)):
+                unpack = True
+                data = [data]
+            if isinstance(data, (list, tuple)):
+                names = list(map(tokenize, data))
+                data = dict(zip(names, data))
 
-            self.worker.update_data(data=data2, report=False)
+            types = valmap(type, data)
+            assert isinstance(data, dict)
+
+            self.worker.update_data(data=data, report=False)
 
             yield self.scheduler.update_data(
-                    who_has={key: [self.worker.address] for key in data2},
-                    nbytes=valmap(sizeof, data2),
+                    who_has={key: [self.worker.address] for key in data},
+                    nbytes=valmap(sizeof, data),
                     client=self.id)
 
-            if isinstance(data, dict):
-                out = {k: Future(k, self) for k in data}
-            elif isinstance(data, (tuple, list, set, frozenset)):
-                out = type(data)([Future(k, self) for k in keys])
-            else:
-                raise TypeError(
-                        "Input to scatter must be a list or dict")
+            out = {k: self._Future(k, self) for k in data}
+            for key, typ in types.items():
+                self.futures[key].finish(type=typ)
 
-            for key in keys:
-                self.futures[key].finish(type=None)
+            if issubclass(input_type, (list, tuple, set, frozenset)):
+                out = input_type(out[k] for k in names)
 
+            if unpack:
+                assert len(out) == 1
+                out = list(out.values())[0]
             raise gen.Return(out)
 
     @gen.coroutine
