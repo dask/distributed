e0a99de6d95443176fc10b3b735baab4edbe26e3
Matthew Rocklin <mrocklin@gmail.com>
2017-09-12 13:06:32 -0400

    Add frame manipulation utilities

---
+++ distributed/diagnostics/profile.py
@@ -0,0 +1,95 @@
+""" This module contains utility functions to construct and manipulate counting
+data structures for frames.
+
+When performing statistical profiling we obtain many call stacks.  We aggregate
+these call stacks into data structures that maintain counts of how many times
+each function in that call stack has been called.  Because these stacks will
+overlap this aggregation counting structure forms a tree, such as is commonly
+visualized by profiling tools.
+
+We represent this tree as a nested dictionary with the following form:
+
+    {
+     'description': 'A long description of the line of code being run.',
+     'count': 10  # the number of times we have seen this line
+     'children': {  # callers of this line. Recursive dicts
+         'ident': {'description': ...
+                   'count': ...
+                   'children': {...}},
+         'ident': {'description': ...
+                   'count': ...
+                   'children': {...}}}
+    }
+"""
+
+
+from collections import defaultdict
+import linecache
+
+
+def identifier(frame):
+    return ';'.join((frame.f_code.co_name,
+                     frame.f_code.co_filename,
+                     str(frame.f_code.co_firstlineno)))
+
+
+def repr_frame(frame):
+    co = frame.f_code
+    text = '  File "%s", line %s, in %s' % (co.co_filename,
+                                            frame.f_lineno,
+                                            co.co_name)
+    line = linecache.getline(co.co_filename, frame.f_lineno, frame.f_globals).lstrip()
+    return text + '\n\t' + line
+
+
+def process(frame, child, state):
+    """ Add counts from a frame stack onto existing state
+
+    This recursively adds counts to the existing state dictionary and creates
+    new entries for new functions.
+
+    Example
+    -------
+    >>> import sys, threading
+    >>> ident = threading.get_ident()  # replace with your thread of interest
+    >>> frame = sys._current_frames()[ident]
+    >>> state = {'children': {}, 'count': 0, 'description': 'root'}
+    >>> process(frame, None, state)
+    >>> state
+    {'count': 1,
+     'description': 'root',
+     'children': {'...'}}
+    """
+    ident = identifier(frame)
+    if frame.f_back is not None:
+        state = process(frame.f_back, frame, state)
+
+    try:
+        d = state['children'][ident]
+    except KeyError:
+        d = {'count': 0,
+             'description': repr_frame(frame),
+             'children': {}}
+        state['children'][ident] = d
+
+    state['count'] += 1
+
+    if child is not None:
+        return d
+    else:
+        d['count'] += 1
+
+
+def merge(*args):
+    """ Merge multiple frame states together """
+    assert len({arg['description'] for arg in args}) == 1
+    children = defaultdict(list)
+    for arg in args:
+        for child in arg['children']:
+            children[child].append(arg['children'][child])
+
+    children = {k: merge(*v) for k, v in children.items()}
+    count = sum(arg['count'] for arg in args)
+    return {'description': args[0]['description'],
+            'children': dict(children),
+            'count': count}
---
+++ distributed/diagnostics/tests/test_profile.py
@@ -0,0 +1,84 @@
+import sys
+import time
+from toolz import first
+from threading import Thread
+
+from distributed.diagnostics.profile import process, merge
+
+
+def test_basic():
+    def test_g():
+        time.sleep(0.01)
+
+    def test_h():
+        time.sleep(0.02)
+
+    def test_f():
+        for i in range(100):
+            test_g()
+            test_h()
+
+    thread = Thread(target=test_f)
+    thread.daemon = True
+    thread.start()
+
+    state = {'count': 0, 'children': {}, 'description': 'root'}
+
+    for i in range(100):
+        time.sleep(0.02)
+        frame = sys._current_frames()[thread.ident]
+        process(frame, None, state)
+
+    assert state['description'] == 'root'
+    assert state['count'] == 100
+    d = state
+    while len(d['children']) == 1:
+        d = first(d['children'].values())
+
+    assert d['count'] == 100
+    assert 'test_f' in d['description']
+    g = [c for c in d['children'].values() if 'test_g' in c['description']][0]
+    h = [c for c in d['children'].values() if 'test_h' in c['description']][0]
+
+    assert g['count'] < h['count']
+    assert g['count'] + h['count'] == 100
+
+
+def test_merge():
+    a1 = {
+         'count': 5,
+         'description': 'a',
+         'children': {
+             'b': {'count': 3,
+                   'description': 'b-func',
+                   'children': {}},
+             'c': {'count': 2,
+                   'description': 'c-func',
+                   'children': {}}}}
+
+    a2 = {
+         'count': 4,
+         'description': 'a',
+         'children': {
+             'd': {'count': 2,
+                   'description': 'd-func',
+                   'children': {}},
+             'c': {'count': 2,
+                   'description': 'c-func',
+                   'children': {}}}}
+
+    expected = {
+         'count': 9,
+         'description': 'a',
+         'children': {
+             'b': {'count': 3,
+                   'description': 'b-func',
+                   'children': {}},
+             'd': {'count': 2,
+                   'description': 'd-func',
+                   'children': {}},
+             'c': {'count': 4,
+                   'description': 'c-func',
+                   'children': {}}}}
+
+    assert merge(a1, a2) == expected
