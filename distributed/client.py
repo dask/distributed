97f76db44e153b65c027e6e13141b30a3a3c39c0
Russ Bubley <russ.bubley@googlemail.com>
2017-09-21 23:48:17 +0100

    Add name to Client, to enable scheduler logs to show which client messages are about.

--- distributed/client.py
+++ distributed/client.py
@@ -403,6 +403,9 @@ class Client(Node):
         Optional security information
     asynchronous: bool (False by default)
         Set to True if this client will be used within a Tornado event loop
+    name: string (optional)
+        Gives the client a name that will be included in logs generated on
+        the scheduler for matters relating to this client
 
     Examples
     --------
@@ -433,7 +436,7 @@ class Client(Node):
     def __init__(self, address=None, loop=None, timeout=5,
                  set_as_default=True, scheduler_file=None,
                  security=None, start=None, asynchronous=False,
-                 **kwargs):
+                 name=None, **kwargs):
         if start is not None:
             raise ValueError("The start= keyword has been deprecated. "
                              "Starting happens automatically. "
@@ -442,7 +445,7 @@ class Client(Node):
         self.futures = dict()
         self.refcount = defaultdict(lambda: 0)
         self.coroutines = []
-        self.id = type(self).__name__ + '-' + str(uuid.uuid1(clock_seq=os.getpid()))
+        self.id = type(self).__name__ + ('-' + name + '-' if name else '-') + str(uuid.uuid1(clock_seq=os.getpid()))
         self.generation = 0
         self.status = None
         self._pending_msg_buffer = []
