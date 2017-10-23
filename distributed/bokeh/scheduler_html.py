705284b60e292ce0261060abd77eb875956933c8
Matthew Rocklin <mrocklin@gmail.com>
2017-10-23 08:54:06 -0400

    Point RequestHandlers to templates in get_template_path
    As suggested in https://github.com/tornadoweb/tornado/issues/2174#issuecomment-338481447

--- distributed/bokeh/scheduler_html.py
+++ distributed/bokeh/scheduler_html.py
@@ -13,66 +13,57 @@ dirname = os.path.dirname(__file__)
 ns = {func.__name__: func for func in [format_bytes, format_time, datetime.fromtimestamp]}
 
 
-class Workers(web.RequestHandler):
+class RequestHandler(web.RequestHandler):
     def initialize(self, server=None):
         self.server = server
 
+    def get_template_path(self):
+        return os.path.join(dirname, 'templates')
+
+
+class Workers(RequestHandler):
     def get(self):
         with log_errors():
-            self.render(os.path.join(dirname, 'workers.html'),
+            self.render('workers.html',
                         title='Workers',
                         **toolz.merge(self.server.__dict__, ns))
 
 
-class Worker(web.RequestHandler):
-    def initialize(self, server=None):
-        self.server = server
-
+class Worker(RequestHandler):
     def get(self, worker):
         worker = escape.url_unescape(worker)
         with log_errors():
-            self.render(os.path.join(dirname, 'worker.html'),
+            self.render('worker.html',
                         title='Worker: ' + worker, Worker=worker,
                         **toolz.merge(self.server.__dict__, ns))
 
 
-class Task(web.RequestHandler):
-    def initialize(self, server=None):
-        self.server = server
-
+class Task(RequestHandler):
     def get(self, task):
         task = escape.url_unescape(task)
         with log_errors():
-            self.render(os.path.join(dirname, 'task.html'),
+            self.render('task.html',
                         title='Task: ' + task,
                         Task=task,
                         server=self.server,
                         **toolz.merge(self.server.__dict__, ns))
 
 
-class Logs(web.RequestHandler):
-    def initialize(self, server=None):
-        self.server = server
-
+class Logs(RequestHandler):
     def get(self):
         with log_errors():
             logs = self.server.get_logs()
-            self.render(os.path.join(dirname, 'logs.html'),
-                        title="Logs", logs=logs)
+            self.render('logs.html', title="Logs", logs=logs)
 
 
-class WorkerLogs(web.RequestHandler):
-    def initialize(self, server=None):
-        self.server = server
-
+class WorkerLogs(RequestHandler):
     @gen.coroutine
     def get(self, worker):
         with log_errors():
             worker = escape.url_unescape(worker)
             logs = yield self.server.get_worker_logs(workers=[worker])
             logs = logs[worker]
-            self.render(os.path.join(dirname, 'logs.html'),
-                        title="Logs: " + worker, logs=logs)
+            self.render('logs.html', title="Logs: " + worker, logs=logs)
 
 
 def get_handlers(server):
