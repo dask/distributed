964fad09b4ca1c578f0e69e7f89c443b3b6da702
Antoine Pitrou <antoine@python.org>
2017-11-13 16:24:07 +0100

    Run the whole test suite again on AppVeyor. The bug is probably timing-dependent in some way.

--- continuous_integration/run_tests.cmd
+++ continuous_integration/run_tests.cmd
@@ -6,4 +6,4 @@ set PYTHONFAULTHANDLER=1
 
 set PYTEST=py.test --tb=native --timeout=120 -r s
 
-%PYTEST% -vx -m "not avoid_travis" --count=10 --junit-xml="%JUNIT_OUT%" distributed/tests/test_tls_functional.py
+%PYTEST% -v -m "not avoid_travis" --junit-xml="%JUNIT_OUT%" distributed
