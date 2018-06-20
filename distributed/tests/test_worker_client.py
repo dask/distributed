e435b2c8fe20a6651987a809af515514950a576b
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-06-20 16:12:21 -0500

    Cleanup test

--- distributed/tests/test_worker_client.py
+++ distributed/tests/test_worker_client.py
@@ -299,15 +299,17 @@ def test_worker_client_rejoins(c, s, a, b):
     assert result
 
 
-@gen_cluster(client=True)
-def test_submit_different_names(c, s, a, b):
+@gen_cluster()
+def test_submit_different_names(s, a, b):
     # https://github.com/dask/distributed/issues/2058
     da = pytest.importorskip('dask.array')
     c = yield Client('localhost:' + s.address.split(":")[-1], loop=s.loop,
                      asynchronous=True)
-    with c:
+    try:
         X = c.persist(da.random.uniform(size=(100, 10), chunks=50))
         yield wait(X)
 
         fut = yield c.submit(lambda x: x.sum().compute(), X)
         assert fut > 0
+    finally:
+        yield c.close()
