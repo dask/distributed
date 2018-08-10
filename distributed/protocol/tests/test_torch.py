1bf06c4a2be00c7d56d14c975642d8d3a2b6f26d
Matthew Rocklin <mrocklin@gmail.com>
2018-08-09 20:05:17 -0400

    Better torch.Tensor implementation

--- distributed/protocol/tests/test_torch.py
+++ distributed/protocol/tests/test_torch.py
@@ -14,11 +14,20 @@ def test_tensor():
     assert (x == t2.numpy()).all()
 
 
+def test_grad():
+    x = np.arange(10)
+    t = torch.Tensor(x)
+    t.grad = torch.zeros_like(t) + 1
+
+    t2 = deserialize(*serialize(t))
+    assert (t2.numpy() == x).all()
+    assert (t2.grad.numpy() == 1).all()
+
+
 def test_resnet():
     torchvision = pytest.importorskip('torchvision')
     model = torchvision.models.resnet.resnet18()
 
     header, frames = serialize(model)
-    import pdb; pdb.set_trace()
     model2 = deserialize(header, frames)
     assert str(model) == str(model2)
--- distributed/protocol/torch.py
+++ distributed/protocol/torch.py
@@ -8,6 +8,10 @@ import numpy as np
 @dask_serialize.register(torch.Tensor)
 def serialize_torch_Tensor(t):
     header, frames = serialize(t.numpy())
+    if t.grad is not None:
+        grad_header, grad_frames = serialize(t.grad.numpy())
+        header['grad'] = {'header': grad_header, 'start': len(frames)}
+        frames += grad_frames
     header['requires_grad'] = t.requires_grad
     header['device'] = t.device.type
     return header, frames
@@ -15,10 +19,25 @@ def serialize_torch_Tensor(t):
 
 @dask_deserialize.register(torch.Tensor)
 def deserialize_torch_Tensor(header, frames):
+    if header.get('grad', False):
+        i = header['grad']['start']
+        frames, grad_frames = frames[:i], frames[i:]
+        grad = dask_deserialize.dispatch(np.ndarray)(header['grad']['header'], grad_frames)
+    else:
+        grad = None
+
     x = dask_deserialize.dispatch(np.ndarray)(header, frames)
-    return torch.tensor(data=x,
-                        device=header['device'],
-                        requires_grad=header['requires_grad'])
+    if header['device'] == 'cpu':
+        t = torch.from_numpy(x)
+        if header['requires_grad']:
+            t = t.requires_grad_(True)
+    else:
+        t = torch.tensor(data=x,
+                         device=header['device'],
+                         requires_grad=header['requires_grad'])
+    if grad is not None:
+        t.grad = torch.from_numpy(grad)
+    return t
 
 
 @dask_serialize.register(torch.nn.Parameter)
