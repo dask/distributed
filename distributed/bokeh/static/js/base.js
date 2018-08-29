bf50888f347552411e45b802c4e3351597ce78bc
Luke Canavan <canavandl@gmail.com>
2018-08-29 18:31:04 -0500

    Move navbar JS into js file

---
+++ distributed/bokeh/static/js/base.js
@@ -0,0 +1,9 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
