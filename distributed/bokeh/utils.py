245104653f4ce6acf8a9b9146b296234f81c2b18
Jim Crist <jiminy.crist@gmail.com>
2018-11-30 12:38:27 -0600

    Turn off bokeh property validation in dashboard
    Bokeh 1.0.0 added a method to turn off property validation contextually,
    improving performance. Here we make use that method to remove validation
    in the dashboard update methods. For older versions of bokeh this PR is
    a no-op.

--- distributed/bokeh/utils.py
+++ distributed/bokeh/utils.py
@@ -1,6 +1,19 @@
 from __future__ import print_function, division, absolute_import
 
+from distutils.version import LooseVersion
+
 from toolz import partition
+
+import bokeh
+
+BOKEH_VERSION = LooseVersion(bokeh.__version__)
+
+
+if BOKEH_VERSION >= '1.0.0':
+    from bokeh.core.properties import without_property_validation
+else:
+    def without_property_validation(f):
+        return f
 
 
 def parse_args(args):
