2f37872f628335424906464143cac3810dabd249
Jim Crist <jiminy.crist@gmail.com>
2019-02-04 18:33:59 -0600

    Find Futures inside SubgraphCallable
    New releases of `dask` contain a `SubgraphCallable`, which is an opaque
    object that contains a subgraph to be computed as a single task. This
    worked fine in many cases, but would fail if that graph contained any
    Futures, as distributed wouldn't find them and convert them to data
    before computing. We now traverse inside `SubgraphCallable` objects,
    pulling out any found futures.
    
    This feels like a bit of a hack to me, but I can't think of a better way
    to do this without making more major changes to both codebases.

--- distributed/tests/test_client.py
+++ distributed/tests/test_client.py
@@ -27,6 +27,7 @@ from tornado.ioloop import IOLoop
 
 import dask
 from dask import delayed
+from dask.optimization import SubgraphCallable
 import dask.bag as db
 from distributed import (Worker, Nanny, fire_and_forget, LocalCluster,
                          get_client, secede, get_worker, Executor, profile,
@@ -5496,6 +5497,34 @@ def test_profile_bokeh(c, s, a, b):
     with tmpfile('html') as fn:
         yield c.profile(filename=fn)
         assert os.path.exists(fn)
+
+
+@gen_cluster(client=True)
+def test_get_mix_futures_and_SubgraphCallable(c, s, a, b):
+    future = c.submit(add, 1, 2)
+
+    subgraph = SubgraphCallable({'_2': (add, '_0', '_1'),
+                                 '_3': (add, future, '_2')},
+                                '_3', ('_0', '_1'))
+    dsk = {'a': 1,
+           'b': 2,
+           'c': (subgraph, 'a', 'b'),
+           'd': (subgraph, 'c', 'b')}
+
+    future2 = c.get(dsk, 'd', sync=False)
+    result = yield future2
+    assert result == 11
+
+    # Nested subgraphs
+    subgraph2 = SubgraphCallable({'_2': (subgraph, '_0', '_1'),
+                                  '_3': (subgraph, '_2', '_1'),
+                                  '_4': (add, '_3', future2)},
+                                 '_4', ('_0', '_1'))
+
+    dsk2 = {'e': 1, 'f': 2, 'g': (subgraph2, 'e', 'f')}
+
+    result = yield c.get(dsk2, 'g', sync=False)
+    assert result == 22
 
 
 if sys.version_info >= (3, 5):
