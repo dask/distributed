4fc6acd9490958120497d91a28e6ed63ab5b73f9
Tom Augspurger <taugspurger@anaconda.com>
2019-02-25 11:20:19 -0800

    fixups

--- distributed/protocol/cuda.py
+++ distributed/protocol/cuda.py
@@ -15,19 +15,31 @@ from .serialize import dask_serialize, dask_deserialize
 def serialize_cupy_ndarray(x):
     # TODO: handle non-contiguous
     # TODO: handle 2d
+    # TODO: 0d
+
+    if x.flags.c_contiguous or x.flags.f_contiguous:
+        strides = x.strides
+        data = x.ravel()  # order='K'
+    else:
+        x = cupy.ascontiguousarray(x)
+        strides = x.strides
+        data = x.ravel()
+
+    dtype = (0, x.dtype.str)
+
     header = x.__cuda_array_interface__.copy()
     header['lengths'] = (x.nbytes,)  # one per stride
     header['compression'] = (None,)  # TODO
     header['is_cuda'] = True
-    return header, [x]
+    header['dtype'] = dtype
+    return header, [data]
 
 
 @dask_deserialize.register(cupy.ndarray)
 def deserialize_cupy_array(header, frames):
-    # MemoryPointer { PoolMemory, offset }
     frame, = frames
     # TODO: put this in ucx... as a kind of "fixup"
     frame.typestr = header['typestr']
-    frame._shape = header['shape']
+    frame.shape = header['shape']
     arr = cupy.asarray(frame)
     return arr
