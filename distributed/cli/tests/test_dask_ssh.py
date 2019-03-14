0bd12f420469788875347088b0ac3bbf913f29ed
Willi Rath <wrath@geomar.de>
2019-03-14 12:06:31 +0100

    Don't do calculations in basic test

--- distributed/cli/tests/test_dask_ssh.py
+++ distributed/cli/tests/test_dask_ssh.py
@@ -16,10 +16,9 @@ def test_basic(loop):
     with popen(['dask-ssh', '--scheduler-port', '54321',
                 '127.0.0.1', '127.0.0.1'],
                stdin=subprocess.DEVNULL):
-        with Client("tcp://127.0.0.1:54321") as c:
+        with Client("tcp://127.0.0.1:54321", loop=loop) as c:
+
             start = time()
             while len(c.scheduler_info()['workers']) != 2:
                 assert time() < start + 10
                 sleep(0.2)
-
-            assert c.submit(lambda x: x + 1, 10, workers=1).result() == 11
