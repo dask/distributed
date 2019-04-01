657381d1b663258fe50383a6f2af0b772964702b
Matthew Rocklin <mrocklin@gmail.com>
2019-04-01 13:50:36 -0700

    fix tests

--- distributed/deploy/tests/test_local.py
+++ distributed/deploy/tests/test_local.py
@@ -186,12 +186,6 @@ def test_Client_twice(loop):
 def test_defaults():
     from distributed.worker import _ncores
 
-    with LocalCluster(scheduler_port=0, silence_logs=False,
-                      diagnostics_port=None) as c:
-        assert sum(w.ncores for w in c.workers) == _ncores
-        assert all(isinstance(w, Nanny) for w in c.workers)
-        assert all(w.ncores == 1 for w in c.workers)
-
     with LocalCluster(processes=False, scheduler_port=0, silence_logs=False,
                       diagnostics_port=None) as c:
         assert sum(w.ncores for w in c.workers) == _ncores
@@ -577,6 +571,8 @@ def test_protocol_tcp(loop):
         assert cluster.scheduler.address.startswith('tcp://')
 
 
+@pytest.mark.skipif(not sys.platform.startswith('linux'),
+                    reason="Need 127.0.0.2 to mean localhost")
 def test_protocol_ip(loop):
     with LocalCluster(ip='tcp://127.0.0.2', loop=loop, processes=False) as cluster:
         assert cluster.scheduler.address.startswith('tcp://127.0.0.2')
