2dcd6829aa3206033b288332ca39f3a5cffd7649
Matthew Rocklin <mrocklin@gmail.com>
2019-05-03 21:45:56 -0500

    more _close -> close renaming

--- distributed/tests/test_worker.py
+++ distributed/tests/test_worker.py
@@ -251,8 +251,8 @@ def test_upload_egg(c, s, a, b):
     result = yield future
     assert result == 10 + 1
 
-    yield a._close()
-    yield b._close()
+    yield a.close()
+    yield b.close()
     assert not os.path.exists(os.path.join(a.local_dir, eggname))
 
 
@@ -278,8 +278,8 @@ def test_upload_pyz(c, s, a, b):
     result = yield future
     assert result == 10 + 1
 
-    yield a._close()
-    yield b._close()
+    yield a.close()
+    yield b.close()
     assert not os.path.exists(os.path.join(a.local_dir, pyzname))
 
 
@@ -309,7 +309,7 @@ def test_worker_with_port_zero():
     assert isinstance(w.port, int)
     assert w.port > 1024
 
-    yield w._close()
+    yield w.close()
 
 
 @slow
@@ -392,7 +392,7 @@ def test_spill_to_disk(c, s):
     yield x
     assert set(w.data.fast) == {x.key, z.key}
     assert set(w.data.slow) == {y.key} or set(w.data.slow) == {x.key, y.key}
-    yield w._close()
+    yield w.close()
 
 
 @gen_cluster(client=True)
@@ -443,7 +443,7 @@ def test_Executor(c, s):
 
         assert e._threads  # had to do some work
 
-        yield w._close()
+        yield w.close()
 
 
 @gen_cluster(
@@ -932,8 +932,8 @@ def test_global_workers(s, a, b):
     n = len(_global_workers)
     w = _global_workers[-1]()
     assert w is a or w is b
-    yield a._close()
-    yield b._close()
+    yield a.close()
+    yield b.close()
     assert len(_global_workers) == n - 2
 
 
@@ -952,7 +952,7 @@ def test_worker_fds(s):
         yield gen.sleep(0.01)
         assert time() < start + 1
 
-    yield worker._close()
+    yield worker.close()
 
     start = time()
     while psutil.Process().num_fds() > start:
@@ -971,19 +971,19 @@ def test_service_hosts_match_worker(s):
     yield w._start("tcp://0.0.0.0")
     sock = first(w.services["bokeh"].server._http._sockets.values())
     assert sock.getsockname()[0] in ("::", "0.0.0.0")
-    yield w._close()
+    yield w.close()
 
     w = Worker(s.address, services={("bokeh", ":0"): BokehWorker})
     yield w._start("tcp://127.0.0.1")
     sock = first(w.services["bokeh"].server._http._sockets.values())
     assert sock.getsockname()[0] in ("::", "0.0.0.0")
-    yield w._close()
+    yield w.close()
 
     w = Worker(s.address, services={("bokeh", 0): BokehWorker})
     yield w._start("tcp://127.0.0.1")
     sock = first(w.services["bokeh"].server._http._sockets.values())
     assert sock.getsockname()[0] == "127.0.0.1"
-    yield w._close()
+    yield w.close()
 
 
 @gen_cluster(ncores=[])
