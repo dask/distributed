ed3f9b628c67e4a9d2d17b0a6abc53563e9cf6bc
Benjamin Zaitlen <quasiben@gmail.com>
2019-05-21 09:59:00 -0400

    clenaup proxy file

--- distributed/proxy/proxy_html.py
+++ distributed/proxy/proxy_html.py
@@ -1,19 +1,5 @@
-from urllib import parse
 
-from tornado import web
-
-from notebook.utils import url_path_join
 from jupyter_server_proxy.handlers import ProxyHandler
-
-import tornado.ioloop
-import tornado.web
-from bokeh.server.server import BaseServer
-from tornado.httpserver import HTTPServer
-
-
-def get_host():
-    return "127.0.0.1"
-
 
 class GlobalProxyHandler(ProxyHandler):
     """
@@ -97,36 +83,3 @@ def check_worker_bokeh_exits(scheduler, worker):
         if addr == w.host and port == str(bokeh_port):
             return True
     return False
-
-
-class Proxy(tornado.web.Application):
-    def __init__(self, scheduler, io_loop=None, prefix="", **kwargs):
-        self.scheduler = scheduler
-        prefix = prefix or ""
-        prefix = prefix.rstrip("/")
-        if prefix and not prefix.startswith("/"):
-            prefix = "/" + prefix
-        self.prefix = prefix
-
-        self.server_kwargs = kwargs
-        self.server_kwargs["prefix"] = prefix or None
-
-        self.loop = io_loop or scheduler.loop
-        self.server = None
-
-    def listen(self, *args, **kwargs):
-        addr, port = args[0]
-        if not addr:
-            # proxy should run with the same host as
-            # scheduler
-            addr = self.scheduler.ip
-        self.ip = addr
-        self.port = port
-
-        handlers = [
-            (r"/proxy/(\d+)(.*)", GlobalProxyHandler, {"server": self.scheduler})
-        ]
-        self.application = tornado.web.Application(handlers)
-
-        self.server = HTTPServer(self.application)
-        self.server.listen(port=self.port, address=self.ip)
