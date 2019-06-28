fe2c0e07376d54837c730809e835d4336060e1d3
Gabriel Sailer <gabriel.sailer@blue-yonder.com>
2019-06-28 16:42:48 +0200

    Add basic task metrics to worker
    Number of tasks in states and number of threads are exposed on the
    workers /metrics endpoints.

--- distributed/dashboard/worker_html.py
+++ distributed/dashboard/worker_html.py
@@ -3,21 +3,39 @@ from .utils import RequestHandler, redirect
 
 class _PrometheusCollector(object):
     def __init__(self, server, prometheus_client):
-        self.server = server
+        self.worker = server
 
     def collect(self):
-        # add your metrics here:
-        #
-        # 1. remove the following lines
-        while False:
-            yield None
-        #
-        # 2. yield your metrics
-        #     yield prometheus_client.core.GaugeMetricFamily(
-        #         'dask_worker_connections',
-        #         'Number of connections currently open.',
-        #         value=???,
-        #     )
+        from prometheus_client.core import GaugeMetricFamily
+
+        yield GaugeMetricFamily(
+            "dask_worker_tasks_stored",
+            "Number of tasks stored",
+            value=len(self.worker.data),
+        )
+        yield GaugeMetricFamily(
+            "dask_worker_tasks_ready",
+            "Number of tasks ready",
+            value=len(self.worker.ready),
+        )
+        yield GaugeMetricFamily(
+            "dask_worker_tasks_waiting",
+            "Number of tasks waiting",
+            value=len(self.worker.waiting_for_data),
+        )
+        yield GaugeMetricFamily(
+            "dask_worker_connections",
+            "Number of task connections",
+            value=len(self.worker.in_flight_workers),
+        )
+        yield GaugeMetricFamily(
+            "dask_worker_tasks_serving",
+            "Number of tasks serving",
+            value=len(self.worker._comms),
+        )
+        yield GaugeMetricFamily(
+            "dask_worker_nthreads", "Number of threads", value=self.worker.nthreads
+        )
 
 
 class PrometheusHandler(RequestHandler):
