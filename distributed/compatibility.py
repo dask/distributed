09feb2b4d4acc4fe923ccf402a805777f71f86af
Matthew Rocklin <mrocklin@gmail.com>
2019-07-27 13:02:58 -0700

    further cleanup compatibility.py

--- distributed/compatibility.py
+++ distributed/compatibility.py
@@ -1,19 +1,11 @@
 from __future__ import print_function, division, absolute_import
 
 import logging
+import platform
 import sys
-
-# flake8: noqa
-
-
-def isqueue(o):
-    return isinstance(o, Queue)
-
 
 logging_names = logging._levelToName.copy()
 logging_names.update(logging._nameToLevel)
-
-import platform
 
 PYPY = platform.python_implementation().lower() == "pypy"
 WINDOWS = sys.platform.startswith("win")
