796caa02304ff352ac22e891f60f2036ccbd8145
rjzamora <rzamora217@gmail.com>
2019-08-23 10:40:06 -0700

    addressing #2992 - fixing pynvml initialization

--- distributed/diagnostics/nvml.py
+++ distributed/diagnostics/nvml.py
@@ -1,12 +1,20 @@
 import pynvml
 
-pynvml.nvmlInit()
-count = pynvml.nvmlDeviceGetCount()
+need_pynvml_init = True
+handles = None
 
-handles = [pynvml.nvmlDeviceGetHandleByIndex(i) for i in range(count)]
+def _initialize_pynvml():
+    global need_pynvml_init, handles
+    pynvml.nvmlInit()
+    count = pynvml.nvmlDeviceGetCount()
+    handles = [pynvml.nvmlDeviceGetHandleByIndex(i) for i in range(count)]
+    need_pynvml_init = False
 
 
 def real_time():
+    global need_pynvml_init, handles
+    if need_pynvml_init:
+        _initialize_pynvml()
     return {
         "utilization": [pynvml.nvmlDeviceGetUtilizationRates(h).gpu for h in handles],
         "memory-used": [pynvml.nvmlDeviceGetMemoryInfo(h).used for h in handles],
@@ -14,7 +22,11 @@ def real_time():
 
 
 def one_time():
+    global need_pynvml_init, handles
+    if need_pynvml_init:
+        _initialize_pynvml()
     return {
         "memory-total": [pynvml.nvmlDeviceGetMemoryInfo(h).total for h in handles],
         "name": [pynvml.nvmlDeviceGetName(h).decode() for h in handles],
     }
+
