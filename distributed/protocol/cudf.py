017c67d23f19ea3946c5dd169ba0fc9eaa565d40
Benjamin Zaitlen <quasiben@gmail.com>
2019-08-26 11:25:48 -0700

    change cudf groupby import

--- distributed/protocol/cudf.py
+++ distributed/protocol/cudf.py
@@ -1,6 +1,6 @@
 import pickle
 import cudf
-import cudf.groupby.groupby
+import cudf.core.groupby.groupby
 from .cuda import cuda_serialize, cuda_deserialize
 from ..utils import log_errors
 
@@ -8,14 +8,18 @@ from ..utils import log_errors
 # all (de-)serializtion code lives in the cudf codebase
 # here we ammend the returned headers with `is_gpu` for
 # UCX buffer consumption
-@cuda_serialize.register((cudf.DataFrame, cudf.Series, cudf.groupby.groupby._Groupby))
+@cuda_serialize.register(
+    (cudf.DataFrame, cudf.Series, cudf.core.groupby.groupby._Groupby)
+)
 def serialize_cudf_dataframe(x):
     with log_errors():
         header, frames = x.serialize()
         return header, frames
 
 
-@cuda_deserialize.register((cudf.DataFrame, cudf.Series, cudf.groupby.groupby._Groupby))
+@cuda_deserialize.register(
+    (cudf.DataFrame, cudf.Series, cudf.core.groupby.groupby._Groupby)
+)
 def serialize_cudf_dataframe(header, frames):
     with log_errors():
         cudf_typ = pickle.loads(header["type"])
