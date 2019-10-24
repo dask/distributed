0bd42fd90d5e7959625a994d750b5267b6801286
SimonBoothroyd <simon.boothroyd@choderalab.org>
2019-10-24 16:34:12 -0400

    Move logic into adaptive main.

--- distributed/deploy/adaptive_core.py
+++ distributed/deploy/adaptive_core.py
@@ -179,11 +179,6 @@ class AdaptiveCore:
 
         try:
 
-            if len(self.plan) != len(self.requested):
-                # Ensure that the number of planned and requested workers
-                # stay in sync.
-                await self.cluster
-
             target = await self.safe_target()
             recommendations = await self.recommendations(target)
 
