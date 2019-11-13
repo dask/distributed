abb150967ba35c6ea7830bf1d2e561e33d68d0c0
Matthew Rocklin <mrocklin@gmail.com>
2019-11-12 16:10:33 -0800

    Remove the phrase "status" from Future repr

--- distributed/client.py
+++ distributed/client.py
@@ -386,13 +386,9 @@ class Future(WrappedKey):
                 typ = self.type.__module__.split(".")[0] + "." + self.type.__name__
             except AttributeError:
                 typ = str(self.type)
-            return "<Future: status: %s, type: %s, key: %s>" % (
-                self.status,
-                typ,
-                self.key,
-            )
+            return "<Future: %s, type: %s, key: %s>" % (self.status, typ, self.key)
         else:
-            return "<Future: status: %s, key: %s>" % (self.status, self.key)
+            return "<Future: %s, key: %s>" % (self.status, self.key)
 
     def _repr_html_(self):
         text = "<b>Future: %s</b> " % html.escape(key_split(self.key))
