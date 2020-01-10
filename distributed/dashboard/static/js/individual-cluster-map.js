30e4f5f3ae8e2915298eb3ab905ca536680544e5
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-01-10 14:31:03 +0000

    Tidy up code, fox reset bugs and reconnect websocket

--- distributed/dashboard/static/js/individual-cluster-map.js
+++ distributed/dashboard/static/js/individual-cluster-map.js
@@ -4,13 +4,20 @@
 // TODO Show future retrieval
 // TODO Show graph submission
 
+const workerColor = "#ECB172";
+const workerIdleSize = 12;
+const workerBusySize = 15;
+
+// Expose in the global scope
+var dashboard;
+
 class Dashboard {
   constructor() {
     this.workers = [];
     this.scheduler = "scheduler";
     this.schedulerNode = null;
     this.dashboard = document.getElementById("vis");
-    this.transfers = [];
+    this.add_scheduler();
   }
 
   handle_event(event) {
@@ -39,6 +46,21 @@ class Dashboard {
         } else if (event["action"] === "transfer") {
           this.run_transfer(
             this.hash_worker(event["source"]),
+            this.hash_worker(event["worker"]),
+            event["stop"] - event["start"]
+          );
+          break;
+        } else if (
+          event["action"] === "disk-read" ||
+          event["action"] === "disk-write"
+        ) {
+          this.run_swap(
+            this.hash_worker(event["worker"]),
+            event["stop"] - event["start"]
+          );
+          break;
+        } else if (event["action"] === "deserialize") {
+          this.run_deserialize(
             this.hash_worker(event["worker"]),
             event["stop"] - event["start"]
           );
@@ -95,7 +117,12 @@ class Dashboard {
   remove_worker(id) {
     // Remove circle element from SVG
     let worker = document.getElementById(id);
-    this.dashboard.removeChild(worker);
+    anime({
+      targets: worker,
+      opacity: 0,
+      duration: 1000,
+      complete: () => this.dashboard.removeChild(worker)
+    });
 
     // Remove worker from list of workers
     let index = this.workers.indexOf(id);
@@ -105,6 +132,25 @@ class Dashboard {
 
     // Reposition other workers to fill in the gap
     this.update_worker_positions();
+  }
+
+  connected() {
+    anime({
+      targets: "#scheduler",
+      opacity: 1,
+      duration: 1000
+    });
+  }
+
+  disconnected() {
+    while (this.workers.length > 0) {
+      this.remove_worker(this.workers[0]);
+    }
+    anime({
+      targets: "#scheduler",
+      opacity: 0.3,
+      duration: 1000
+    });
   }
 
   update_worker_positions() {
@@ -118,7 +164,7 @@ class Dashboard {
       let y = k + r * Math.sin(θ);
       anime({
         targets: "#" + this.workers[i],
-        r: 15,
+        r: workerIdleSize,
         cx: x + "%",
         cy: y + "%",
         easing: "easeInOutQuint",
@@ -142,31 +188,35 @@ class Dashboard {
       .add(
         {
           fill: color,
+          r: workerBusySize,
           begin: () => this.dashboard.removeChild(arc)
         },
         500
       )
-      .add({ fill: null }, "+=" + duration);
+      .add({ fill: workerColor, r: workerIdleSize }, "+=" + duration);
   }
 
   run_transfer(start_worker, end_worker, duration) {
     start_worker = document.getElementById(start_worker);
     end_worker = document.getElementById(end_worker);
-    duration = Math.max(250, duration);
+    duration = Math.max(250, duration / 1000);
     let color = "rgba(255, 0, 0, .6)";
     let arc = this.draw_arc(start_worker, end_worker, color, "transfer");
 
     anime
       .timeline({
-        targets: ["#" + start_worker, "#" + end_worker]
+        targets: ["#" + start_worker, "#" + end_worker],
+        duration: 250
       })
       .add({
         fill: color,
+        r: workerBusySize,
         begin: () => this.dashboard.insertBefore(arc, this.schedulerNode)
       })
       .add(
         {
-          fill: null,
+          fill: workerColor,
+          r: workerIdleSize,
           begin: () => this.dashboard.removeChild(arc)
         },
         "+=" + duration
@@ -180,11 +230,32 @@ class Dashboard {
         duration: 250
       })
       .add({
-        fill: "#D67548"
+        fill: "#D67548",
+        r: workerBusySize
       })
       .add(
         {
-          fill: null
+          fill: workerColor,
+          r: workerIdleSize
+        },
+        "+=" + duration
+      );
+  }
+
+  run_deserialize(worker, duration) {
+    anime
+      .timeline({
+        targets: "#" + worker,
+        duration: 250
+      })
+      .add({
+        fill: "gray",
+        r: workerBusySize
+      })
+      .add(
+        {
+          fill: workerColor,
+          r: workerIdleSize
         },
         "+=" + duration
       );
@@ -202,46 +273,33 @@ class Dashboard {
     for (var i = 0; i < this.workers.length; i++) {
       anime({
         targets: "#" + this.workers[i],
-        fill: null,
+        fill: workerColor,
+        r: workerIdleSize,
         duration: 250
       });
     }
-    for (var arc in this.transfers) {
-      this.dashboard.removeChild(tarc);
-      var index = this.transfers.indexOf(arc);
-      if (index !== -1) this.transfers.splice(index, 1);
-    }
   }
 
-  draw_arc(start_element, end_element, color, class_name) {
-    let start_x = getAbsoluteXY(start_element)[0];
-    let start_y = getAbsoluteXY(start_element)[1];
-    let end_x = getAbsoluteXY(end_element)[0];
-    let end_y = getAbsoluteXY(end_element)[1];
-    let arc = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    arc.setAttributeNS(null, "id", class_name);
-    arc.setAttributeNS(null, "class", class_name);
-    arc.setAttributeNS(null, "stroke", color);
-
+  calculate_arc(start_x, start_y, end_x, end_y) {
     // mid-point of line:
-    var mpx = (start_x + end_x) * 0.5;
-    var mpy = (start_y + end_y) * 0.5;
+    let mpx = (start_x + end_x) * 0.5;
+    let mpy = (start_y + end_y) * 0.5;
 
     // angle of perpendicular to line:
-    var theta = Math.atan2(start_y - end_y, start_x - end_x) - Math.PI / 2;
+    let theta = Math.atan2(start_y - end_y, start_x - end_x) - Math.PI / 2;
 
     // distance of control point from mid-point of line:
-    var offset = Math.random() * 50;
+    let offset = Math.random() * 50;
     if (Math.random() >= 0.5) {
       offset = -offset;
     }
 
     // location of control point:
-    var c1x = mpx + offset * Math.cos(theta);
-    var c1y = mpy + offset * Math.sin(theta);
+    let c1x = mpx + offset * Math.cos(theta);
+    let c1y = mpy + offset * Math.sin(theta);
 
     // construct the command to draw a quadratic curve
-    var curve =
+    return (
       "M" +
       end_x +
       " " +
@@ -253,46 +311,56 @@ class Dashboard {
       " " +
       start_x +
       " " +
-      start_y;
+      start_y
+    );
+  }
+
+  draw_arc(start_element, end_element, color, class_name) {
+    let curve = this.calculate_arc(
+      this.getAbsoluteXY(start_element)[0],
+      this.getAbsoluteXY(start_element)[1],
+      this.getAbsoluteXY(end_element)[0],
+      this.getAbsoluteXY(end_element)[1]
+    );
+
+    let arc = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    arc.setAttributeNS(null, "id", class_name);
+    arc.setAttributeNS(null, "class", class_name);
+    arc.setAttributeNS(null, "stroke", color);
     arc.setAttribute("d", curve);
     return arc;
   }
+
+  getAbsoluteXY(element) {
+    var box = element.getBoundingClientRect();
+    var x = box.left + box.width / 4;
+    var y = box.top + box.height / 4;
+    return [x, y];
+  }
 }
 
-function getAbsoluteXY(element) {
-  var box = element.getBoundingClientRect();
-  var x = box.left + box.width / 4;
-  var y = box.top + box.height / 4;
-  return [x, y];
-}
-
-var dashboard;
-
-function websocket_url(s) {
+function get_websocket_url(endpoint) {
   var l = window.location;
   return (
     (l.protocol === "https:" ? "wss://" : "ws://") +
     l.hostname +
     (l.port != 80 && l.port != 443 ? ":" + l.port : "") +
-    s
+    endpoint
   );
 }
 
 function main() {
-  console.log("Starting...");
   dashboard = new Dashboard();
-  dashboard.add_scheduler();
 
-  var ws = new WebSocket(websocket_url("/eventstream"));
+  var ws = new ReconnectingWebSocket(get_websocket_url("/eventstream"));
   ws.onopen = function() {
-    ws.send(
-      JSON.stringify({
-        name: "ping"
-      })
-    );
+    dashboard.connected();
   };
   ws.onmessage = function(event) {
     dashboard.handle_event(JSON.parse(event.data));
+  };
+  ws.onclose = function() {
+    dashboard.disconnected();
   };
 }
 
