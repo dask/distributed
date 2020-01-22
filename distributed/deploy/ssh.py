8050788f7df776d476c0ddb6d54ee819f9e5c40e
Peter Andreas Entschev <peter@entschev.com>
2020-01-22 11:22:20 -0800

    Avoid ssh attributes from getting overwritten by superclass

--- distributed/deploy/ssh.py
+++ distributed/deploy/ssh.py
@@ -73,14 +73,14 @@ class Worker(Process):
         loop=None,
         name=None,
     ):
+        super().__init__()
+
         self.address = address
         self.scheduler = scheduler
         self.worker_module = worker_module
         self.connect_options = connect_options
         self.kwargs = kwargs
         self.name = name
-
-        super().__init__()
 
     async def start(self):
         import asyncssh  # import now to avoid adding to module startup time
@@ -131,11 +131,11 @@ class Scheduler(Process):
     """
 
     def __init__(self, address: str, connect_options: dict, kwargs: dict):
+        super().__init__()
+
         self.address = address
         self.kwargs = kwargs
         self.connect_options = connect_options
-
-        super().__init__()
 
     async def start(self):
         import asyncssh  # import now to avoid adding to module startup time
