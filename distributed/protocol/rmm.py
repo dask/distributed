0ef31b011c934b01c0dc0dea0c6a9b2e8a0abc80
John Kirkham <jakirkham@gmail.com>
2020-02-05 08:34:48 -0800

    Fix some minor formatting issues

--- distributed/protocol/rmm.py
+++ distributed/protocol/rmm.py
@@ -4,12 +4,12 @@ from .cuda import cuda_serialize, cuda_deserialize
 
 # Used for RMM 0.11.0+ otherwise Numba serializers used
 if hasattr(rmm, "DeviceBuffer"):
+
     @cuda_serialize.register(rmm.DeviceBuffer)
     def serialize_rmm_device_buffer(x):
         header = x.__cuda_array_interface__.copy()
         frames = [x]
         return header, frames
-
 
     @cuda_deserialize.register(rmm.DeviceBuffer)
     def deserialize_rmm_device_buffer(header, frames):
