72f71c8ac2bd2a52676b49159816f09f9a86518b
John Kirkham <jakirkham@gmail.com>
2020-02-04 20:58:27 -0800

    Test serializing RMM `DeviceBuffer` from Numba

--- distributed/protocol/tests/test_rmm.py
+++ distributed/protocol/tests/test_rmm.py
@@ -1,4 +1,5 @@
 from distributed.protocol import serialize, deserialize
+import pickle
 import pytest
 
 numpy = pytest.importorskip("numpy")
@@ -17,6 +18,23 @@ def test_serialize_rmm_device_buffer(size):
     cuda.to_device(x_np, to=cuda.as_cuda_array(x))
 
     header, frames = serialize(x, serializers=("cuda", "dask", "pickle"))
+    y = deserialize(header, frames, deserializers=("cuda", "dask", "pickle", "error"))
+    y_np = cuda.as_cuda_array(y).copy_to_host()
+
+    assert (x_np == y_np).all()
+
+
+@pytest.mark.parametrize("size", [0, 3, 10])
+def test_serialize_rmm_device_buffer_from_numba_array(size):
+    DeviceBuffer = getattr(rmm, "DeviceBuffer", None)
+    if not DeviceBuffer:
+        pytest.skip("RMM pre-0.11.0 does not have DeviceBuffer")
+
+    x_np = numpy.arange(size, dtype="u1")
+    x = cuda.to_device(x_np)
+
+    header, frames = serialize(x, serializers=("cuda", "dask", "pickle"))
+    header["type-serialized"] = pickle.dumps(DeviceBuffer)
     y = deserialize(header, frames, deserializers=("cuda", "dask", "pickle", "error"))
     y_np = cuda.as_cuda_array(y).copy_to_host()
 
