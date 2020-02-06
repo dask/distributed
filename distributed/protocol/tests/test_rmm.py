2fa724fd2cd8a172b0e85c529f56523338dc86b2
John Kirkham <jakirkham@gmail.com>
2020-02-06 10:27:20 -0800

    Drop unneeded Numba-backed RMM serialization test
    As RMM is used preferentially for allocations when available, there
    shouldn't be a case where we need to serialize an RMM `DeviceBuffer`
    from a Numba array. So drop this test.

--- distributed/protocol/tests/test_rmm.py
+++ distributed/protocol/tests/test_rmm.py
@@ -21,19 +21,3 @@ def test_serialize_rmm_device_buffer(size):
     y_np = y.copy_to_host()
 
     assert (x_np == y_np).all()
-
-
-@pytest.mark.parametrize("size", [0, 3, 10])
-def test_serialize_rmm_device_buffer_from_numba_array(size):
-    if not hasattr(rmm, "DeviceBuffer"):
-        pytest.skip("RMM pre-0.11.0 does not have DeviceBuffer")
-
-    x_np = numpy.arange(size, dtype="u1")
-    x = cuda.to_device(x_np)
-
-    header, frames = serialize(x, serializers=("cuda", "dask", "pickle"))
-    header["type-serialized"] = pickle.dumps(rmm.DeviceBuffer)
-    y = deserialize(header, frames, deserializers=("cuda", "dask", "pickle", "error"))
-    y_np = cuda.as_cuda_array(y).copy_to_host()
-
-    assert (x_np == y_np).all()
