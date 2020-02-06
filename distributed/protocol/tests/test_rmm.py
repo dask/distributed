0e39ae12dfe8111994001535cb3e14c5079eb4ca
John Kirkham <jakirkham@gmail.com>
2020-02-05 20:51:26 -0800

    Use namespace with `DeviceBuffer`

--- distributed/protocol/tests/test_rmm.py
+++ distributed/protocol/tests/test_rmm.py
@@ -9,12 +9,11 @@ rmm = pytest.importorskip("rmm")
 
 @pytest.mark.parametrize("size", [0, 3, 10])
 def test_serialize_rmm_device_buffer(size):
-    DeviceBuffer = getattr(rmm, "DeviceBuffer", None)
-    if not DeviceBuffer:
+    if not hasattr(rmm, "DeviceBuffer"):
         pytest.skip("RMM pre-0.11.0 does not have DeviceBuffer")
 
     x_np = numpy.arange(size, dtype="u1")
-    x = DeviceBuffer(size=size)
+    x = rmm.DeviceBuffer(size=size)
     cuda.to_device(x_np, to=cuda.as_cuda_array(x))
 
     header, frames = serialize(x, serializers=("cuda", "dask", "pickle"))
@@ -26,15 +25,14 @@ def test_serialize_rmm_device_buffer(size):
 
 @pytest.mark.parametrize("size", [0, 3, 10])
 def test_serialize_rmm_device_buffer_from_numba_array(size):
-    DeviceBuffer = getattr(rmm, "DeviceBuffer", None)
-    if not DeviceBuffer:
+    if not hasattr(rmm, "DeviceBuffer"):
         pytest.skip("RMM pre-0.11.0 does not have DeviceBuffer")
 
     x_np = numpy.arange(size, dtype="u1")
     x = cuda.to_device(x_np)
 
     header, frames = serialize(x, serializers=("cuda", "dask", "pickle"))
-    header["type-serialized"] = pickle.dumps(DeviceBuffer)
+    header["type-serialized"] = pickle.dumps(rmm.DeviceBuffer)
     y = deserialize(header, frames, deserializers=("cuda", "dask", "pickle", "error"))
     y_np = cuda.as_cuda_array(y).copy_to_host()
 
