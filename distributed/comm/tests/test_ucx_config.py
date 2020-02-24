deb761bc13e9fd24ad17e80ece89e03d011d6a65
Benjamin Zaitlen <quasiben@gmail.com>
2020-02-24 09:55:09 -0800

    dgx check

--- distributed/comm/tests/test_ucx_config.py
+++ distributed/comm/tests/test_ucx_config.py
@@ -1,14 +1,33 @@
+import os
 import pytest
 from time import sleep
-
-ucp = pytest.importorskip("ucp")
-rmm = pytest.importorskip("rmm")
 
 import dask
 from dask.utils import format_bytes
 from distributed import Client, Worker, Scheduler
 from distributed.utils_test import gen_test, loop, inc, cleanup, popen  # noqa: 401
 from distributed.utils import get_ip
+
+
+def _check_dgx_version():
+    dgx_server = None
+
+    if not os.path.isfile("/etc/dgx-release"):
+        return dgx_server
+
+    for line in open("/etc/dgx-release"):
+        if line.startswith("DGX_PLATFORM"):
+            if "DGX Server for DGX-1" in line:
+                dgx_server = 1
+            elif "DGX Server for DGX-2" in line:
+                dgx_server = 2
+            break
+
+    return dgx_server
+
+
+if _check_dgx_version() is None:
+    pytest.skip("Not a DGX server", allow_module_level=True)
 
 try:
     HOST = get_ip()
@@ -18,6 +37,9 @@ except Exception:
 
 @pytest.mark.asyncio
 async def test_ucx_config(cleanup):
+    ucp = pytest.importorskip("ucp")
+    rmm = pytest.importorskip("rmm")
+
     ucx = {
         "nvlink": True,
         "infiniband": True,
@@ -46,12 +68,16 @@ async def test_ucx_config(cleanup):
 
 @pytest.mark.asyncio
 async def test_ucx_w_rmm(cleanup):
+    ucp = pytest.importorskip("ucp")
+    rmm = pytest.importorskip("rmm")
+
+    port = 13338
     ucx = {"nvlink": True, "infiniband": True, "tcp-over-ucx": True, "net-devices": ""}
     size = "1000.00 MB"
     rmm_dict = {"pool-size": size}
 
     with dask.config.set(ucx=ucx, rmm=rmm_dict):
-        async with Scheduler(protocol="ucx") as s:
+        async with Scheduler(protocol="ucx", port=port) as s:
             async with Worker(s.address) as a:
                 async with Client(s.address, asynchronous=True) as c:
 
@@ -76,7 +102,9 @@ async def test_ucx_w_rmm(cleanup):
                     assert size == format_bytes(rmm_usage.free)
 
 
-def test_ucx_config_env_var(cleanup, loop, monkeypatch):
+def test_ucx_config_w_env_var(cleanup, loop, monkeypatch):
+    ucp = pytest.importorskip("ucp")
+    rmm = pytest.importorskip("rmm")
 
     monkeypatch.setenv("DASK_UCX__NVLINK", "False")
     monkeypatch.setenv("DASK_UCX__INFINIBAND", "True")
@@ -85,7 +113,7 @@ def test_ucx_config_env_var(cleanup, loop, monkeypatch):
 
     dask.config.refresh()
 
-    port = "13337"
+    port = "13339"
     sched_addr = "ucx://%s:%s" % (HOST, port)
 
     with popen(
