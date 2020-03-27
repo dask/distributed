99a2730eb80fb56ec059d7a19bf2ca222687ac29
James Bourbeau <jrbourbeau@gmail.com>
2020-03-27 11:23:05 -0500

    Make tests fail [skip travis]

--- conftest.py
+++ conftest.py
@@ -15,6 +15,9 @@ else:
     faulthandler.enable()
 
 
+raise ValueError("AAAAAAAAAAHHHHHHHHH")
+
+
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true", help="run slow tests")
 
