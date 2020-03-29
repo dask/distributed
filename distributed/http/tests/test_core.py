236665969a36e1865bc77bc3c98a9babe1564435
Matthew Rocklin <mrocklin@gmail.com>
2020-03-29 16:48:47 -0700

    Fixup http test

--- distributed/http/tests/test_core.py
+++ distributed/http/tests/test_core.py
@@ -5,5 +5,7 @@ from tornado.httpclient import AsyncHTTPClient
 @gen_cluster(client=True)
 async def test_scheduler(c, s, a, b):
     client = AsyncHTTPClient()
-    response = await client.fetch("http://localhost:8080/health")
+    response = await client.fetch(
+        "http://localhost:{}/health".format(s.http_server.port)
+    )
     assert response.code == 200
