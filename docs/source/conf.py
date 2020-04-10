d06276edd15b480be0ee9088c04e41326805b893
Matthew Rocklin <mrocklin@gmail.com>
2020-04-10 16:25:02 -0700

    Try using sphinx-jsonschema

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "numpydoc",
     "sphinx_click.ext",
+    "sphinx-jsonschema",
 ]
 
 numpydoc_show_class_members = False
@@ -393,7 +394,7 @@ redirect_files = [
     ("joblib.html", "https://ml.dask.org/joblib.html"),
     ("setup.html", "https://docs.dask.org/en/latest/setup.html"),
     ("ec2.html", "https://docs.dask.org/en/latest/setup/cloud.html"),
-    ("configuration.html", "https://docs.dask.org/en/latest/configuration.html"),
+    # ("configuration.html", "https://docs.dask.org/en/latest/configuration.html"),
     (
         "local-cluster.html",
         "https://docs.dask.org/en/latest/setup/single-distributed.html",
---
+++ docs/source/configuration.rst
@@ -0,0 +1,6 @@
+Configuration
+=============
+
+.. jsonschema:: https://raw.githubusercontent.com/jupyterhub/zero-to-jupyterhub-k8s/master/jupyterhub/schema.yaml
+
+.. jsonschema:: ../../distributed/distributed-schema.yaml
