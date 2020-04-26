0a3884a4949a1334edce5dd721ab19d20b2f8f17
Matthew Rocklin <mrocklin@gmail.com>
2020-04-25 18:42:45 -0700

    Apply suggestions from code review
    Co-Authored-By: Benjamin Zaitlen <quasiben@users.noreply.github.com>

--- distributed/tests/test_gpu_metrics.py
+++ distributed/tests/test_gpu_metrics.py
@@ -6,10 +6,10 @@ pytest.importorskip("pynvml")
 
 @gen_cluster()
 async def test_gpu_metrics(s, a, b):
-    from distributed.diagnostics.nvml import count
+from distributed.diagnostics.nvml import handles
 
     assert "gpu" in a.metrics
-    assert len(s.workers[a.address].metrics["gpu"]["memory-used"]) == count
+len(s.workers[a.address].metrics["gpu"]["memory-used"]) == len(handles)
 
     assert "gpu" in a.startup_information
-    assert len(s.workers[a.address].extra["gpu"]["name"]) == count
+len(s.workers[a.address].extra["gpu"]["name"]) == len(handles)
