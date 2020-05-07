5822941081575a8f23dec89d672ec8d20c4a021f
galipremsagar <sagarprem75@gmail.com>
2020-05-06 17:21:33 -0700

    enable commented test

--- distributed/tests/test_client.py
+++ distributed/tests/test_client.py
@@ -1252,18 +1252,18 @@ async def test_get_nbytes(c, s, a, b):
     assert s.get_nbytes(summary=False) == {x.key: sizeof(1), y.key: sizeof(2)}
 
 
-# @pytest.mark.skipif(
-#     not sys.platform.startswith("linux"), reason="Need 127.0.0.2 to mean localhost"
-# )
-# @gen_cluster([("127.0.0.1", 1), ("127.0.0.2", 2)], client=True)
-# async def test_nbytes_determines_worker(c, s, a, b):
-#     x = c.submit(identity, 1, workers=[a.ip])
-#     y = c.submit(identity, tuple(range(100)), workers=[b.ip])
-#     await c.gather([x, y])
+@pytest.mark.skipif(
+    not sys.platform.startswith("linux"), reason="Need 127.0.0.2 to mean localhost"
+)
+@gen_cluster([("127.0.0.1", 1), ("127.0.0.2", 2)], client=True)
+async def test_nbytes_determines_worker(c, s, a, b):
+    x = c.submit(identity, 1, workers=[a.ip])
+    y = c.submit(identity, tuple(range(100)), workers=[b.ip])
+    await c.gather([x, y])
 
-#     z = c.submit(lambda x, y: None, x, y)
-#     await z
-#     assert s.tasks[z.key].who_has == {s.workers[b.address]}
+    z = c.submit(lambda x, y: None, x, y)
+    await z
+    assert s.tasks[z.key].who_has == {s.workers[b.address]}
 
 
 @gen_cluster(client=True)
