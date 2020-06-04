9ab510f9924a36b7f517098b4e105bec43c58fbc
John Kirkham <jakirkham@gmail.com>
2020-06-04 09:16:32 -0700

    Test serializing a collection of `bytes` objects

--- distributed/protocol/tests/test_serialize.py
+++ distributed/protocol/tests/test_serialize.py
@@ -194,7 +194,14 @@ def test_empty_loads_deep():
     "kwargs", [{}, {"serializers": ["pickle"]},],
 )
 def test_serialize_bytes(kwargs):
-    for x in [1, "abc", np.arange(5), b"ab" * int(40e6), 2 ** 26 * b"ab"]:
+    for x in [
+        1,
+        "abc",
+        np.arange(5),
+        b"ab" * int(40e6),
+        2 ** 26 * b"ab",
+        (2 ** 25 * b"ab", 2 ** 25 * b"ab"),
+    ]:
         b = serialize_bytes(x, **kwargs)
         assert isinstance(b, bytes)
         y = deserialize_bytes(b)
