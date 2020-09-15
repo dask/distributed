f972f215ccb5215461a5f45d11b6c8049b317a24
Matthew Rocklin <mrocklin@gmail.com>
2020-09-15 12:19:34 -0700

    Allow RuntimeErrors when closing global clients
    These tend to be due to the event loop being closed.

--- distributed/client.py
+++ distributed/client.py
@@ -4817,7 +4817,7 @@ def _close_global_client():
     c = _get_global_client()
     if c is not None:
         c._should_close_loop = False
-        with suppress(TimeoutError):
+        with suppress(TimeoutError, RuntimeError):
             c.close(timeout=2)
 
 
