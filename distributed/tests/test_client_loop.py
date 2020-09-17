2b76649ef831295a1b603686d0fc6169de096c8a
Sergey Kozlov <skozlovf@gmail.com>
2020-09-17 18:51:12 +0600

    Rework test, improve performance

--- distributed/tests/test_client_loop.py
+++ distributed/tests/test_client_loop.py
@@ -1,49 +1,34 @@
 import pytest
-import threading
-from time import sleep
 from distributed import LocalCluster, Client
 from distributed.utils import LoopRunner
 
 
-def run_client(loop):
-    with LocalCluster(loop=loop, asynchronous=False) as cluster:
-        client = Client(cluster, loop=loop, asynchronous=False)
-        sleep(1)  # Allow IO loop process events.
-        client.close()
-
-
-# Test if Client correctly tears down LoopRunner on close.
+# Test if Client stops LoopRunner on close.
 @pytest.mark.parametrize("with_own_loop", [True, False])
 def test_close_loop_sync(with_own_loop):
     loop_runner = loop = None
 
-    # Number of threads before and after the Cluster running.
-    threads_before = []
-    threads_after = []
+    # Setup simple cluster with one threaded worker.
+    # Complex setup is not required here since we test only IO loop teardown.
+    cluster_params = dict(n_workers=1, dashboard_address=None, processes=False)
 
-    for i in range(3):
-        threads_before.append(threading.enumerate())
+    loops_before = LoopRunner._all_loops.copy()
 
-        if with_own_loop:
-            loop_runner = LoopRunner(asynchronous=False)
-            loop_runner.start()
-            loop = loop_runner.loop
+    # Start own loop or use current thread's one.
+    if with_own_loop:
+        loop_runner = LoopRunner()
+        loop_runner.start()
+        loop = loop_runner.loop
 
-        run_client(loop)
+    with LocalCluster(loop=loop, **cluster_params) as cluster:
+        with Client(cluster, loop=loop) as client:
+            client.run(max, 1, 2)
 
-        if with_own_loop:
-            loop_runner.stop()
+    # own loop must be explicitly stopped.
+    if with_own_loop:
+        loop_runner.stop()
 
-        # Allow IO loop process events and wait until daemon threads finished.
-        sleep(1)
-        threads_after.append(threading.enumerate())
-
-    # TCP backend starts globally and runs few own threads on demand.
-    # 'threads_before' populates before that and contains less threads on first
-    # iteration. That's why we drop first values.
-    threads_before = threads_before[1:]
-    threads_after = threads_after[1:]
-
-    # In all other iterations there must be same number of threads before and
-    # after the Client running.
-    assert all([x == y for (x, y) in zip(threads_before, threads_after)])
+    # Internal loops registry must the same as before cluster running.
+    # This means loop runners in LocalCluster and Client correctly stopped.
+    # See LoopRunner._stop_unlocked().
+    assert loops_before == LoopRunner._all_loops
