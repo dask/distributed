591a6e2d60bf0735de5534860b8f4481e4c5ca6c
John Kirkham <jakirkham@gmail.com>
2020-10-06 16:22:14 -0700

    Test attributes of single handle

--- distributed/tests/test_gpu_metrics.py
+++ distributed/tests/test_gpu_metrics.py
@@ -1,14 +1,16 @@
 import pytest
 from distributed.utils_test import gen_cluster
 
-pytest.importorskip("pynvml")
+pynvml = pytest.importorskip("pynvml")
 
 
 @gen_cluster()
 async def test_gpu_metrics(s, a, b):
     from distributed.diagnostics.nvml import _pynvml_handles as handles
 
+    h = handles()
+
     assert "gpu" in a.metrics
-    assert len(s.workers[a.address].metrics["gpu"]["memory-used"]) == len(handles)
+    assert s.workers[a.address].metrics["gpu"]["memory-used"] == pynvml.nvmlDeviceGetMemoryInfo(h).used
     assert "gpu" in a.startup_information
-    assert len(s.workers[a.address].extra["gpu"]["name"]) == len(handles)
+    assert s.workers[a.address].extra["gpu"]["name"] == pynvml.nvmlDeviceGetName(h).decode()
