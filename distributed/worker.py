0a86e2d248bec946ff30891df7ce4d2b440191a0
manuels <manuel.schoelling@gmx.de>
2020-11-30 12:55:53 +0100

    Print Traceback for an Exception on a worker

--- distributed/worker.py
+++ distributed/worker.py
@@ -2595,10 +2595,12 @@ class Worker(ServerNode):
                         "args:      %s\n"
                         "kwargs:    %s\n"
                         "Exception: %s\n",
+                        "Traceback: %s\n",
                         str(funcname(function))[:1000],
                         convert_args_to_str(args2, max_len=1000),
                         convert_kwargs_to_str(kwargs2, max_len=1000),
                         repr(result["exception"].data),
+                        "".join(traceback.format_tb(result["traceback"])),
                     )
                     self.transition(ts, "error")
 
