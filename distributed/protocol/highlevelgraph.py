852233b71fe6bbbcf8f2c8f2df373a23204059d2
Matthew Rocklin <mrocklin@gmail.com>
2020-12-10 11:40:23 -0800

    Use annotations machinery for workers/priority/...
    Previously we had two systems to send per-task metadata like retries or
    workers or priorities to the scheduler.
    
    1.  Older system with explicit workers= keywords and expand_foo functions
    2.  Newer system with annotations
    
    The annotations system is nicer for a few reasons:
    
    1.  It's more generic
    2.  It's more consistent (there were some bugs in the expand foo
        functions, especially when dealing with collections)
    3.  We ship values up on a per-layer basis rather than a per-key basis
    
    This work-in-progress commit rips out the old system and uses the new system,
    but it still missing a lot:
    
    1.  It only implements this for the Client.compute method.
        We need to repeat this for persist, submit, and map
    2.  It doesn't handle the allow_other_workers -> loose_restrictions
        conversion anywhere yet.  (this will need to be added to the
        scheduler)
    
    rebase fail

--- distributed/protocol/highlevelgraph.py
+++ distributed/protocol/highlevelgraph.py
@@ -135,7 +135,7 @@ def _materialized_layer_unpack(state, dsk, dependencies, annotations):
         )
 
 
-def highlevelgraph_unpack(dumped_hlg):
+def highlevelgraph_unpack(dumped_hlg, annotations: dict):
     """Unpack the high level graph for Scheduler -> Worker communication
 
     The approach is to delegate the packaging to each layer in the high
@@ -148,6 +148,11 @@ def highlevelgraph_unpack(dumped_hlg):
     dumped_hlg: list of header and payload
         Packed high level graph serialized by dumps_msgpack
 
+    annotations: dict
+        A top-level annotations object which may be partially populated,
+        and which may be further filled by annotations from the layers
+        of the dumped_hlg.
+
     Returns
     -------
     dsk: dict
@@ -157,18 +162,21 @@ def highlevelgraph_unpack(dumped_hlg):
     annotations: dict
         Annotations for `dsk`
     """
-
     hlg = loads_msgpack(*dumped_hlg)
 
     dsk = {}
     deps = {}
-    annotations = {}
+    out_annotations = {}
     for layer in hlg["layers"]:
+        if annotations:
+            if layer["state"]["annotations"] is None:
+                layer["state"]["annotations"] = {}
+            layer["state"]["annotations"].update(annotations)
         if layer["__module__"] is None:  # Default implementation
             unpack_func = _materialized_layer_unpack
         else:
             mod = import_allowed_module(layer["__module__"])
             unpack_func = getattr(mod, layer["__name__"]).__dask_distributed_unpack__
-        unpack_func(layer["state"], dsk, deps, annotations)
+        unpack_func(layer["state"], dsk, deps, out_annotations)
 
-    return dsk, deps, annotations
+    return dsk, deps, out_annotations
