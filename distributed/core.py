01e862a050597a8ce572a531a4dad2f96755f2bb
Ajmal Aboobacker <43377443+B3EF@users.noreply.github.com>
2021-01-10 16:08:49 +0530

    Update core.py

--- distributed/core.py
+++ distributed/core.py
@@ -10,6 +10,15 @@ import traceback
 import uuid
 import weakref
 import warnings
+import io
+import builtins
+safe_builtins = {
+    'range',
+    'complex',
+    'set',
+    'frozenset',
+    'slice',
+}
 
 import dask
 import tblib
@@ -40,6 +49,19 @@ from .utils import (
 )
 from . import protocol
 
+class RestrictedUnpickler(pickle.Unpickler):
+
+    def find_class(self, module, name):
+        """Only allow safe classes from builtins"""
+        if module == "builtins" and name in safe_builtins:
+            return getattr(builtins, name)
+        """Forbid everything else"""
+        raise pickle.UnpicklingError("global '%s.%s' is forbidden" %
+                                     (module, name))
+
+def restricted_loads(s):
+    """Helper function analogous to pickle.loads()"""
+    return RestrictedUnpickler(io.BytesIO(s)).load()
 
 class Status(Enum):
     """
