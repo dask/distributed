63c60d0811356e9b6c620d801959da241cbeb145
John Kirkham <jakirkham@gmail.com>
2021-01-28 16:52:53 -0800

    Annotate `keys` & `new`

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -5925,8 +5925,9 @@ class Scheduler(SchedulerState, ServerNode):
         reach a steady state
         """
         parent: SchedulerState = cast(SchedulerState, self)
-        keys = set()
+        keys: set = set()
         recommendations = recommendations.copy()
+        new: dict
         while recommendations:
             key, finish = recommendations.popitem()
             keys.add(key)
