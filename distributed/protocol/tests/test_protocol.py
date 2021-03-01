b221c6e103cd1febbfa50937d8e9f1ab7acff8ca
Mads R. B. Kristensen <madsbk@gmail.com>
2021-03-01 13:53:08 +0100

    to_serialize tests using bytearrays

--- distributed/protocol/tests/test_protocol.py
+++ distributed/protocol/tests/test_protocol.py
@@ -22,13 +22,13 @@ def test_compression_1():
     assert {"x": x.tobytes()} == y
 
 
-@pytest.mark.xfail(reason="TODO: handle bytes in dumps()")
 def test_compression_2():
     pytest.importorskip("lz4")
     np = pytest.importorskip("numpy")
     x = np.random.random(10000)
-    header, payload = dumps(x.tobytes())
-    assert not header or not msgpack.loads(header, encoding="utf8").get("compression")
+    msg = dumps(to_serialize(x.tobytes()))
+    compression = msgpack.loads(msg[1]).get("compression")
+    assert all(c is None for c in compression)
 
 
 def test_compression_without_deserialization():
@@ -86,16 +86,14 @@ def test_maybe_compress_sample():
     assert compressed == payload
 
 
-@pytest.mark.xfail(reason="TODO: handle bytes in dumps()")
 def test_large_bytes():
     for tp in (bytes, bytearray):
-        msg = {"x": tp(b"0" * 1000000), "y": 1}
+        msg = {"x": to_serialize(tp(b"0" * 1000000)), "y": 1}
         frames = dumps(msg)
+        msg["x"] = msg["x"].data
         assert loads(frames) == msg
         assert len(frames[0]) < 1000
         assert len(frames[1]) < 1000
-
-        assert loads(frames, deserialize=False) == msg
 
 
 @pytest.mark.slow
