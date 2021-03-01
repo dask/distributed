2c5d2cf814f13b0efc0fb21acc890476158468da
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2021-03-01 10:59:55 +0000

    Expose system monitor dashboard as individual plot for lab extension (#4540)

--- distributed/dashboard/scheduler.py
+++ distributed/dashboard/scheduler.py
@@ -34,6 +34,7 @@ from .components.scheduler import (
     individual_memory_by_key_doc,
     individual_compute_time_per_key_doc,
     individual_aggregate_time_per_action_doc,
+    individual_systemmonitor_doc,
 )
 from .worker import counters_doc
 from .components.nvml import gpu_memory_doc, gpu_utilization_doc  # noqa: 1708
@@ -90,4 +91,5 @@ applications = {
     "/individual-aggregate-time-per-action": individual_aggregate_time_per_action_doc,
     "/individual-gpu-memory": gpu_memory_doc,
     "/individual-gpu-utilization": gpu_utilization_doc,
+    "/individual-scheduler-system": individual_systemmonitor_doc,
 }
