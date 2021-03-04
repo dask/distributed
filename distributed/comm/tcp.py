074bc4809480a88dfdf4d05e33169fc9c19a69d1
Mads R. B. Kristensen <madsbk@gmail.com>
2021-03-04 03:37:28 +0100

    tcp.write(): cast memoryview to 1byte itemsize (#4555)

--- distributed/comm/tcp.py
+++ distributed/comm/tcp.py
@@ -261,6 +261,12 @@ class TCP(Comm):
                 if each_frame_nbytes:
                     if stream._write_buffer is None:
                         raise StreamClosedError()
+
+                    if isinstance(each_frame, memoryview):
+                        # Make sure that len(data) == data.nbytes`
+                        # See <https://github.com/tornadoweb/tornado/pull/2996>
+                        each_frame = memoryview(each_frame).cast("B")
+
                     stream._write_buffer.append(each_frame)
                     stream._total_write_index += each_frame_nbytes
 
