63aa4b773395151d28ce8fbbb38208f11c271fb1
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-03-15 12:44:43 -0700

    Use run_on_scheduler to get monitor count

--- distributed/client.py
+++ distributed/client.py
@@ -4614,7 +4614,9 @@ class performance_report:
 
     async def __aenter__(self):
         self.start = time()
-        self.last = get_client().cluster.scheduler.monitor.count
+        self.last_count = await get_client().run_on_scheduler(
+            lambda dask_scheduler: dask_scheduler.monitor.count
+        )
         await get_client().get_task_stream(start=0, stop=0)  # ensure plugin
 
     async def __aexit__(self, typ, value, traceback, code=None):
@@ -4625,7 +4627,7 @@ class performance_report:
             except Exception:
                 code = ""
         data = await get_client().scheduler.performance_report(
-            start=self.start, last=self.last, code=code
+            start=self.start, last_count=self.last_count, code=code
         )
         with open(self.filename, "w") as f:
             f.write(data)
