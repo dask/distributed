4202900edf7fea49d921648a7a067f526aabfe08
Sultan Orazbayev <contact@econpoint.com>
2021-03-17 00:44:51 +0600

    Update distributed/tests/test_client.py
    Co-authored-by: James Bourbeau <jrbourbeau@users.noreply.github.com>

--- distributed/tests/test_client.py
+++ distributed/tests/test_client.py
@@ -6525,7 +6525,7 @@ async def test_custom_key_with_batches(c, s, a, b):
         lambda x: x ** 2,
         list(range(10)),
         batch_size=5,
-        key=[f"{x}" for x in list(range(10))],
+        key=[f"{x}" for x in range(10)],
     )
     assert len(futs) == 10
     await wait(futs)
