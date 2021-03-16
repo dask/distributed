8ba0457485e944600ebdda1ad5623c75ea28fad8
Oksana Shadura <ksu.shadura@gmail.com>
2021-03-16 16:09:25 +0100

    Allow scheduler to preserve worker hostnames
    Patch by bbockelm.
    This provides a configuration option allowing the scheduler to
    preserve worker hostnames (as opposed to the current & default
    behavior of resolving hostnames immediately to IP address).
    Preserving the hostname should convey advantages in two situations:
    1. Dual-network setups where some hosts have IPv4-only or IPv6-only.
       This allows the host that wants to communicate with the worker
       to determine the best network to route over (as opposed to the
       scheduler).
    2. Clusters that route TLS connections based on SNI.  If the hostname
       is kept internally, it will be pased to OpenSSL when making a
       connection.  This allows the OpenSSL client to embed the desired
       hostname in a SNI header in the TLS handshake.  If the worker is
       behind a TLS-router (not entirely uncommon in a Kubernetes setup),
       then this SNI is required to successfully route the request.

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -164,6 +164,8 @@ DEFAULT_DATA_SIZE = declare(
     Py_ssize_t, parse_bytes(dask.config.get("distributed.scheduler.default-data-size"))
 )
 
+RESOLVE_WORKER_HOSTNAME = dask.config.get("distributed.scheduler.resolve-worker-hostname")
+
 DEFAULT_EXTENSIONS = [
     LockExtension,
     MultiLockExtension,
@@ -3833,7 +3835,7 @@ class Scheduler(SchedulerState, ServerNode):
         comm=None,
         *,
         address,
-        resolve_address: bool = True,
+        resolve_address=RESOLVE_WORKER_HOSTNAME,
         now: float = None,
         resources: dict = None,
         host_info: dict = None,
@@ -3936,7 +3938,7 @@ class Scheduler(SchedulerState, ServerNode):
         keys=(),
         nthreads=None,
         name=None,
-        resolve_address=True,
+        resolve_address=RESOLVE_WORKER_HOSTNAME,
         nbytes=None,
         types=None,
         now=None,
@@ -6723,7 +6725,7 @@ class Scheduler(SchedulerState, ServerNode):
                 parent._resources[resource] = dr = dict()
             del dr[worker]
 
-    def coerce_address(self, addr, resolve=True):
+    def coerce_address(self, addr, resolve=RESOLVE_WORKER_HOSTNAME):
         """
         Coerce possible input addresses to canonical form.
         *resolve* can be disabled for testing with fake hostnames.
