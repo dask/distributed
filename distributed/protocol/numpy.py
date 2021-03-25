3b8b97e31ed10598531fb458b53db16683c92c47
James Bourbeau <jrbourbeau@users.noreply.github.com>
2021-03-25 01:52:43 -0500

    Include LIST_PICKLE in NumPy array serialization (#4632)

--- distributed/protocol/numpy.py
+++ distributed/protocol/numpy.py
@@ -20,7 +20,7 @@ def itemsize(dt):
 
 @dask_serialize.register(np.ndarray)
 def serialize_numpy_ndarray(x, context=None):
-    if x.dtype.hasobject:
+    if x.dtype.hasobject or (x.dtype.flags & np.core.multiarray.LIST_PICKLE):
         header = {"pickle": True}
         frames = [None]
         buffer_callback = lambda f: frames.append(memoryview(f))
