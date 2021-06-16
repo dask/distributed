0a227a080628d94f210974718cb09470bf752af4
DPeterK <peter.killick@informaticslab.co.uk>
2021-06-16 14:33:09 +0100

    Different approach for getting scheduler/worker addresses

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -20,7 +20,13 @@ from .client import (
     wait,
 )
 from .core import Status, connect, rpc
-from .deploy import Adaptive, LocalCluster, SpecCluster, SSHCluster
+from .deploy import (
+    Adaptive,
+    LocalCluster,
+    LocalEnvCluster,
+    SpecCluster,
+    SSHCluster,
+)
 from .diagnostics.plugin import (
     Environ,
     NannyPlugin,
