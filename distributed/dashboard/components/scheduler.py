a0796f4872266965820aeb86cd043a1dd747532c
ncclementi <natyclementi@gmail.com>
2021-06-30 17:24:12 -0400

    add tabs for more than 50 workers

--- distributed/dashboard/components/scheduler.py
+++ distributed/dashboard/components/scheduler.py
@@ -2807,38 +2807,51 @@ def status_doc(scheduler, extra, doc):
 
         if len(scheduler.workers) < 50:
             nbytes_workers = NBytes(scheduler, sizing_mode="stretch_both")
-            processing = CurrentLoad(scheduler, sizing_mode="stretch_both")
+            current_load = CurrentLoad(scheduler, sizing_mode="stretch_both")
             occupancy = Occupancy(scheduler, sizing_mode="stretch_both")
-            processing_root = processing.processing_figure
-            cpu_root = processing.cpu_figure
+
+            processing_root = current_load.processing_figure
+            cpu_root = current_load.cpu_figure
             occupancy_root = occupancy.root
+
             processing_root.y_range = nbytes_workers.root.y_range
             cpu_root.y_range = nbytes_workers.root.y_range
             occupancy_root.y_range = nbytes_workers.root.y_range
         else:
             nbytes_workers = NBytesHistogram(scheduler, sizing_mode="stretch_both")
-            processing = ProcessingHistogram(scheduler, sizing_mode="stretch_both")
-            processing_root = processing.root
-            row(nbytes_workers.root, processing.root, sizing_mode="stretch_both")
+            processing_hist = ProcessingHistogram(scheduler, sizing_mode="stretch_both")
+            current_load = CurrentLoad(scheduler, sizing_mode="stretch_both")
+            occupancy = Occupancy(scheduler, sizing_mode="stretch_both")
+
+            cpu_root = current_load.cpu_figure
+            processing_hist_root = processing_hist.root
+            occupancy_root = occupancy.root
+
+            row(nbytes_workers.root, processing_hist.root, sizing_mode="stretch_both")
 
         nbytes_workers.update()
-        processing.update()
+        current_load.update()
         occupancy.update()
+
         add_periodic_callback(doc, nbytes_workers, 100)
-        add_periodic_callback(doc, processing, 100)
+        add_periodic_callback(doc, current_load, 100)
         add_periodic_callback(doc, occupancy, 100)
 
         doc.add_root(nbytes_workers.root)
 
         if len(scheduler.workers) < 50:
             tab1 = Panel(child=processing_root, title="Processing")
-            tab2 = Panel(child=cpu_root, title="CPU")
-            tab3 = Panel(child=occupancy_root, title="Occupancy")
-            proc_tabs = Tabs(tabs=[tab1, tab2, tab3], name="processing_tabs")
-
-            doc.add_root(proc_tabs)
         else:
-            print("im here")
+            processing_hist.update()
+            add_periodic_callback(doc, processing_hist, 100)
+
+            tab1 = Panel(child=processing_hist_root, title="Processing")
+
+        tab2 = Panel(child=cpu_root, title="CPU")
+        tab3 = Panel(child=occupancy_root, title="Occupancy")
+
+        proc_tabs = Tabs(tabs=[tab1, tab2, tab3], name="processing_tabs")
+        doc.add_root(proc_tabs)
 
         task_stream = TaskStream(
             scheduler,
