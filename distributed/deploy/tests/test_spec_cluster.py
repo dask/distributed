62825c2a00ac424ce68327d1f2227c84767c8ab2
fjetter <fjetter@users.noreply.github.com>
2021-07-12 19:51:49 +0200

    MultiWorker brings its own await

--- distributed/deploy/tests/test_spec_cluster.py
+++ distributed/deploy/tests/test_spec_cluster.py
@@ -409,6 +409,9 @@ class MultiWorker(Worker, ProcessInterface):
 
     __repr__ = __str__
 
+    def __await__(self):
+        return self.start().__await__()
+
     async def start(self):
         await asyncio.gather(*self.workers)
 
