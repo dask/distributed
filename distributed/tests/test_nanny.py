62d3805f06c0b8ea98779cd04156bbb46f6c9279
Matthew Rocklin <mrocklin@gmail.com>
2021-07-22 08:29:47 -0500

    respect local environment over config

--- distributed/tests/test_nanny.py
+++ distributed/tests/test_nanny.py
@@ -373,14 +373,16 @@ async def test_environment_variable(c, s):
 @gen_cluster(
     nthreads=[],
     client=True,
-    config={"distributed.nanny.environ": {"A": 1, "B": 2}},
+    config={"distributed.nanny.environ": {"A": 1, "B": 2, "D": 4}},
 )
 async def test_environment_variable_config(c, s):
+    os.environ["D"] = "123"
     async with Nanny(s.address, env={"B": 3, "C": 4}) as n:
         results = await c.run(lambda: os.environ)
         assert results[n.worker_address]["A"] == "1"
         assert results[n.worker_address]["B"] == "3"
         assert results[n.worker_address]["C"] == "4"
+        assert results[n.worker_address]["D"] == "123"
 
 
 @gen_cluster(nthreads=[], client=True)
