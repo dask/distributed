b5469976903bb6b79a480712ef57ba7fefcd4571
Gabe Joseph <gjoseph92@gmail.com>
2021-07-22 19:41:45 -0800

    driveby: WIP color task graph by worker
    This is just a hack currently, but maybe it would actually be useful?

--- distributed/diagnostics/graph_layout.py
+++ distributed/diagnostics/graph_layout.py
@@ -27,6 +27,7 @@ class GraphLayout(SchedulerPlugin):
         self.new = []
         self.new_edges = []
         self.state_updates = []
+        self.worker_updates = []
         self.visible_updates = []
         self.visible_edge_updates = []
 
@@ -93,11 +94,18 @@ class GraphLayout(SchedulerPlugin):
                 self.new_edges.append(edge)
 
     def transition(self, key, start, finish, *args, **kwargs):
+        task = self.scheduler.tasks[key]
+        idx = self.index[key]
         if finish != "forgotten":
-            self.state_updates.append((self.index[key], finish))
+            self.state_updates.append((idx, finish))
+            ws = (
+                task.queued_on
+                or task.processing_on
+                or (next(iter(task.who_has)) if task.who_has else None)
+            )
+            self.worker_updates.append((idx, int(ws.name) if ws else None))
         else:
-            self.visible_updates.append((self.index[key], "False"))
-            task = self.scheduler.tasks[key]
+            self.visible_updates.append((idx, "False"))
             for dep in task.dependents:
                 edge = (key, dep.key)
                 self.visible_edge_updates.append((self.index_edge.pop(edge), "False"))
@@ -124,6 +132,7 @@ class GraphLayout(SchedulerPlugin):
         self.new_edges = []
         self.visible_updates = []
         self.state_updates = []
+        self.worker_updates = []
         self.visible_edge_updates = []
 
         self.index = {}
