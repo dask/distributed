bd9c22a8f4a126a707c0dc9b300cc8dc179dfd5d
Matthew Rocklin <mrocklin@gmail.com>
2021-08-01 12:52:35 -0500

    Decouple serialization, frame splitting, and compression in protocol
    Currently compression and frame splitting are tightly interwoven with
    traversing through messages.  This can be efficient, but results in a
    complex system where it's hard to reason about when things get split or
    compressed (indeed, this lead to a difficult to track down bug with
    frame splitting).
    
    This commit separates these processes into three separate stages:
    
    1.  Serialize all objects into frames
    2.  Split large frames
    3.  Compress compressible frames
    
    This results in a much more uniform application of splitting and
    compressing.  However, this comes with a couple of undesired effects.
    
    1.  We add a new header if either splitting or compressing has occurred
    2.  We no longer avoid decompression when we don't want to deserialize
    
    There is probably a clean way to achieve most/all of our goals here.
    I wanted to push this up to start this conversation.

--- distributed/protocol/core.py
+++ distributed/protocol/core.py
@@ -1,16 +1,12 @@
+import functools
 import logging
 
 import msgpack
 
-from .compression import decompress, maybe_compress
-from .serialize import (
-    Serialize,
-    Serialized,
-    merge_and_deserialize,
-    msgpack_decode_default,
-    msgpack_encode_default,
-    serialize_and_split,
-)
+from .compression import compressions, maybe_compress
+from .serialize import Serialize, Serialized
+from .serialize import deserialize as _deserialize
+from .serialize import msgpack_decode_default, msgpack_encode_default, serialize
 from .utils import msgpack_opts
 
 logger = logging.getLogger(__name__)
@@ -54,14 +50,12 @@ def dumps(
                 if typ is Serialized:
                     sub_header, sub_frames = obj.header, obj.frames
                 else:
-                    sub_header, sub_frames = serialize_and_split(
+                    sub_header, sub_frames = serialize(
                         obj,
                         serializers=serializers,
                         on_error=on_error,
                         context=context,
-                        size=frame_split_size,
                     )
-                    _inplace_compress_frames(sub_header, sub_frames)
                 sub_header["num-sub-frames"] = len(sub_frames)
                 frames.append(
                     msgpack.dumps(
@@ -75,14 +69,44 @@ def dumps(
 
         frames[0] = msgpack.dumps(msg, default=_encode_default, use_bin_type=True)
 
-        if len(frames[0]) > frame_split_size:
-            from distributed.protocol.utils import frame_split_size as split
+        # Split large frames
+        frames2 = []
+        lengths = []
+        compressions = []
+        from distributed.protocol.utils import frame_split_size as split
 
-            msg_frames = split(frames[0], n=frame_split_size)
-            header = msgpack.dumps({"large-header": True, "count": len(msg_frames)})
-            frames = [header] + msg_frames + frames[1:]
+        for frame in frames:
+            if frame_split_size and len(frame) > frame_split_size:
+                sub_frames = split(frame, n=frame_split_size)
+                frames2.extend(sub_frames)
+                lengths.append(len(sub_frames))
+            else:
+                frames2.append(frame)
+                lengths.append(1)
 
-        return frames
+        frames = frames2
+
+        # Compress frames
+        if context and "compression" in context:
+            compress_opts = {"compression": context["compression"]}
+        else:
+            compress_opts = {}
+
+        for i, frame in enumerate(frames):
+            compression, compressed = maybe_compress(frame, **compress_opts)
+            frames[i] = compressed  # work in-place to avoid memory buildup
+            compressions.append(compression)
+
+        if any(length > 1 for length in lengths) or any(compressions):
+            header = {
+                "split-and-compressed": True,
+                "lengths": lengths,
+                "compressions": compressions,
+            }
+            return [msgpack.dumps(header)] + frames
+
+        else:
+            return frames
 
     except Exception:
         logger.critical("Failed to Serialize", exc_info=True)
@@ -94,7 +118,7 @@ def loads(frames, deserialize=True, deserializers=None):
 
     try:
 
-        def _decode_default(obj):
+        def _decode_default(obj, frames=[]):
             offset = obj.get("__Serialized__", 0)
             if offset > 0:
                 sub_header = msgpack.loads(
@@ -106,9 +130,7 @@ def loads(frames, deserialize=True, deserializers=None):
                 offset += 1
                 sub_frames = frames[offset : offset + sub_header["num-sub-frames"]]
                 if deserialize:
-                    if "compression" in sub_header:
-                        sub_frames = decompress(sub_header, sub_frames)
-                    return merge_and_deserialize(
+                    return _deserialize(
                         sub_header, sub_frames, deserializers=deserializers
                     )
                 else:
@@ -117,14 +139,48 @@ def loads(frames, deserialize=True, deserializers=None):
                 return msgpack_decode_default(obj)
 
         result = msgpack.loads(
-            frames[0], object_hook=_decode_default, use_list=False, **msgpack_opts
+            frames[0],
+            object_hook=functools.partial(_decode_default, frames=frames),
+            use_list=False,
+            **msgpack_opts,
         )
-        if isinstance(result, dict) and "large-header" in result:
-            frame = b"".join(frames[1 : result["count"] + 1])
-            frames = [frame] + frames[result["count"] + 1 :]
-            return loads(frames, deserialize=deserialize, deserializers=deserializers)
-        else:
-            return result
+
+        # Complex case, need to merge and decompress frames
+        if isinstance(result, dict) and "split-and-compressed" in result:
+            frames = frames[1:]
+
+            # Decompress all compressed frames
+            for i, (frame, compression) in enumerate(
+                zip(frames, result["compressions"])
+            ):
+                if compression:
+                    decompress = compressions[compression]["decompress"]
+                    decompressed = decompress(frame)
+                    frames[i] = decompressed
+
+            # Join all split frames
+            frames2 = []
+            i = 0
+            for length in result["lengths"]:
+                if length == 1:
+                    frames2.append(frames[i])
+                    frames[i] = None  # keep memory footprint low
+                else:
+                    frames2.append(bytearray().join(frames[i : i + length]))
+                    for j in range(i, i + length):
+                        frames[j] = None  # keep memory footprint low
+                i += length
+
+            frames = frames2
+
+            result = msgpack.loads(
+                frames[0],
+                object_hook=functools.partial(_decode_default, frames=frames),
+                use_list=False,
+                **msgpack_opts,
+            )
+
+        return result
 
     except Exception:
         logger.critical("Failed to deserialize", exc_info=True)
--- distributed/protocol/tests/test_numpy.py
+++ distributed/protocol/tests/test_numpy.py
@@ -220,13 +220,13 @@ def test_compress_numpy():
     frames = dumps({"x": to_serialize(x)})
     assert sum(map(nbytes, frames)) < x.nbytes
 
-    header = msgpack.loads(frames[1], raw=False, use_list=False, strict_map_key=False)
+    header = msgpack.loads(frames[0], raw=False, use_list=False, strict_map_key=False)
     try:
         import blosc  # noqa: F401
     except ImportError:
         pass
     else:
-        assert all(c == "blosc" for c in header["compression"])
+        assert any(c == "blosc" for c in header["compressions"])
 
 
 def test_compress_memoryview():
