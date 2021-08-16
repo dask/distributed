f8901d514c93f7a4003ce6e7a5dd3776ac50523c
Florian Jetter <fjetter@users.noreply.github.com>
2021-08-16 16:33:34 +0200

    Allow comm as argument again

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -7531,7 +7531,7 @@ class Scheduler(SchedulerState, ServerNode):
     def unsubscribe_topic(self, topic, client):
         self.event_subscriber[topic].discard(client)
 
-    def get_events(self, comm, topic):
+    def get_events(self, comm=None, topic=None):
         if topic is not None:
             return tuple(self.events[topic])
         else:
