3300d4a37a47106a37527134fba452169da748ef
Michael Adkins <michael@prefect.io>
2021-09-23 10:51:08 -0500

    Add tests

--- distributed/tests/test_utils.py
+++ distributed/tests/test_utils.py
@@ -1,5 +1,6 @@
 import array
 import asyncio
+import functools
 import io
 import os
 import queue
@@ -604,3 +605,12 @@ def test_typename_deprecated():
 def test_iscoroutinefunction_unhashable_input():
     # Ensure iscoroutinefunction can handle unhashable callables
     assert not iscoroutinefunction(_UnhashableCallable())
+
+
+def test_iscoroutinefunction_nested_partial():
+    async def my_async_callable(x, y, z):
+        pass
+
+    assert iscoroutinefunction(
+        functools.partial(functools.partial(my_async_callable, 1), 2)
+    )
