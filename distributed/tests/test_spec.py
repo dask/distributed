88dab1bc9bc85f75f4e4711d8f55d3b4f10a994b
Michael Adkins <michael@prefect.io>
2021-10-27 19:22:29 -0500

    Add rough testing for `close_clusters` behavior

--- distributed/tests/test_spec.py
+++ distributed/tests/test_spec.py
@@ -1,4 +1,12 @@
-from distributed.deploy.spec import ProcessInterface
+import uuid
+from unittest.mock import MagicMock
+
+from distributed.deploy.spec import (
+    ProcessInterface,
+    SpecCluster,
+    Status,
+    close_clusters,
+)
 
 
 def test_address_default_none():
@@ -16,3 +24,25 @@ def test_child_address_persists():
     assert c.address is None
     c = Child("localhost")
     assert c.address == "localhost"
+
+
+def test_close_clusters(monkeypatch):
+    def mock_cluster(status: Status, shutdown_on_close: bool = True):
+        mock = MagicMock(spec=SpecCluster)
+        mock.status = status
+        mock.shutdown_on_close = shutdown_on_close
+        return mock
+
+    closed_cluster = mock_cluster(Status.closed)
+    skipped_cluster = mock_cluster(Status.running, shutdown_on_close=False)
+    running_cluster = mock_cluster(Status.running)
+
+    monkeypatch.setattr(SpecCluster, "_instances", {running_cluster, closed_cluster})
+
+    close_clusters()
+
+    closed_cluster.sync.assert_not_called()
+    skipped_cluster.sync.assert_not_called()
+    running_cluster.sync.assert_called_once_with(
+        running_cluster.close, asynchronous=False, callback_timeout=10
+    )
