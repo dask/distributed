d27070a7f9432bda36a13dc98dfa4ae4bcf2aea0
ncclementi <natyclementi@gmail.com>
2021-11-29 15:27:25 -0500

    adapt test_spill to use new SpillBuffer

--- distributed/tests/test_spill.py
+++ distributed/tests/test_spill.py
@@ -2,6 +2,7 @@ import pytest
 
 from dask.sizeof import sizeof
 
+from distributed.protocol import serialize_bytelist
 from distributed.spill import SpillBuffer
 
 
@@ -11,72 +12,78 @@ def test_spillbuffer(tmpdir):
     assert buf.memory is buf.fast
     assert buf.disk is buf.slow
 
-    assert not buf.spilled_by_key
-    assert buf.spilled_total == 0
+    assert not buf.slow.weight_by_key
+    assert buf.slow.total_weight == 0
 
     a, b, c, d = "a" * 100, "b" * 100, "c" * 100, "d" * 100
-    s = sizeof(a)
+    s = sizeof(a)  # this is size of a in memory
+
+    pickle = serialize_bytelist(a)
+    pickle_size = sum(len(frame) for frame in pickle)
+
     # Test assumption made by this test, mostly for non CPython implementations
     assert 100 < s < 200
 
     buf["a"] = a
     assert not buf.disk
-    assert not buf.spilled_by_key
-    assert buf.spilled_total == 0
+    assert not buf.slow.weight_by_key
+    assert buf.slow.total_weight == 0
     assert buf["a"] == a
 
     buf["b"] = b
     assert not buf.disk
-    assert not buf.spilled_by_key
-    assert buf.spilled_total == 0
+    assert not buf.slow.weight_by_key
+    assert buf.slow.total_weight == 0
 
     buf["c"] = c
     assert set(buf.disk) == {"a"}
-    assert buf.spilled_by_key == {"a": s}
-    assert buf.spilled_total == s
+    assert buf.slow.weight_by_key == {"a": pickle_size}
+    assert buf.slow.total_weight == pickle_size
 
     assert buf["a"] == a
     assert set(buf.disk) == {"b"}
-    assert buf.spilled_by_key == {"b": s}
-    assert buf.spilled_total == s
+    assert buf.slow.weight_by_key == {"b": pickle_size}
+    assert buf.slow.total_weight == pickle_size
 
     buf["d"] = d
     assert set(buf.disk) == {"b", "c"}
-    assert buf.spilled_by_key == {"b": s, "c": s}
-    assert buf.spilled_total == s * 2
+    assert buf.slow.weight_by_key == {"b": pickle_size, "c": pickle_size}
+    assert buf.slow.total_weight == pickle_size * 2
 
     # Deleting an in-memory key does not automatically move spilled keys back to memory
     del buf["a"]
     assert set(buf.disk) == {"b", "c"}
-    assert buf.spilled_by_key == {"b": s, "c": s}
-    assert buf.spilled_total == s * 2
+    assert buf.slow.weight_by_key == {"b": pickle_size, "c": pickle_size}
+    assert buf.slow.total_weight == pickle_size * 2
     with pytest.raises(KeyError):
         buf["a"]
 
     # Deleting a spilled key updates the metadata
     del buf["b"]
     assert set(buf.disk) == {"c"}
-    assert buf.spilled_by_key == {"c": s}
-    assert buf.spilled_total == s
+    assert buf.slow.weight_by_key == {"c": pickle_size}
+    assert buf.slow.total_weight == pickle_size
     with pytest.raises(KeyError):
         buf["b"]
 
     # Updating a spilled key moves it to the top of the LRU and to memory
     buf["c"] = c * 2
     assert set(buf.disk) == {"d"}
-    assert buf.spilled_by_key == {"d": s}
-    assert buf.spilled_total == s
+    assert buf.slow.weight_by_key == {"d": pickle_size}
+    assert buf.slow.total_weight == pickle_size
 
     # Single key is larger than target and goes directly into slow
     e = "e" * 500
-    slarge = sizeof(e)
+    pickle_large = serialize_bytelist(e)
+    pickle_large_size = sum(len(frame) for frame in pickle_large)
+
     buf["e"] = e
     assert set(buf.disk) == {"d", "e"}
-    assert buf.spilled_by_key == {"d": s, "e": slarge}
-    assert buf.spilled_total == s + slarge
+    assert buf.slow.weight_by_key == {"d": pickle_size, "e": pickle_large_size}
+    assert buf.slow.total_weight == pickle_size + pickle_large_size
 
     # Updating a spilled key with another larger than target updates slow directly
     buf["d"] = "d" * 500
     assert set(buf.disk) == {"d", "e"}
-    assert buf.spilled_by_key == {"d": slarge, "e": slarge}
-    assert buf.spilled_total == slarge * 2
+    assert buf.slow.weight_by_key == {"d": pickle_large_size, "e": pickle_large_size}
+    assert buf.slow.total_weight == pickle_large_size * 2
