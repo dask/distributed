93b3f7be516bfebdf29a11b7cddb0cc3f63514f8
ncclementi <natyclementi@gmail.com>
2021-11-29 16:53:21 -0500

    add tests for spill buffer with max_spill limit

--- distributed/tests/test_spill.py
+++ distributed/tests/test_spill.py
@@ -18,8 +18,7 @@ def test_spillbuffer(tmpdir):
     a, b, c, d = "a" * 100, "b" * 100, "c" * 100, "d" * 100
     s = sizeof(a)  # this is size of a in memory
 
-    pickle = serialize_bytelist(a)
-    pickle_size = sum(len(frame) for frame in pickle)
+    pickle_size = sum(len(frame) for frame in serialize_bytelist(a))
 
     # Test assumption made by this test, mostly for non CPython implementations
     assert 100 < s < 200
@@ -74,8 +73,7 @@ def test_spillbuffer(tmpdir):
 
     # Single key is larger than target and goes directly into slow
     e = "e" * 500
-    pickle_large = serialize_bytelist(e)
-    pickle_large_size = sum(len(frame) for frame in pickle_large)
+    pickle_large_size = sum(len(frame) for frame in serialize_bytelist(e))
 
     buf["e"] = e
     assert set(buf.disk) == {"d", "e"}
@@ -87,3 +85,59 @@ def test_spillbuffer(tmpdir):
     assert set(buf.disk) == {"d", "e"}
     assert buf.slow.weight_by_key == {"d": pickle_large_size, "e": pickle_large_size}
     assert buf.slow.total_weight == pickle_large_size * 2
+
+
+def test_spillbuffer_maxlim(tmpdir):
+    buf = SpillBuffer(str(tmpdir), target=200, max_spill=600)
+    # Convenience aliases
+    assert buf.memory is buf.fast
+    assert buf.disk is buf.slow
+
+    assert not buf.slow.weight_by_key
+    assert buf.slow.total_weight == 0
+
+    a, b, c, d = "a" * 200, "b" * 100, "c" * 100, "d" * 200
+
+    psize_a = sum(len(frame) for frame in serialize_bytelist(a))  # this is size in disk
+
+    # size of a is bigger than target and is smaller than max_spill, key should be in slow
+    buf["a"] = a
+    assert not buf.memory
+    assert not buf.fast.weights
+    assert set(buf.disk) == {"a"}
+    assert buf.slow.weight_by_key == {"a": psize_a}
+    assert buf.slow.total_weight == psize_a
+    assert buf["a"] == a
+
+    # size of b is smaller than target key should be in fast
+    sb = sizeof(b)
+    buf["b"] = b
+    assert set(buf.memory) == {"b"}
+    assert buf.fast.weights == {"b": sb}
+    assert buf["b"] == b
+    assert buf.fast.total_weight == sb
+
+    # size of c is smaller than target but b+c > target, c should stay in fast and b move to slow
+    # since the max_spill limit has not been reached yet
+    sc = sizeof(c)
+    psize_b = sum(len(frame) for frame in serialize_bytelist(b))
+
+    buf["c"] = c
+    assert set(buf.memory) == {"c"}
+    assert buf.fast.weights == {"c": sc}
+    assert buf["c"] == c
+    assert buf.fast.total_weight == sc
+
+    assert set(buf.disk) == {"a", "b"}
+    assert buf.slow.weight_by_key == {"a": psize_a, "b": psize_b}
+    assert buf.slow.total_weight == psize_a + psize_b
+
+    # size of d > target, d should go to slow but slow reached the max_spill limit then d
+    # will end up on fast with c (which can't be move to slow because it won't fit either)
+    sd = sizeof(d)
+
+    buf["d"] = d
+    assert set(buf.memory) == {"c", "d"}
+    assert buf.fast.weights == {"c": sc, "d": sd}
+    assert buf["d"] == d
+    assert buf.fast.total_weight == sc + sd
