68aa83fcf11333a49fa791e4c983f90bd4ea3697
ncclementi <natyclementi@gmail.com>
2021-12-03 16:50:32 -0500

    fix extra line of code

--- distributed/tests/test_worker.py
+++ distributed/tests/test_worker.py
@@ -7,7 +7,6 @@ import threading
 import traceback
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from concurrent.futures.process import BrokenProcessPool
-from distutils.version import LooseVersion
 from numbers import Number
 from operator import add
 from time import sleep
@@ -929,14 +928,9 @@ async def test_dataframe_attribute_error(c, s, a, b):
 
 @gen_cluster(client=True)
 async def test_fail_write_to_disk(c, s, a, b):
-    import zict
-
-    # class MyError(Exception):
-    #     pass
-
     class Bad:
         def __getstate__(self):
-            raise TypeError()  # MyError() tried to use a custom error but didn't work kept raising TypeError (it comes from serialization)
+            raise TypeError()
 
         def __sizeof__(self):
             return int(100e9)
@@ -950,14 +944,8 @@ async def test_fail_write_to_disk(c, s, a, b):
         await future
 
     futures = c.map(inc, range(10))
-
-    if LooseVersion(zict.__version__) <= "2.0.0":
-        results = await c._gather(futures)
-        assert results == list(map(inc, range(10)))
-    else:
-        with pytest.raises(TypeError):
-            # in zict > 2.0.0 when exception is raised keys are kept, meaning that it won't be able to serialize
-            results = await c._gather(futures)
+    results = await c._gather(futures)
+    assert results == list(map(inc, range(10)))
 
 
 @pytest.mark.skip(reason="Our logic here is faulty")
