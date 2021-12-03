5c7fcea8c80237ef254a0bb9f4ff854b5bd9e9ec
ncclementi <natyclementi@gmail.com>
2021-12-03 17:01:17 -0500

    fix version check on spill.py

--- distributed/spill.py
+++ distributed/spill.py
@@ -27,9 +27,9 @@ class SpillBuffer(zict.Buffer):
         self, spill_directory: str, target: int, max_spill: int | Literal[False] = False
     ):
         if (
-            max_spill is not False and LooseVersion(zict.__version__) <= "2.0"
+            max_spill is not False and LooseVersion(zict.__version__) <= "2.0.0"
         ):  # FIX ME WHEN zict is released us LooseVersion(zict.__version__) <= "2.0.0"
-            # is not False allows spill limit 0, decide if this case is ok?
+            # is not False allows spill limit 0, decide if this case is ok
             raise ValueError("zict > 2.0.0 required to set max_weight")
 
         super().__init__(
