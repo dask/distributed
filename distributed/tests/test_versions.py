daedacc2cb03634b249bf9a3b0ca666d984ca834
Jacob Tomlinson <jtomlinson@nvidia.com>
2021-12-09 15:16:57 +0000

    Change error wording

--- distributed/tests/test_versions.py
+++ distributed/tests/test_versions.py
@@ -76,7 +76,7 @@ def test_version_mismatch(node, effect, kwargs_not_matching, pattern):
     column_matching = {"client": 1, "scheduler": 2, "workers": 3}
     msg = error_message(**kwargs_not_matching)
     i = column_matching.get(node, 3)
-    assert "Mismatched versions found" in msg["warning"]
+    assert "Mismatched package versions found" in msg["warning"]
     assert "distributed" in msg["warning"]
     assert "Critical" in msg["warning"]
     assert (
@@ -108,7 +108,7 @@ def test_scheduler_additional_irrelevant_package(kwargs_matching):
 def test_python_mismatch(kwargs_matching):
     kwargs_matching["client"]["packages"]["python"] = "0.0.0"
     msg = error_message(**kwargs_matching)
-    assert "Mismatched versions found" in msg["warning"]
+    assert "Mismatched package versions found" in msg["warning"]
     assert "python" in msg["warning"]
     assert "Critical" in msg["warning"]
     assert (
