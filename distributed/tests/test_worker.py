3b1a889ae1d1dfeaf509c35f0e68632b2138e750
crusaderky <crusaderky@gmail.com>
2022-02-20 11:24:06 +0000

    xfail

--- distributed/tests/test_worker.py
+++ distributed/tests/test_worker.py
@@ -35,7 +35,7 @@ from distributed import (
     wait,
 )
 from distributed.comm.registry import backends
-from distributed.compatibility import LINUX, WINDOWS
+from distributed.compatibility import LINUX, MACOS, WINDOWS
 from distributed.core import CommClosedError, Status, rpc
 from distributed.diagnostics import nvml
 from distributed.diagnostics.plugin import PipInstall
@@ -1377,7 +1377,14 @@ async def test_spill_no_target_threshold(c, s, a):
     # whole data to disk (memory_limit * target = 0)
     start = time()
     while time() < start + 0.5:
-        assert a.data.memory
+        if not a.data.memory:
+            # The hysteresis system fails on MacOSX because process memory is very slow
+            # to shrink down after calls to PyFree. As a result, Worker.memory_monitor
+            # will continue spilling until there's nothing left. Nothing we can do about
+            # this short of finding either a way to change this behaviour at OS level or
+            # a better measure of allocated memory.
+            assert MACOS
+            raise pytest.xfail("Process memory is slow to shrink down on MacOSX")
         await asyncio.sleep(0)
 
 
