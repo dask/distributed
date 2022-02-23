14008b88f8ae24221e98dde2ff84b4255e294d18
John Kirkham <jakirkham@gmail.com>
2022-02-22 18:50:11 -0800

    Wrap line

--- distributed/comm/utils.py
+++ distributed/comm/utils.py
@@ -22,7 +22,8 @@ if isinstance(OFFLOAD_THRESHOLD, str):
 try:
     # Use NumPy, when available, to avoid memory initialization cost.
     # A `bytearray` is zero-initialized using `calloc`, which we don't need.
-    # `np.empty` both skips the zero-initialization, and uses hugepages when available (https://github.com/numpy/numpy/pull/14216).
+    # `np.empty` both skips the zero-initialization, and
+    # uses hugepages when available (https://github.com/numpy/numpy/pull/14216).
     import numpy
 
     host_array = lambda n: memoryview(numpy.empty((n,), dtype="u1"))  # type: ignore
