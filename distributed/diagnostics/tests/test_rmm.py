baa450ee38c905cf0816f825698d49e7ef32e986
Ashwin Srinath <shwina@users.noreply.github.com>
2022-03-07 09:21:45 -0500

    Add to test

--- distributed/diagnostics/tests/test_rmm.py
+++ distributed/diagnostics/tests/test_rmm.py
@@ -3,6 +3,7 @@ import asyncio
 import pytest
 
 from dask import delayed
+from dask.utils import parse_bytes
 from distributed.utils_test  import gen_cluster
 
 
@@ -16,13 +17,18 @@ pynvml = pytest.importorskip("pynvml")
     client=True,
     nthreads=[("127.0.0.1", 1)],
     Worker=dask_cuda.CUDAWorker,
-    worker_kwargs={"rmm_track_allocations": True}
+    worker_kwargs={
+        "rmm_pool_size": parse_bytes("10MiB"),
+        "rmm_track_allocations": True
+    }
 )
 async def test_rmm_metrics(c, s, *workers):
     w = list(s.workers.values())[0]
     assert "rmm" in w.metrics
     assert w.metrics["rmm"]["rmm-used"] == 0
+    assert w.metrics["rmm"]["rmm-total"] == parse_bytes("10MiB")
     result = delayed(rmm.DeviceBuffer)(size=10)
     result = result.persist()
     await asyncio.sleep(1)
     assert w.metrics["rmm"]["rmm-used"] != 0
+    assert w.metrics["rmm"]["rmm-total"] == parse_bytes("10MiB")
