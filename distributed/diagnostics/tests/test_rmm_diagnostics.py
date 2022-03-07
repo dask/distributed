6d44aacc2c041c76f3ecf69f6b5933f94839bc5e
Ashwin Srinath <shwina@users.noreply.github.com>
2022-03-07 09:59:48 -0500

    Rename

--- distributed/diagnostics/tests/test_rmm_diagnostics.py
+++ distributed/diagnostics/tests/test_rmm_diagnostics.py
@@ -4,8 +4,8 @@ import pytest
 
 from dask import delayed
 from dask.utils import parse_bytes
-from distributed.utils_test  import gen_cluster
 
+from distributed.utils_test import gen_cluster
 
 pytestmark = pytest.mark.gpu
 
@@ -13,14 +13,15 @@ dask_cuda = pytest.importorskip("dask_cuda")
 rmm = pytest.importorskip("rmm")
 pynvml = pytest.importorskip("pynvml")
 
+
 @gen_cluster(
     client=True,
     nthreads=[("127.0.0.1", 1)],
     Worker=dask_cuda.CUDAWorker,
     worker_kwargs={
         "rmm_pool_size": parse_bytes("10MiB"),
-        "rmm_track_allocations": True
-    }
+        "rmm_track_allocations": True,
+    },
 )
 async def test_rmm_metrics(c, s, *workers):
     w = list(s.workers.values())[0]
