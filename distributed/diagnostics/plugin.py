872edb6bf59f56531e44ac493f016032ccf425d1
Carlos Rendon <carlos@coiled.io>
2022-03-11 16:13:29 -0600

    this example now runs as intended

--- distributed/diagnostics/plugin.py
+++ distributed/diagnostics/plugin.py
@@ -127,7 +127,8 @@ class WorkerPlugin:
     ...                 exc_info=exc_info
     ...             )
 
-    >>> plugin = ErrorLogger()
+    >>> import logger
+    >>> plugin = ErrorLogger(logger)
     >>> client.register_worker_plugin(plugin)  # doctest: +SKIP
     """
 
