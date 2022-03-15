b6eb26a3d155f434b716438fe82a633374f86bce
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2022-03-15 15:44:45 +0100

    Add option to specify a scheduler address for workers to use
    scheduler_options:contact_address can be used to specify a
    different address on which workers should contact the scheduler.
    This can be useful when the scheduler binds to a private address
    (e.g. inside a container) that can't be used by the workers to
    contact it.
    
    Co-authored-by: Oksana Shadura <oksana.shadura@cern.ch>

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -3641,6 +3641,12 @@ class Scheduler(SchedulerState, ServerNode):
     Users typically do not interact with the scheduler directly but rather with
     the client object ``Client``.
 
+    The ``contact_address`` parameter allows to advertise a specific address to
+    the workers for communication with the scheduler, which is different than
+    the address the scheduler binds to. This is useful when the scheduler
+    listens on a private address, which therefore cannot be used by the workers
+    to contact it.
+
     **State**
 
     The scheduler contains the following state variables.  Each variable is
@@ -3705,11 +3711,14 @@ class Scheduler(SchedulerState, ServerNode):
         preload=None,
         preload_argv=(),
         plugins=(),
+        contact_address=None,
         **kwargs,
     ):
         self._setup_logging(logger)
 
         # Attributes
+        if contact_address:
+            self.contact_address = contact_address
         if allowed_failures is None:
             allowed_failures = dask.config.get("distributed.scheduler.allowed-failures")
         self.allowed_failures = allowed_failures
