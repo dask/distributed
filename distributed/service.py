6743ff265280481f6bb146a6b87a9d4cc5d42bad
Gabe Joseph <gjoseph92@gmail.com>
2022-03-16 00:32:36 -0600

    docs, and numerous other changes

--- distributed/service.py
+++ distributed/service.py
@@ -1,8 +1,134 @@
 from __future__ import annotations
 
+r"""
+Tasks that are actually stateful services running on all workers, for doing out-of-band
+operations in a controlled way.
+
+*Services* provide a structured way to combine task graphs of pure functions (Dask's main
+use case) with stateful, distributed subsystems. Some algorithms (DataFrame shuffling,
+array rechunking, etc.) are inefficient when represented as task graphs, but can be
+implemented simply and performantly as bespoke systems that handle their own low-level
+data transfer and computation manually.
+
+The `Service` interface offers a structured way to "hand off" data from the task-graph
+paradigm to these out-of-band operations, and to "hand back" the results so downstream
+tasks can use them, while maintaining resilience of the overall graph.
+
+In a graph like::
+
+    {
+        ("input", 1): 1,
+        ("input", 2): 2,
+        "foo-service": (FooService, [("input", 1), ("input", 2)]),  # annotated as a service
+        ("output", 1): (service_result, "foo-service"),             # never actually runs
+        ("output", 2): (service_result, "foo-service"),             # never actually runs
+        ("output", 3): (service_result, "foo-service"),             # never actually runs
+        ("downstream", 1): (use, ("output", 1)),
+        ("downstream", 2): (use, ("output", 2)),
+        ("downstream", 3): (use, ("output", 3)),
+    }
+
+Or visually::
+
+    d1   d2   d3
+    |    |    |
+    o1   o2   o3
+      \  |   /
+    foo-service
+        / \
+      i1  i2
+
+If the single ``foo-service`` task is annotated as a service, it'll actually be computed
+by launching ``FooService`` instances on every worker, passing the inputs into them,
+letting them do whatever out-of-band operations they wish (including communicating with
+each other or external systems), and waiting for them to produce the output keys. Once
+each output key is produced (the `service_result` functions never actually run), any
+tasks downstream of it can run.
+
+---------------------------------------------------------------------------------------
+
+Dask handles startup/shutdown, resilience, peer discovery, leader election, RPC
+interfaces, and composition for Services:
+
+Startup/shutdown
+----------------
+
+* Once the *first* input key to a service task is ready (state ``memory``), service
+  instances are started on all workers in the cluster.
+* Once the last output key is produced by a service, all instances are told to stop.
+* Any instance can error the service task; all other instances will be told to stop.
+* Any instance can restart the service task; all other instances will be told to stop,
+  then re-launched from scratch.
+* If the service task is cancelled (because keys downstream from it are released, etc.),
+  all instances are told to stop.
+
+Resilience
+----------
+
+* If a worker that's running a service leaves, the entire service task is restarted on
+  all workers. TODO change once `peer_joined`/`peer_left` are added.
+* If keys downstream of a service need to be recomputed, the service will be rerun, just
+  like regular tasks are.
+* The service is informed at runtime which inputs to expect and which outputs to
+  produce, so partial recomputation (some outputs already in memory, some not) can be
+  handled correctly.
+
+Peer discovery and RPC
+----------------------
+
+* At startup, services are given a list of their peers as RPC handles.
+* The RPC handle allows a `Service` to call arbitrary methods on its peers on other
+  workers.
+* Loopback calls (using the RPC to call yourself) take a fastpath, bypassing
+  serialization and the network stack.
+
+Leader election
+---------------
+* At startup, one instance is designated as the "leader", making it easier to coordinate
+  initialization tasks that need to run exactly once.
+
+Composition
+-----------
+
+One Service's outputs can be another Service's inputs.
+
+A `Service` can depend on multiple input Services, or both normal tasks and Services, or
+have no dependencies at all. Note, though, that input keys are always treated as a flat
+structure (`Service.add_key` is called, one key at a time, from whatever worker already
+holds the key). Therefore, if particular inputs need to be co-located, the `Service` is
+responsible for transferring them around internally. (Doing just that in clever ways is
+often the whole point of a `Service`.)
+
+                 o   o   o
+                  \  |  /
+              combiner-service
+          /   /        \   \   \
+         o   o          o   o   o
+          \ /            \  |  /
+     bar-service        baz-service
+     /   |   \          /  /  \  \
+    o    o    o        i  i    i  i
+      \  |   /          \  \  /  /
+     foo-service       normal-tasks
+
+Restrictions
+~~~~~~~~~~~~
+
+If a task depends on a `Service`, that must be its only dependency. For example, this
+will raise an error when the graph is submitted to the scheduler:
+
+     o     o   <-- tasks that depend on a Service cannot have >1 dependency
+     | \ / |
+     | / \ |
+    s-1   s-2
+     /\    /\
+    i  i  i  i
+
+A `Future` cannot refer to a `Service` task; that is, `TaskState.who_wants` must be
+empty if `TaskState.service` is True. In other words, you can't compute a `Service`
+directly, only its output keys.
 """
-Stateful services that run on all workers, for doing out-of-band operations in a controlled way.
-"""
+
 from typing import (
     Any,
     Awaitable,
@@ -10,19 +136,40 @@ from typing import (
     Generic,
     Mapping,
     NewType,
+    NoReturn,
     Protocol,
     Sequence,
     TypeVar,
 )
 
+from typing_extensions import ParamSpec
+
+
+def service_result(service: Service) -> NoReturn:
+    """
+    Placeholder function used in output tasks for a `Service`.
+
+    The scheduler will not actually run tasks that depend on a `Service`;
+    they are just added to the graph to create dependency structure.
+    Instead, Services use `Concierge.produce_key` to mark their output keys
+    as complete. Then, normal tasks that depend on those output keys can run.
+    """
+    raise RuntimeError(
+        f"`service_result` should not actually be called on {service!r}."
+        "Are you sure you're running on a distributed cluster?"
+    )
+
+
 ServiceTask = NewType("ServiceTask", str)
-# ^ TODO need a better term. This refers to a single key in the graph, which creates Service instances
-# across many workers to accomplish its task. `ServiceInvocation`? `ServiceTask`? `ServiceTask`?
-# There can be multiple types of Services (`ShuffleService`, `RechunkService`, `XGBoostService`),
-# and each one can be invoked multiple times (`'shuffle-1234'`, `'shuffle-5678'` `'rechunk-9876'`).
-# Each invocation is what we're calling the `ServiceID`. We need a term for both the invocation in general,
-# and for the group of instances (across many workers) handling that invocation.
+"""
+The name of a single key in the graph that's computed using a `Service`.
+
+The term "ServiceTask" is used to refer to overall computation of this key,
+performed by many `Service` instances running on different workers.
+"""
 Address = NewType("Address", str)
+T = TypeVar("T", bound="Service")
+# TODO: use pep-673 Self type https://peps.python.org/pep-0673/
 
 
 class Service:
@@ -56,7 +203,7 @@ class Service:
         ----------
         key:
             The key in the graph that created the `Service`. All instances created from
-            that key (across all workers) receive the same ServiceTask ``key``.
+            that key (across all workers) receive the same ``key``.
 
             Multiple instances of the same type of `Service` can exist on a worker at
             once, but all are guaranteed to have a different ``key``.
@@ -65,12 +212,15 @@ class Service:
             simultaneously being started, and `ServiceHandles` to communicate with them.
 
             Note that peer services are not guaranteed to be running yet.
+
+            The order of ``peers`` is undefined, but it will be the same for every
+            `Service` in ``peers``.
         input_keys:
             The input keys this ServiceTask will receive, *across all workers*, in
             priority order.
         output_keys:
-            The output keys this ServiceTask is expected to produce, *across all
-            workers*, in priority order.
+            The output keys this ServiceTask must produce, *across all workers*, in
+            priority order.
         concierge:
             A `Concierge` instance the `Service` can use to communicate with the
             `Worker`. Used to hand off output keys back to Dask, restart or fail the
@@ -86,23 +236,25 @@ class Service:
             for Services, to make it easier to coordinate initialization tasks that must
             run exactly once.
         **kwargs:
-            Forward-compatibility only—any additional arguments can be ignored.
+            Forward-compatibility only; any additional arguments can be ignored.
         """
         ...
 
-    # Not going to implement `all_started`; it's way too brittle.
-    # What if some peers die before they start?
-    # Then those addresses will _never_ start. Probably better to let Services
-    # that need this sort of coordination handle it themselves via a Semaphore.
+    # TODO: remove this in favor of `peer_joined` and `peer_left`.
+    # Once those methods are supported, and workers joining/leaving doesn't automatically
+    # restart the ServiceTask, we can't support `all_started` anymore: what if one of the
+    # original peers dies before it's started?
+    # `peer_joined` and `peer_left` are more powerful, because they allow for Service resizing,
+    # but still allow services to restart when workers leave (and wait for all to arrive) if
+    # they wish.
+    async def all_started(self) -> None:
+        """
+        Called once all instances in the original ``peers`` list have started.
 
-    # async def all_started(self) -> None:
-    #     """
-    #     Called once all instances in the original ``peers`` list have started.
-
-    #     Do not make the ``start`` method block until ``all_started`` has been called;
-    #     this will cause a deadlock.
-    #     """
-    #     ...
+        Do not make the ``start`` method block until ``all_started`` has been called;
+        this will cause a deadlock.
+        """
+        ...
 
     async def add_key(self, key: str, data: Any) -> None:
         """
@@ -114,6 +266,11 @@ class Service:
 
         ``add_key`` can be called as soon as ``start`` has returned. It will only be
         called with keys in ``input_keys``.
+
+        For a given input key, ``add_key`` will be called exactly once *across all workers*.
+        ``add_key`` is always called from a worker that already holds ``key`` (data is
+        not transferred just for ``add_key``). If multiple workers hold replicas of the key,
+        the worker on which ``add_key`` is called is undefined.
         """
         ...
 
@@ -148,7 +305,7 @@ class Service:
         Cases in which `stop` is called:
         * Success: all ``output_keys`` have been produced.
         * Failure: `Concierge.error` was called, or `Service.add_key` raised an error.
-        * Restart: `Concierge.restart` was called
+        * Restart: `Concierge.restart` was called.
         * Restart: a peer worker left (TODO remove this case!).
 
         The `Service` is not informed what the cause for stopping is.
@@ -157,18 +314,34 @@ class Service:
         before `stop` returns.
 
         After `stop` returns, a new `Service` instance with the same ServiceTask
-        may be created on the `Worker`.
+        key may be created on the `Worker`.
+        """
+        ...
+
+    def __sizeof__(self) -> int:
+        """
+        Amount of memory this `Service` is using for internal state.
+
+        This will be counted as managed memory by dask.
+        """
+        ...
+
+    def spilled_bytes(self) -> int:
+        """
+        Number of bytes on disk this `Service` is using for internal state.
+
+        This will be counted as spilled memory by dask.
         """
         ...
 
 
-T = TypeVar("T", bound=Service)
-# TODO: use pep-673 Self type https://peps.python.org/pep-0673/
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
 class Concierge:
     "Interface for `Service` instances to update the progress of a ServiceTask"
-    id: ServiceTask
+    key: ServiceTask
 
     async def produce_key(self, key: str, data: Any) -> None:
         """
@@ -205,14 +378,32 @@ class Concierge:
 
         `Service.stop` is guaranteed not to be called until after `error` has returned.
 
-        Once `restart` has returned, `Service.stop` will be called on all instances for
+        Once `error` has returned, `Service.stop` will be called on all instances for
         the ServiceTask, and workers will release their references those instances. Once
         `stop` has returned on all instances, the task will transition to ``erred`` on
         the scheduler. The result of the task will be ``exc``.
 
-        If multiple instances call `error`, only the first ``exc`` to reach the
+        If `error` is called multiple times, only the first ``exc`` to reach the
         scheduler will become the result; all others will be dropped (though all calls
         to `error` are logged on the `Worker`).
+        """
+        ...
+
+    async def run_in_executor(
+        self, func: Callable[P, R], *args: P.args, **kwargs: P.kwargs
+    ) -> Awaitable[R]:
+        """
+        Schedule a function to run in the Worker's default executor.
+
+        This will take up a slot in `Worker.executing_count`, reducing the number of
+        other tasks (or `run_in_executor` calls) that can run concurrently. This allows
+        multiple Services (or Services and normal tasks) to cooperatively run
+        resource-intensive synchronous functions concurrently without oversubscribing
+        the `Worker`. This way, `Worker.nthreads` is respected even for out-of-band
+        functions.
+
+        For event-loop-blocking functions that aren't resource-intensive (network calls,
+        etc.), `distributed.compatibility.to_thread` is preferred.
         """
         ...
 
@@ -221,11 +412,11 @@ class Concierge:
 class ServiceHandle(Generic[T]):
     "RPC to a Service instance running on another worker"
 
-    def __getattr__(self, attr: str) -> RPC:
+    def __getattr__(self, attr: str) -> _RPC:
         ...
 
 
 # https://stackoverflow.com/a/65564936/17100540
-class RPC(Protocol):
+class _RPC(Protocol):
     def __call__(self, *args, **kwargs) -> Awaitable:
         ...
