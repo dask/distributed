126a70258e62b96e47d31cc493b92d9bceba3052
Simon Perkins <simon.perkins@gmail.com>
2022-03-30 18:25:46 +0200

    Remove context_vars check. Difficult to test ContextVars in isolation

--- distributed/tests/test_context_vars.py
+++
@@ -1,9 +0,0 @@
-from distributed.context_vars import STIMULUS_ID
-
-
-def a_very_underlined_function():
-    return STIMULUS_ID.get()
-
-
-def test_stimulus_id():
-    assert a_very_underlined_function().startswith("a-very-underlined-function")
