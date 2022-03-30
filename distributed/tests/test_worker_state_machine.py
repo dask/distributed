9a5b006a335159d79782345e39a3c099d6337722
crusaderky <crusaderky@gmail.com>
2022-03-30 20:49:59 +0100

    Cleanup

--- distributed/tests/test_worker_state_machine.py
+++ distributed/tests/test_worker_state_machine.py
@@ -109,9 +109,3 @@ def test_sendmsg_to_dict():
     # Arbitrary sample class
     smsg = ReleaseWorkerDataMsg(key="x")
     assert smsg.to_dict() == {"op": "release-worker-data", "key": "x"}
-
-
-@pytest.mark.parametrize("cls", StateMachineEvent.__subclasses__())
-def test_event_slots(cls):
-    smsg = cls(**dict.fromkeys(cls.__annotations__), stimulus_id="test")
-    assert not hasattr(smsg, "__dict__")
