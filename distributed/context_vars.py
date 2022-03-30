fb7eba85f44892ec671d45032fb5a81ec7ba8954
Simon Perkins <simon.perkins@gmail.com>
2022-03-30 18:24:46 +0200

    STIMULUS_ID improvements

--- distributed/context_vars.py
+++ distributed/context_vars.py
@@ -6,9 +6,11 @@ from dask.utils import funcname
 
 from distributed.metrics import time
 
+STIMULUS_ID_CTXVAR: ContextVar = ContextVar("stimulus_id")
+
 
 class STIMULUS_ID:
-    STIMULUS_ID_CTXVAR: ContextVar = ContextVar("stimulus_id")
+    """Mediates access to STIMULUS_ID_CTXVAR"""
 
     @classmethod
     def from_function(cls, func: Callable[..., Any]):
@@ -39,16 +41,19 @@ class STIMULUS_ID:
         return f"{name}-{time()}"
 
     @classmethod
-    def get(cls, on_error: Union[str, bool] = "generate"):
+    def get(cls, on_error: Union[str, bool] = "generate", default: Any = None):
         """Gets the stimulus_id from the current context.
 
         Parameters
         ----------
-        on_error: {"generate", "raise", True, False}
+        on_error: {"generate", "raise", "default", True, False}
             If the context does not have a stimulus_id, this will be used to
             generate one. If the value is "generate" or False, a new stimulus_id
             will be generated. If the value is "raise" or True, the
             original LookupError will be re-reraised.
+            If `on_error` is "default", `default` will be returned.
+        default : optional
+            The default value to return if the context does not have a stimulus_id.
 
         Returns
         -------
@@ -56,31 +61,33 @@ class STIMULUS_ID:
             The stimulus_id from the current context.
         """
         try:
-            return cls.STIMULUS_ID_CTXVAR.get()
+            return STIMULUS_ID_CTXVAR.get()
         except LookupError:
             if on_error is False or on_error == "generate":
                 stimulus_id = cls.from_callstack(depth=2)
-                cls.STIMULUS_ID_CTXVAR.set(stimulus_id)
+                STIMULUS_ID_CTXVAR.set(stimulus_id)
                 return stimulus_id
             elif on_error is True or on_error == "raise":
                 raise
+            elif on_error == "default":
+                return default
             else:
                 raise ValueError(
-                    f"'{on_error}' not in {'raise', 'generate', True, False}"
+                    f"'{on_error}' not in {'raise', 'generate', 'default', True, False}"
                 )
 
     @classmethod
     def set(cls, value: str):
-        return cls.STIMULUS_ID_CTXVAR.set(value)
+        return STIMULUS_ID_CTXVAR.set(value)
 
     def setdefault(cls, value: str, return_token=False) -> Union[str, Tuple[str, Any]]:
         token: Any
 
         try:
-            stimulus_id = cls.STIMULUS_ID_CTXVAR.get()
+            stimulus_id = STIMULUS_ID_CTXVAR.get()
         except LookupError:
             stimulus_id = value
-            token = cls.STIMULUS_ID_CTXVAR.set(value)
+            token = STIMULUS_ID_CTXVAR.set(value)
         else:
             token = Token.MISSING
 
@@ -91,4 +98,4 @@ class STIMULUS_ID:
 
     @classmethod
     def reset(cls, token: Token):
-        return cls.STIMULUS_ID_CTXVAR.reset(token)
+        return STIMULUS_ID_CTXVAR.reset(token)
