7cf7901daf1c138e225798714d95cd53f6cba4b6
Matthew Rocklin <mrocklin@gmail.com>
2022-04-14 15:37:04 -0500

    Skip windows segfault test

--- distributed/tests/test_chaos.py
+++ distributed/tests/test_chaos.py
@@ -4,10 +4,17 @@ import pytest
 
 from distributed import Nanny
 from distributed.chaos import KillWorker
-from distributed.utils_test import gen_cluster
+from distributed.utils_test import WINDOWS, gen_cluster
 
 
-@pytest.mark.parametrize("mode", ["sys.exit", "graceful", "segfault"])
+@pytest.mark.parametrize(
+    "mode",
+    [
+        "sys.exit",
+        "graceful",
+        pytest.param("segfault", marks=pytest.mark.skipif(WINDOWS, reason="unknown")),
+    ],
+)
 @gen_cluster(nthreads=[("127.0.0.1", 1)], client=True, Worker=Nanny)
 async def test_KillWorker(c, s, w, mode):
     plugin = KillWorker(delay="1ms", mode=mode)
