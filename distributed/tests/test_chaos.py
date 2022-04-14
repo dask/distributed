b1534352498ea84ff446e4bbe3e499732bb9dcdb
Matthew Rocklin <mrocklin@gmail.com>
2022-04-14 14:45:54 -0500

    avoid thread leak in test

--- distributed/tests/test_chaos.py
+++ distributed/tests/test_chaos.py
@@ -14,5 +14,12 @@ async def test_KillWorker(c, s, w, mode):
 
     await c.register_worker_plugin(plugin)
 
+    # Otherwise we sometimes stop the test just when we're starting a new
+    # nanny, and leak a thread
+    def f(dask_worker):
+        dask_worker.auto_restart = False
+
+    await c.run(f, nanny=True)
+
     while s.workers:
         await asyncio.sleep(0.001)
