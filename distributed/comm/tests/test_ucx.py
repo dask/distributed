68238637286bf07ca13cfd961888861536324bb3
Benjamin Zaitlen <quasiben@gmail.com>
2022-04-27 12:09:48 -0400

    reduce data size in ucx tranpose test

--- distributed/comm/tests/test_ucx.py
+++ distributed/comm/tests/test_ucx.py
@@ -348,9 +348,9 @@ async def test_transpose():
     da = pytest.importorskip("dask.array")
 
     async with LocalCluster(protocol="ucx", asynchronous=True) as cluster:
-        async with Client(cluster, asynchronous=True):
+        async with Client(cluster, asynchronous=True) as client:
             assert cluster.scheduler_address.startswith("ucx://")
-            x = da.ones((10000, 10000), chunks=(1000, 1000)).persist()
+            x = da.ones((1000, 1000), chunks=(200, 200)).persist()
             await x
             y = (x + x.T).sum()
             await y
