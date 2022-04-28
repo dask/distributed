a1399a111a08a3bb8202d2890f928d7b76e7ba99
Matthew Rocklin <mrocklin@gmail.com>
2022-04-28 15:12:45 -0500

    Revert "Remove test_reconnect"
    This reverts commit b59ccd03c2e78d536cb7d818af968156bd8b610d.

--- distributed/cli/tests/test_dask_worker.py
+++ distributed/cli/tests/test_dask_worker.py
@@ -13,7 +13,7 @@ from dask.utils import tmpfile
 
 from distributed import Client
 from distributed.cli.dask_worker import _apportion_ports, main
-from distributed.compatibility import LINUX
+from distributed.compatibility import LINUX, to_thread
 from distributed.deploy.utils import nprocesses_nthreads
 from distributed.metrics import time
 from distributed.utils_test import gen_cluster, popen, requires_ipv6
@@ -271,6 +271,58 @@ async def test_memory_limit(c, s):
 async def test_no_nanny(c, s):
     with popen(["dask-worker", s.address, "--no-nanny", "--no-dashboard"]):
         await c.wait_for_workers(1)
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("nanny", ["--nanny", "--no-nanny"])
+@gen_cluster(client=True, nthreads=[])
+async def test_no_reconnect(c, s, nanny):
+    with popen(
+        [
+            "dask-worker",
+            s.address,
+            "--no-reconnect",
+            nanny,
+            "--no-dashboard",
+        ]
+    ) as worker:
+        # roundtrip works
+        assert await c.submit(lambda x: x + 1, 10) == 11
+
+        (comm,) = s.stream_comms.values()
+        comm.abort()
+
+        # worker terminates as soon as the connection is aborted
+        await to_thread(worker.wait, timeout=5)
+        assert worker.returncode == 0
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("nanny", ["--nanny", "--no-nanny"])
+@gen_cluster(client=True, nthreads=[])
+async def test_reconnect(c, s, nanny):
+    with popen(
+        [
+            "dask-worker",
+            s.address,
+            "--reconnect",
+            nanny,
+            "--no-dashboard",
+        ]
+    ) as worker:
+        # roundtrip works
+        assert await c.submit(lambda x: x + 1, 10) == 11
+
+        (comm,) = s.stream_comms.values()
+        comm.abort()
+
+        # roundtrip still works, which means the worker reconnected
+        assert await c.submit(lambda x: x + 1, 11) == 12
+
+        # closing the scheduler cleanly does terminate the worker
+        await s.close()
+        await to_thread(worker.wait, timeout=5)
+        assert worker.returncode == 0
 
 
 @pytest.mark.slow
