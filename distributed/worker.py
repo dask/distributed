122df572a97a41111dea80eec13e0763d35e0ad1
Gabe Joseph <gjoseph92@gmail.com>
2022-05-04 11:37:03 -0500

    Fix `fail_hard` for sync functions
    The `else` meant we would, in fact, not fail hard at all.

--- distributed/worker.py
+++ distributed/worker.py
@@ -195,8 +195,7 @@ def fail_hard(method):
                         },
                     )
                     logger.exception(e)
-                else:
-                    self.loop.add_callback(_force_close, self)
+                self.loop.add_callback(_force_close, self)
 
     return wrapper
 
