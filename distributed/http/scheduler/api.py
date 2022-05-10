b27770ac7a2830e02774764723ef8159c7b19983
Matthew Murray <matthewmurray711@gmail.com>
2022-05-10 12:46:19 -0400

    Add adaptive_target and get_workers

--- distributed/http/scheduler/api.py
+++ distributed/http/scheduler/api.py
@@ -2,31 +2,49 @@ from __future__ import annotations
 
 import json
 
-from tornado import web
+from distributed.http.utils import RequestHandler
 
 
-class APIHandler(web.RequestHandler):
+class APIHandler(RequestHandler):
     def get(self):
         self.write("API V1")
         self.set_header("Content-Type", "text/plain")
 
 
-class RetireWorkersHandler(web.RequestHandler):
-    def initialize(self, dask_server):
-        self.dask_server = dask_server
-
+class RetireWorkersHandler(RequestHandler):
     async def post(self):
         self.set_header("Content-Type", "text/json")
+        scheduler = self.server
         try:
             params = json.loads(self.request.body)
-            workers_info = await self.dask_server.retire_workers(**params)
+            self.write(params)
+            workers_info = await scheduler.retire_workers(**params)
             self.write(json.dumps(workers_info))
         except Exception as e:
             self.set_status(400, str(e))
             self.write(json.dumps({"Error": "Bad request"}))
 
+class GetWorkersHandler(RequestHandler):
+    def get(self):
+        scheduler = self.server
+        response = {
+            "num_workers": len(scheduler.workers),
+            "workers": [{"name": ws.name, "address": ws.address} for ws in scheduler.workers.values()]
+        }
+        self.write(response)
+
+class AdaptiveTargetHandler(RequestHandler):
+    def get(self):
+        scheduler = self.server
+        desired_workers = scheduler.adaptive_target()
+        response = {
+            "desired_workers": desired_workers,
+        }
+        self.write(response)
 
 routes: list[tuple] = [
     ("/api/v1", APIHandler, {}),
     ("/api/v1/retire_workers", RetireWorkersHandler, {}),
+    ("/api/v1/get_workers", GetWorkersHandler, {}),
+    ("/api/v1/adaptive_target", AdaptiveTargetHandler, {}),
 ]
