97275b7ee35a026b9d433626bd48dfdf1cb8fbd2
Matthew Murray <matthewmurray711@gmail.com>
2022-05-16 12:03:23 -0400

    Address more comments

--- distributed/http/scheduler/api.py
+++ distributed/http/scheduler/api.py
@@ -17,55 +17,50 @@ class RetireWorkersHandler(RequestHandler):
         scheduler = self.server
         try:
             params = json.loads(self.request.body)
-            workers_info = await scheduler.retire_workers(**params)
+            workers = params["workers"]
+            workers_info = await scheduler.retire_workers(workers)
             self.write(json.dumps(workers_info))
         except Exception as e:
-            self.set_status(400, str(e))
-            self.write(json.dumps({"Error": "Bad request"}))
-
-
-class WorkersToCloseHandler(RequestHandler):
-    async def post(self):
-        self.set_header("Content-Type", "text/json")
-        scheduler = self.server
-        try:
-            params = json.loads(self.request.body)
-            workers_to_close = {"workers": scheduler.workers_to_close(**params)}
-            self.write(json.dumps(workers_to_close))
-        except Exception as e:
             self.set_status(500, str(e))
-            self.write(json.dumps({"Error": "Bad request"}))
+            self.write(json.dumps({"Error": "Internal Server Error"}))
 
 
 class GetWorkersHandler(RequestHandler):
     def get(self):
         self.set_header("Content-Type", "text/json")
         scheduler = self.server
-        response = {
-            "num_workers": len(scheduler.workers),
-            "workers": [
-                {"name": ws.name, "address": ws.address}
-                for ws in scheduler.workers.values()
-            ],
-        }
-        self.write(json.dumps(response))
+        try:
+            response = {
+                "num_workers": len(scheduler.workers),
+                "workers": [
+                    {"name": ws.name, "address": ws.address}
+                    for ws in scheduler.workers.values()
+                ],
+            }
+            self.write(json.dumps(response))
+        except Exception as e:
+            self.set_status(500, str(e))
+            self.write(json.dumps({"Error": "Internal Server Error"}))
 
 
 class AdaptiveTargetHandler(RequestHandler):
     def get(self):
         self.set_header("Content-Type", "text/json")
         scheduler = self.server
-        desired_workers = scheduler.adaptive_target()
-        response = {
-            "workers": desired_workers,
-        }
-        self.write(json.dumps(response))
+        try:
+            desired_workers = scheduler.adaptive_target()
+            response = {
+                "workers": desired_workers,
+            }
+            self.write(json.dumps(response))
+        except Exception as e:
+            self.set_status(500, str(e))
+            self.write(json.dumps({"Error": "Internal Server Error"}))
 
 
 routes: list[tuple] = [
     ("/api/v1", APIHandler, {}),
     ("/api/v1/retire_workers", RetireWorkersHandler, {}),
-    ("/api/v1/workers_to_close", WorkersToCloseHandler, {}),
     ("/api/v1/get_workers", GetWorkersHandler, {}),
     ("/api/v1/adaptive_target", AdaptiveTargetHandler, {}),
 ]
