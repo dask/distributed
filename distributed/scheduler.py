ae9a98f98f39569c124a906d3ffce99f1b55fc2a
Gabe Joseph <gjoseph92@gmail.com>
2022-05-23 10:03:28 -0600

    Don't prematurely remove closing workers

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -4732,7 +4732,7 @@ class Scheduler(SchedulerState, ServerNode):
         ws.long_running.add(ts)
         self.check_idle_saturated(ws)
 
-    async def handle_worker_status_change(
+    def handle_worker_status_change(
         self, status: str, worker: str, stimulus_id: str
     ) -> None:
         ws = self.workers.get(worker)
@@ -4760,12 +4760,8 @@ class Scheduler(SchedulerState, ServerNode):
                 worker_msgs: dict = {}
                 self._transitions(recs, client_msgs, worker_msgs, stimulus_id)
                 self.send_all(client_msgs, worker_msgs)
-        elif ws.status == Status.paused:
-            self.running.remove(ws)
-        elif ws.status == Status.closing:
-            await self.remove_worker(
-                address=ws.address, stimulus_id=stimulus_id, close=False
-            )
+        else:
+            self.running.discard(ws)
 
     async def handle_worker(self, comm=None, worker=None, stimulus_id=None):
         """
