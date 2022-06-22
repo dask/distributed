f52d3d172085a3948aec45e578b857798f52c3be
fjetter <fjetter@users.noreply.github.com>
2022-06-22 11:28:26 +0200

    Eliminate all occurences of hard-coded port 8786

--- distributed/cli/tests/test_dask_ssh.py
+++ distributed/cli/tests/test_dask_ssh.py
@@ -20,7 +20,7 @@ def test_version_option():
 
 
 @pytest.mark.slow
-def test_ssh_cli_nprocs_renamed_to_nworkers(loop):
+def test_ssh_cli_nprocs_renamed_to_nworkers(loop, requires_default_port):
     with popen(
         ["dask-ssh", "--nprocs=2", "--nohost", "localhost"],
         capture_output=True,
@@ -33,7 +33,7 @@ def test_ssh_cli_nprocs_renamed_to_nworkers(loop):
         wait_for_log_line(b"renamed to --nworkers", proc.stdout, max_lines=15)
 
 
-def test_ssh_cli_nworkers_with_nprocs_is_an_error():
+def test_ssh_cli_nworkers_with_nprocs_is_an_error(requires_default_port):
     with popen(
         ["dask-ssh", "localhost", "--nprocs=2", "--nworkers=2"],
         capture_output=True,
