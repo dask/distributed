54cd71d7a881b7efe8fb9fc8d56b535608db89cf
Florian Jetter <fjetter@users.noreply.github.com>
2022-06-27 15:46:12 +0200

    Remove unused __started Event in Server (#6615)

--- distributed/core.py
+++ distributed/core.py
@@ -427,7 +427,6 @@ class Server:
         self.io_loop.add_callback(set_thread_ident)
         self._startup_lock = asyncio.Lock()
         self.__startup_exc: Exception | None = None
-        self.__started = asyncio.Event()
 
         self.rpc = ConnectionPool(
             limit=connection_limit,
@@ -499,7 +498,6 @@ class Server:
                 await _close_on_failure(exc)
                 raise RuntimeError(f"{type(self).__name__} failed to start.") from exc
             self.status = Status.running
-            self.__started.set()
         return self
 
     async def __aenter__(self):
