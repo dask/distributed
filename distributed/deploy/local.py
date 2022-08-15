14288884483aac924eb93401c12b16e4a189ff67
Crislana Rafael <crislanarafael@gmail.com>
2022-08-15 09:20:59 -0700

    Update local.py
    Included additional data type options to memory_limit arg and additional notes.

--- distributed/deploy/local.py
+++ distributed/deploy/local.py
@@ -30,8 +30,11 @@ class LocalCluster(SpecCluster):
     ----------
     n_workers: int
         Number of workers to start
-    memory_limit: str
-        Sets the memory limit per worker. Cannot exceed the total memory available.
+    memory_limit: str, float, int, or None, default "auto"
+        Sets the memory limit *per worker*.
+        
+        Note that the limit will only be enforced when ``processes=True``, and the limit is only
+        enforced on a best-effort basis — it's still possible for workers to exceed this limit.
     processes: bool
         Whether to use processes (True) or threads (False).  Defaults to True, unless
         worker_class=Worker, in which case it defaults to False.
