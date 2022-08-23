54b72eadfa51db028e39e4a89bb99e35eabc56d5
Doug Davis <ddavis@ddavis.io>
2022-08-23 10:50:50 -0500

    flake8

--- distributed/cli/tests/test_dask_ssh.py
+++ distributed/cli/tests/test_dask_ssh.py
@@ -3,10 +3,8 @@ from __future__ import annotations
 import pytest
 from click.testing import CliRunner
 
-from distributed import Client
 from distributed.cli.dask_ssh import main
 from distributed.compatibility import MACOS, WINDOWS
-from distributed.utils_test import popen, wait_for_log_line
 
 pytest.importorskip("paramiko")
 pytestmark = [
