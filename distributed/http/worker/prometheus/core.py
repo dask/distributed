51f9781a8f513ec5b83b44ee6488823d24b3aa26
Hendrik Makait <hendrik@coiled.io>
2022-08-30 12:56:02 +0200

    Fix lint

--- distributed/http/worker/prometheus/core.py
+++ distributed/http/worker/prometheus/core.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+from typing import ClassVar
 
 from distributed.http.prometheus import PrometheusCollector
 from distributed.http.utils import RequestHandler
