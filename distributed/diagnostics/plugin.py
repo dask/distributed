7375bb69463fa9276f9425a7d90fd6240853ac1c
crusaderky <crusaderky@gmail.com>
2022-10-17 16:34:54 +0100

    lint

--- distributed/diagnostics/plugin.py
+++ distributed/diagnostics/plugin.py
@@ -389,7 +389,12 @@ class CondaInstall(PackageInstall):
 
     conda_options: list[str]
 
-    def __init__(self, packages: list[str], conda_options: list[str] | None = None, restart: bool = False):
+    def __init__(
+        self,
+        packages: list[str],
+        conda_options: list[str] | None = None,
+        restart: bool = False,
+    ):
         super().__init__(packages, restart=restart)
         self.conda_options = conda_options or []
 
