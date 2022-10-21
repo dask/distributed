43a9cbdf4791f0245e16664a261df2e2829974b6
Laurence Watts <laurencewatts@hotmail.co.uk>
2022-10-21 17:53:43 +0100

    Fix test indentation

--- distributed/tests/test_client.py
+++ distributed/tests/test_client.py
@@ -6602,31 +6602,32 @@ async def test_log_event(c, s, a):
 
 @gen_cluster(client=True, nthreads=[])
 async def test_log_event_multiple_clients(c, s):
-        async with Client(s.address, asynchronous=True) as c2, Client(
-            s.address, asynchronous=True
-        ) as c3:
-            received_events = []
+    async with Client(s.address, asynchronous=True) as c2, Client(
+        s.address, asynchronous=True
+    ) as c3:
+        received_events = []
 
-            def get_event_handler(handler_id):
-                def handler(event):
-                    received_events.append((handler_id, event))
+        def get_event_handler(handler_id):
+            def handler(event):
+                received_events.append((handler_id, event))
 
-                return handler
+            return handler
 
-            c.subscribe_topic("test-topic", get_event_handler(1))
-            c2.subscribe_topic("test-topic", get_event_handler(2))
+        c.subscribe_topic("test-topic", get_event_handler(1))
+        c2.subscribe_topic("test-topic", get_event_handler(2))
 
-            while len(s.event_subscriber["test-topic"]) != 2:
-                await asyncio.sleep(0.01)
+        while len(s.event_subscriber["test-topic"]) != 2:
+            await asyncio.sleep(0.01)
 
+        with captured_logger(logging.getLogger("distributed.client")) as logger:
             await c.log_event("test-topic", {})
 
-            while len(received_events) < 2:
-                await asyncio.sleep(0.01)
+        while len(received_events) < 2:
+            await asyncio.sleep(0.01)
 
-            assert len(received_events) == 2
-            assert {handler_id for handler_id, _ in received_events} == {1, 2}
-            assert "ValueError" not in logger.getvalue()
+        assert len(received_events) == 2
+        assert {handler_id for handler_id, _ in received_events} == {1, 2}
+        assert "ValueError" not in logger.getvalue()
 
 
 @gen_cluster(client=True)
