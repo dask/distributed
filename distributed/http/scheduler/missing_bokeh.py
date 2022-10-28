a5c60d9b9444473a1cfc497af0357d3c72806fcf
James Bourbeau <jrbourbeau@gmail.com>
2022-10-28 10:18:15 -0500

    Temporarily restrict bokeh<3

--- distributed/http/scheduler/missing_bokeh.py
+++ distributed/http/scheduler/missing_bokeh.py
@@ -9,9 +9,9 @@ class MissingBokeh(RequestHandler):
     @log_errors
     def get(self):
         self.write(
-            f"<p>Dask needs bokeh >= {_min_bokeh_version} for the dashboard.</p>"
-            f"<p>Install with conda: conda install bokeh>={_min_bokeh_version}</p>"
-            f"<p>Install with pip: pip install bokeh>={_min_bokeh_version}</p>"
+            f"<p>Dask needs bokeh >= {_min_bokeh_version}, < 3 for the dashboard.</p>"
+            f"<p>Install with conda: conda install bokeh>={_min_bokeh_version},<3</p>"
+            f"<p>Install with pip: pip install bokeh>={_min_bokeh_version},<3</p>"
         )
 
 
