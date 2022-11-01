4d12fe5a9d75e86261b46df90a0331447bc923db
Peter Andreas Entschev <peter@entschev.com>
2022-11-01 18:10:18 +0100

    Reset CUDA device context during UCX test cleanup fixture
    Co-authored-by: Lawrence Mitchell <wence@gmx.li>

--- distributed/utils_test.py
+++ distributed/utils_test.py
@@ -2153,6 +2153,15 @@ def ucx_loop():
     import distributed.comm.ucx
 
     distributed.comm.ucx.ucp = None
+    # If the test created a context, clean it up.
+    # TODO: should we check if there's already a context _before_ the test runs?
+    # I think that would be useful.
+    from distributed.diagnostics.nvml import has_cuda_context
+    ctx = has_cuda_context()
+    if ctx.has_context:
+        import numba.cuda
+        ctx = numba.cuda.current_context()
+        ctx.device.reset()
 
 
 def wait_for_log_line(
