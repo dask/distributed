3df50eab5c4a3e0b5321c5bbd45826d6ce219f2d
fjetter <fjetter@users.noreply.github.com>
2023-01-31 14:59:53 +0100

    Add hash_join based on P2P

--- distributed/shuffle/__init__.py
+++ distributed/shuffle/__init__.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 from distributed.shuffle._arrow import check_minimal_arrow_version
+from distributed.shuffle._merge import HashJoinP2PLayer, hash_join_p2p
 from distributed.shuffle._scheduler_extension import ShuffleSchedulerExtension
 from distributed.shuffle._shuffle import P2PShuffleLayer, rearrange_by_column_p2p
 from distributed.shuffle._worker_extension import ShuffleWorkerExtension
 
 __all__ = [
     "check_minimal_arrow_version",
+    "hash_join_p2p",
+    "HashJoinP2PLayer",
     "P2PShuffleLayer",
     "rearrange_by_column_p2p",
     "ShuffleSchedulerExtension",
