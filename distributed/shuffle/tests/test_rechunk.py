3382f728a7cabb8ef2ab32b18f610b1365c34fcb
Hendrik Makait <hendrik@coiled.io>
2023-02-17 15:35:14 +0100

    Even more tests

--- distributed/shuffle/tests/test_rechunk.py
+++ distributed/shuffle/tests/test_rechunk.py
@@ -2,13 +2,16 @@ from __future__ import annotations
 
 import asyncio
 import random
+import warnings
 
 import numpy as np
 import pytest
 
+import dask
 import dask.array as da
 from dask.array.core import concatenate3
 from dask.array.rechunk import normalize_chunks, rechunk
+from dask.array.utils import assert_eq
 
 from distributed.shuffle._limiter import ResourceLimiter
 from distributed.shuffle._scheduler_extension import get_worker_for_range_sharding
@@ -286,3 +289,306 @@ async def test_rechunk_0d(c, s, *ws):
 async def test_rechunk_empty_array(c, s, *ws, arr):
     arr.rechunk(rechunk="p2p")
     assert arr.size == 0
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_empty(c, s, *ws):
+    x = da.ones((0, 10), chunks=(5, 5))
+    y = x.rechunk((2, 2), rechunk="p2p")
+    assert y.chunks == ((0,), (2,) * 5)
+    assert_eq(x, y, scheduler=c)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_zero_dim_array(c, s, *ws):
+    x = da.zeros((4, 0), chunks=3)
+    y = x.rechunk({0: 4}, rechunk="p2p")
+    assert y.chunks == ((4,), (0,))
+    assert_eq(x, y, scheduler=c)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_zero_dim_array_II(c, s, *ws):
+    x = da.zeros((4, 0, 6, 10), chunks=3)
+    y = x.rechunk({0: 4, 2: 2}, rechunk="p2p")
+    assert y.chunks == ((4,), (0,), (2, 2, 2), (3, 3, 3, 1))
+    assert_eq(x, y, scheduler=c)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_same(c, s, *ws):
+    x = da.ones((24, 24), chunks=(4, 8))
+    y = x.rechunk(x.chunks, rechunk="p2p")
+    assert x is y
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_with_zero_placeholders(c, s, *ws):
+    x = da.ones((24, 24), chunks=((12, 12), (24, 0)))
+    y = da.ones((24, 24), chunks=((12, 12), (12, 12)))
+    y = y.rechunk(((12, 12), (24, 0)), rechunk="p2p")
+    assert x.chunks == y.chunks
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_minus_one(c, s, *ws):
+    x = da.ones((24, 24), chunks=(4, 8))
+    y = x.rechunk((-1, 8), rechunk="p2p")
+    assert y.chunks == ((24,), (8, 8, 8))
+    assert_eq(x, y, scheduler=c)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_warning(c, s, *ws):
+    N = 20
+    x = da.random.normal(size=(N, N, 100), chunks=(1, N, 100))
+    with warnings.catch_warnings(record=True) as w:
+        x = x.rechunk((N, 1, 100), rechunk="p2p")
+    assert not w
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_unknown_from_pandas(c, s, *ws):
+    dd = pytest.importorskip("dask.dataframe")
+    pd = pytest.importorskip("pandas")
+
+    arr = np.random.randn(50, 10)
+    x = dd.from_pandas(pd.DataFrame(arr), 2).values
+    result = x.rechunk((None, (5, 5)), rechunk="p2p")
+    assert np.isnan(x.chunks[0]).all()
+    assert np.isnan(result.chunks[0]).all()
+    assert result.chunks[1] == (5, 5)
+    expected = da.from_array(arr, chunks=((25, 25), (10,))).rechunk(
+        (None, (5, 5)), rechunk="p2p"
+    )
+    assert_eq(result, expected, scheduler=c)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_unknown_from_array(c, s, *ws):
+    dd = pytest.importorskip("dask.dataframe")
+    # pd = pytest.importorskip('pandas')
+    x = dd.from_array(da.ones(shape=(4, 4), chunks=(2, 2))).values
+    # result = x.rechunk({1: 5}, rechunk="p2p")
+    result = x.rechunk((None, 4), rechunk="p2p")
+    assert np.isnan(x.chunks[0]).all()
+    assert np.isnan(result.chunks[0]).all()
+    assert x.chunks[1] == (4,)
+    assert_eq(x, result, scheduler=c)
+
+
+@pytest.mark.parametrize(
+    "x, chunks",
+    [
+        (da.ones(shape=(50, 10), chunks=(25, 10)), (None, 5)),
+        (da.ones(shape=(50, 10), chunks=(25, 10)), {1: 5}),
+        (da.ones(shape=(50, 10), chunks=(25, 10)), (None, (5, 5))),
+        (da.ones(shape=(1000, 10), chunks=(5, 10)), (None, 5)),
+        (da.ones(shape=(1000, 10), chunks=(5, 10)), {1: 5}),
+        (da.ones(shape=(1000, 10), chunks=(5, 10)), (None, (5, 5))),
+        (da.ones(shape=(10, 10), chunks=(10, 10)), (None, 5)),
+        (da.ones(shape=(10, 10), chunks=(10, 10)), {1: 5}),
+        (da.ones(shape=(10, 10), chunks=(10, 10)), (None, (5, 5))),
+        (da.ones(shape=(10, 10), chunks=(10, 2)), (None, 5)),
+        (da.ones(shape=(10, 10), chunks=(10, 2)), {1: 5}),
+        (da.ones(shape=(10, 10), chunks=(10, 2)), (None, (5, 5))),
+    ],
+)
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_unknown(c, s, *ws, x, chunks):
+    dd = pytest.importorskip("dask.dataframe")
+    y = dd.from_array(x).values
+    result = y.rechunk(chunks, rechunk="p2p")
+    expected = x.rechunk(chunks, rechunk="p2p")
+
+    assert_chunks_match(result.chunks, expected.chunks)
+    assert_eq(result, expected, scheduler=c)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_unknown_explicit(c, s, *ws):
+    dd = pytest.importorskip("dask.dataframe")
+    x = da.ones(shape=(10, 10), chunks=(5, 2))
+    y = dd.from_array(x).values
+    result = y.rechunk(((float("nan"), float("nan")), (5, 5)), rechunk="p2p")
+    expected = x.rechunk((None, (5, 5)), rechunk="p2p")
+    assert_chunks_match(result.chunks, expected.chunks)
+    assert_eq(result, expected, scheduler=c)
+
+
+def assert_chunks_match(left, right):
+    for x, y in zip(left, right):
+        if np.isnan(x).any():
+            assert np.isnan(x).all()
+        else:
+            assert x == y
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_unknown_raises(c, s, *ws):
+    dd = pytest.importorskip("dask.dataframe")
+
+    x = dd.from_array(da.ones(shape=(10, 10), chunks=(5, 5))).values
+    with pytest.raises(ValueError):
+        x.rechunk((None, (5, 5, 5)), rechunk="p2p")
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_zero_dim(c, s, *ws):
+    da = pytest.importorskip("dask.array")
+
+    x = da.ones((0, 10, 100), chunks=(0, 10, 10)).rechunk((0, 10, 50), rechunk="p2p")
+    assert len(await c.compute(x)) == 0
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_empty_chunks(c, s, *ws):
+    x = da.zeros((7, 24), chunks=((7,), (10, 0, 0, 9, 0, 5)))
+    y = x.rechunk((2, 3), rechunk="p2p")
+    assert_eq(x, y, scheduler=c)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_avoid_needless_chunking(c, s, *ws):
+    x = da.ones(16, chunks=2)
+    y = x.rechunk(8, rechunk="p2p")
+    dsk = y.__dask_graph__()
+    assert len(dsk) <= 8 + 2
+
+
+@pytest.mark.parametrize(
+    "shape,chunks,bs,expected",
+    [
+        (100, 1, 10, (10,) * 10),
+        (100, 50, 10, (10,) * 10),
+        (100, 100, 10, (10,) * 10),
+        (20, 7, 10, (7, 7, 6)),
+        (20, (1, 1, 1, 1, 6, 2, 1, 7), 5, (5, 5, 5, 5)),
+    ],
+)
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_auto_1d(c, s, *ws, shape, chunks, bs, expected):
+    x = da.ones(shape, chunks=(chunks,))
+    y = x.rechunk({0: "auto"}, block_size_limit=bs * x.dtype.itemsize, rechunk="p2p")
+    assert y.chunks == (expected,)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_auto_2d(c, s, *ws):
+    x = da.ones((20, 20), chunks=(2, 2))
+    y = x.rechunk(
+        {0: -1, 1: "auto"}, block_size_limit=20 * x.dtype.itemsize, rechunk="p2p"
+    )
+    assert y.chunks == ((20,), (1,) * 20)
+
+    x = da.ones((20, 20), chunks=(2, 2))
+    y = x.rechunk((-1, "auto"), block_size_limit=80 * x.dtype.itemsize, rechunk="p2p")
+    assert y.chunks == ((20,), (4,) * 5)
+
+    x = da.ones((20, 20), chunks=((2, 2)))
+    y = x.rechunk({0: "auto"}, block_size_limit=20 * x.dtype.itemsize, rechunk="p2p")
+    assert y.chunks[1] == x.chunks[1]
+    assert y.chunks[0] == (10, 10)
+
+    x = da.ones((20, 20), chunks=((2,) * 10, (2, 2, 2, 2, 2, 5, 5)))
+    y = x.rechunk({0: "auto"}, block_size_limit=20 * x.dtype.itemsize, rechunk="p2p")
+    assert y.chunks[1] == x.chunks[1]
+    assert y.chunks[0] == (4, 4, 4, 4, 4)  # limited by largest
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_auto_3d(c, s, *ws):
+    x = da.ones((20, 20, 20), chunks=((2, 2, 2)))
+    y = x.rechunk(
+        {0: "auto", 1: "auto"}, block_size_limit=200 * x.dtype.itemsize, rechunk="p2p"
+    )
+    assert y.chunks[2] == x.chunks[2]
+    assert y.chunks[0] == (10, 10)
+    assert y.chunks[1] == (10, 10)  # even split
+
+
+@pytest.mark.parametrize("n", [100, 1000])
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_auto_image_stack(c, s, *ws, n):
+    with dask.config.set({"array.chunk-size": "10MiB"}):
+        x = da.ones((n, 1000, 1000), chunks=(1, 1000, 1000), dtype="uint8")
+        y = x.rechunk("auto", rechunk="p2p")
+        assert y.chunks == ((10,) * (n // 10), (1000,), (1000,))
+        assert y.rechunk("auto", rechunk="p2p").chunks == y.chunks  # idempotent
+
+    with dask.config.set({"array.chunk-size": "7MiB"}):
+        z = x.rechunk("auto", rechunk="p2p")
+        if n == 100:
+            assert z.chunks == ((7,) * 14 + (2,), (1000,), (1000,))
+        else:
+            assert z.chunks == ((7,) * 142 + (6,), (1000,), (1000,))
+
+    with dask.config.set({"array.chunk-size": "1MiB"}):
+        x = da.ones((n, 1000, 1000), chunks=(1, 1000, 1000), dtype="float64")
+        z = x.rechunk("auto", rechunk="p2p")
+        assert z.chunks == ((1,) * n, (362, 362, 276), (362, 362, 276))
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_down(c, s, *ws):
+    with dask.config.set({"array.chunk-size": "10MiB"}):
+        x = da.ones((100, 1000, 1000), chunks=(1, 1000, 1000), dtype="uint8")
+        y = x.rechunk("auto", rechunk="p2p")
+        assert y.chunks == ((10,) * 10, (1000,), (1000,))
+
+    with dask.config.set({"array.chunk-size": "1MiB"}):
+        z = y.rechunk("auto", rechunk="p2p")
+        assert z.chunks == ((4,) * 25, (511, 489), (511, 489))
+
+    with dask.config.set({"array.chunk-size": "1MiB"}):
+        z = y.rechunk({0: "auto"}, rechunk="p2p")
+        assert z.chunks == ((1,) * 100, (1000,), (1000,))
+
+        z = y.rechunk({1: "auto"}, rechunk="p2p")
+        assert z.chunks == ((10,) * 10, (104,) * 9 + (64,), (1000,))
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_zero(c, s, *ws):
+    with dask.config.set({"array.chunk-size": "1B"}):
+        x = da.ones(10, chunks=(5,))
+        y = x.rechunk("auto", rechunk="p2p")
+        assert y.chunks == ((1,) * 10,)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_bad_keys(c, s, *ws):
+    x = da.zeros((2, 3, 4), chunks=1)
+    assert x.rechunk({-1: 4}, rechunk="p2p").chunks == ((1, 1), (1, 1, 1), (4,))
+    assert x.rechunk({-x.ndim: 2}, rechunk="p2p").chunks == (
+        (2,),
+        (1, 1, 1),
+        (1, 1, 1, 1),
+    )
+
+    with pytest.raises(TypeError) as info:
+        x.rechunk({"blah": 4}, rechunk="p2p")
+
+    assert "blah" in str(info.value)
+
+    with pytest.raises(ValueError) as info:
+        x.rechunk({100: 4}, rechunk="p2p")
+
+    assert "100" in str(info.value)
+
+    with pytest.raises(ValueError) as info:
+        x.rechunk({-100: 4}, rechunk="p2p")
+
+    assert "-100" in str(info.value)
+
+
+@gen_cluster(client=True, config={"optimization.fuse.active": False})
+async def test_rechunk_with_zero(c, s, *ws):
+    a = da.ones((8, 8), chunks=(4, 4))
+    result = a.rechunk(((4, 4), (4, 0, 0, 4)), rechunk="p2p")
+    expected = da.ones((8, 8), chunks=((4, 4), (4, 0, 0, 4)))
+
+    # reverse:
+    a, expected = expected, a
+    result = a.rechunk((4, 4), rechunk="p2p")
+    assert_eq(result, expected, scheduler=c)
