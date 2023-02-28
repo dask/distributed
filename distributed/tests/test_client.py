bb906e15408d61e23d66e8d1f535341ed5c57184
fjetter <fjetter@users.noreply.github.com>
2023-02-28 13:34:26 +0100

    only pick p2p if pyarrow is installed

--- distributed/tests/test_client.py
+++ distributed/tests/test_client.py
@@ -3258,11 +3258,18 @@ async def test_cancel_clears_processing(c, s, *workers):
 
 
 def test_default_get(loop_in_thread):
+    has_pyarrow = False
+    try:
+        import pyarrow  # noqa: F401
+
+        has_pyarrow = True
+    except ImportError:
+        pass
     loop = loop_in_thread
     with cluster() as (s, [a, b]):
         pre_get = dask.base.get_scheduler()
         # These may change in the future but the selection below should not
-        distributed_default = "p2p"
+        distributed_default = "p2p" if has_pyarrow else "tasks"
         local_default = "disk"
         assert get_default_shuffle_algorithm() == local_default
         with Client(s["address"], set_as_default=True, loop=loop) as c:
