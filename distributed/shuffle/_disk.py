7cc67c26a98c3906fc0f81344a603fbc4472533f
Lawrence Mitchell <lmitchell@nvidia.com>
2023-03-17 12:24:07 +0000

    p2p: Add in-memory buffering for output DiskShardsBuffer
    It is likely that at least some part (possibly all) of the output of
    the shuffle will fit in memory. In this circumstance, we don't need to
    necessarily write output shards to disk only to read them in later.
    
    To enable this, provide (optional) in-memory buffering on the output
    DiskShardsBuffer. While the total output size is less than some limit,
    don't bother hitting the disk, but rather just hold in memory
    references. Once too much data has arrived, block to flush these
    buffers to disk.
    
    When reading, we now might have some shards in memory, so we
    concatenate these with the on-disk buffers.

--- distributed/shuffle/_disk.py
+++ distributed/shuffle/_disk.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import contextlib
 import pathlib
 import shutil
+from collections import defaultdict
+
+from dask.utils import parse_bytes
 
 from distributed.shuffle._buffer import ShardsBuffer
 from distributed.shuffle._limiter import ResourceLimiter
@@ -38,6 +41,9 @@ class DiskShardsBuffer(ShardsBuffer):
         implementation of this scheme.
     """
 
+    # TODO: Configure based on worker memory limits (possibly dynamically?)
+    max_buffer_size = parse_bytes("1 GiB")
+
     def __init__(
         self,
         directory: str | pathlib.Path,
@@ -50,6 +56,8 @@ class DiskShardsBuffer(ShardsBuffer):
         )
         self.directory = pathlib.Path(directory)
         self.directory.mkdir(exist_ok=True)
+        self._in_memory = 0
+        self._memory_buf: defaultdict[str, list[bytes]] = defaultdict(list)
 
     async def _process(self, id: str, shards: list[bytes]) -> None:
         """Write one buffer to file
@@ -65,38 +73,62 @@ class DiskShardsBuffer(ShardsBuffer):
         dropping the write into communicate above.
         """
 
+        # Normalisation for safety
+        id = str(id)
         with log_errors():
             # Consider boosting total_size a bit here to account for duplication
             with self.time("write"):
-                with open(
-                    self.directory / str(id), mode="ab", buffering=100_000_000
-                ) as f:
-                    for shard in shards:
-                        f.write(shard)
+                if not self.max_buffer_size:
+                    # Fast path if we're always hitting the disk
+                    self._write(id, shards)
+                else:
+                    while shards:
+                        if self._in_memory < self.max_buffer_size:
+                            self._memory_buf[id].append(newdata := shards.pop())
+                            self._in_memory += len(newdata)
+                        else:
+                            # Flush old data
+                            # This could be offloaded to a background
+                            # task at the cost of going further over
+                            # the soft memory limit.
+                            for id, bufs in self._memory_buf.items():
+                                self._write(id, bufs)
+                            self._memory_buf.clear()
+                            self._in_memory = 0
 
-    def read(self, id: int | str) -> bytes:
-        """Read a complete file back into memory"""
-        self.raise_on_exception()
-        if not self._inputs_done:
-            raise RuntimeError("Tried to read from file before done.")
-
-        try:
-            with self.time("read"):
-                with open(
-                    self.directory / str(id), mode="rb", buffering=100_000_000
-                ) as f:
-                    data = f.read()
-                    size = f.tell()
-        except FileNotFoundError:
-            raise KeyError(id)
-
-        if data:
-            self.bytes_read += size
-            return data
-        else:
-            raise KeyError(id)
+    def _write(self, id: str, shards: list[bytes]) -> None:
+        with open(self.directory / str(id), mode="ab", buffering=100_000_000) as f:
+            for s in shards:
+                f.write(s)
 
     async def close(self) -> None:
         await super().close()
         with contextlib.suppress(FileNotFoundError):
             shutil.rmtree(self.directory)
+
+    def read(self, id: int | str) -> bytes:
+        """Read a complete file back into memory, concatting with any
+        in memory parts"""
+        self.raise_on_exception()
+        if not self._inputs_done:
+            raise RuntimeError("Tried to read from file before done.")
+
+        id = str(id)
+        data = self._memory_buf[id]
+        try:
+            with self.time("read"):
+                with open(
+                    self.directory / str(id), mode="rb", buffering=100_000_000
+                ) as f:
+                    data.append(f.read())
+        except FileNotFoundError:
+            if not data:
+                # Neither disk nor in memory
+                raise KeyError(id)
+
+        if data:
+            buf = b"".join(data)
+            self.bytes_read += len(buf)
+            return buf
+        else:
+            raise KeyError(id)
