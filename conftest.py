54e66e53b32b5b2aedc9bb54cbe5bcac4263dbfd
Miles Granger <miles59923@gmail.com>
2023-04-05 09:54:38 +0200

    Remove gilknocker patch

--- conftest.py
+++ conftest.py
@@ -41,35 +41,4 @@ def pytest_collection_modifyitems(config, items):
             item.add_marker(pytest.mark.workerstate)
 
 
-@pytest.fixture(autouse=True)
-def patch_gilknocker(request):
-    """
-    A very subtle bug where a SIGABRT can kill the main Python thread out
-    from underneath gilknocker's GIL sampling thread which can lead to:
-
-        FATAL: exception not rethrown
-        Fatal Python error: Aborted
-
-        Current thread 0x00007fbae56bc640 (most recent call first):
-        <no Python frame>
-
-    Due to a test/object/ect not properly calling 'gilknocker.KnockKnock.stop'.
-    This bug rarely shows up as is, even when trying to bring it up deliberatly.
-    More details can be found here: https://github.com/PyO3/pyo3/issues/2102
-
-    Therefore, we patch all tests so `start` doesn't trigger the sampling thread,
-    except when explicitly tested in 'test_gil_contention' test.
-    """
-    if "gil" in request.node.name:
-        yield
-    else:
-        try:
-            import gilknocker  # noqa F401
-        except ImportError:
-            yield
-        else:
-            with mock.patch("gilknocker.KnockKnock.start"):
-                yield
-
-
 pytest_plugins = ["distributed.pytest_resourceleaks"]
