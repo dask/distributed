7387dbc46fa53bda96b95893538d06cd79676030
crusaderky <crusaderky@gmail.com>
2023-04-12 17:54:40 +0100

    Add test

--- distributed/tests/test_compatibility.py
+++ distributed/tests/test_compatibility.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import random
 from collections import Counter
 
 from distributed.compatibility import randbytes
@@ -11,4 +12,13 @@ def test_randbytes():
     assert len(x) == 256_000
     c = Counter(x)
     for i in range(256):
-        assert 800 < c[i] < 1200
+        assert 800 < c[i] < 1200, (i, c[i])
+
+
+def test_randbytes_seed():
+    state = random.getstate()
+    try:
+        random.seed(0)
+        assert randbytes(8) == b'\xcd\x07,\xd8\xbeo\x9fb'
+    finally:
+        random.setstate(state)
