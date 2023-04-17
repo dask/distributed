32bd3511656418c23fd5c5e06f0f1ba00a6bf5d4
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-04-17 13:07:17 +0100

    Ensure idle_since is returned correctly

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -7872,7 +7872,6 @@ class Scheduler(SchedulerState, ServerNode):
         if self.transition_counter != self._idle_transition_counter:
             self._idle_transition_counter = self.transition_counter
             self.idle_since = None
-            return None
 
         if (
             self.queued
@@ -7880,11 +7879,9 @@ class Scheduler(SchedulerState, ServerNode):
             or any(ws.processing for ws in self.workers.values())
         ):
             self.idle_since = None
-            return None
 
         if not self.idle_since:
             self.idle_since = time()
-            return self.idle_since
 
         if self.jupyter:
             last_activity = (
@@ -7892,7 +7889,6 @@ class Scheduler(SchedulerState, ServerNode):
             )
             if last_activity > self.idle_since:
                 self.idle_since = last_activity
-                return self.idle_since
 
         if self.idle_timeout:
             if time() > self.idle_since + self.idle_timeout:
@@ -7902,7 +7898,8 @@ class Scheduler(SchedulerState, ServerNode):
                     format_time(self.idle_timeout),
                 )
                 self._ongoing_background_tasks.call_soon(self.close)
-        return None
+
+        return self.idle_since
 
     def adaptive_target(self, target_duration=None):
         """Desired number of workers based on the current workload
