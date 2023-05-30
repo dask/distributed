a4d5a71387b985504e12aba5a6f1ff2b9b46d016
crusaderky <crusaderky@gmail.com>
2023-05-30 23:50:31 +0100

    TaskGroup.start can move backwards (#7867)

--- distributed/tests/test_scheduler.py
+++ distributed/tests/test_scheduler.py
@@ -2643,6 +2643,39 @@ async def test_task_groups(c, s, a, b):
     assert "compute" in tg.all_durations
 
 
+@gen_cluster(client=True, nthreads=[("", 2)], Worker=NoSchedulerDelayWorker)
+async def test_task_groups_update_start_stop(c, s, a):
+    """TaskGroup.stop increases as the tasks in the group finish.
+    TaskGroup.start can move backwards in the following use case:
+
+    - task (x, 0) starts
+    - task (x, 1) starts
+    - task (x, 1) finishes
+    - task (x, 0) finishes
+    """
+    ev = Event()
+    t0 = time()
+    x0 = c.submit(ev.wait, key=("x", 0))
+    await wait_for_state(str(x0.key), "executing", a)
+    tg = s.task_groups["x"]
+    assert tg.start == tg.stop == 0
+    t1 = time()
+    x1 = c.submit(inc, 1, key=("x", 1))
+    await x1
+    t2 = time()
+    assert t0 < t1 < tg.start < tg.stop < t2
+
+    await ev.set()
+    await x0
+    t3 = time()
+    assert t0 < tg.start < t1 < t2 < tg.stop < t3
+
+    x2 = c.submit(inc, 1, key=("x", 2))
+    await x2
+    t4 = time()
+    assert t0 < tg.start < t1 < t2 < t3 < tg.stop < t4
+
+
 @gen_cluster(client=True)
 async def test_task_prefix(c, s, a, b):
     da = pytest.importorskip("dask.array")
