38898f7b80375acf8175125dcc9c3a3df9e6e5db
Hendrik Makait <hendrik@coiled.io>
2023-06-12 18:01:23 +0200

    Copy only if necessary

--- distributed/shuffle/_arrow.py
+++ distributed/shuffle/_arrow.py
@@ -69,7 +69,7 @@ def convert_partition(data: bytes, meta: pd.DataFrame) -> pd.DataFrame:
         return None
 
     df = table.to_pandas(self_destruct=True, types_mapper=default_types_mapper)
-    return df.astype(meta.dtypes)
+    return df.astype(meta.dtypes, copy=False)
 
 
 def list_of_buffers_to_table(data: list[bytes]) -> pa.Table:
