60d4c8120f6f7c7fd8d7ac59848918a507fd308f
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-06-29 23:35:24 -0500

    Update distributed/worker_memory.py

--- distributed/worker_memory.py
+++ distributed/worker_memory.py
@@ -25,7 +25,7 @@ import logging
 import os
 import sys
 import warnings
-from collections.abc import Callable, Container, Hashable, Iterable, MutableMapping
+from collections.abc import Callable, Container, Hashable, MutableMapping
 from contextlib import suppress
 from functools import partial
 from typing import TYPE_CHECKING, Any, Literal, Union, cast
