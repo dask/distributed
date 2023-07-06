9d804dc859be35f25a3aab8323453b102238a597
Hendrik Makait <hendrik@coiled.io>
2023-07-06 20:06:41 +0200

    warning

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -5071,9 +5071,18 @@ class Scheduler(SchedulerState, ServerNode):
         awaitables = []
         for plugin in list(self.plugins.values()):
             try:
-                result = plugin.remove_worker(
-                    scheduler=self, worker=address, stimulus_id=stimulus_id
-                )
+                parameters = inspect.signature(plugin.remove_worker).parameters
+                if "stimulus_id" in parameters or "kwargs" in parameters:
+                    result = plugin.remove_worker(
+                        scheduler=self, worker=address, stimulus_id=stimulus_id
+                    )
+                else:
+                    warnings.warn(
+                        "The `stimulus_id` keyword argument has been added to `SchedulerPlugin.remove_worker`. "
+                        "Not supporting the `stimulus_id` keyword argument or `**kwargs` will no longer be supported in future versions.",
+                        FutureWarning,
+                    )
+                    result = plugin.remove_worker(scheduler=self, worker=address)  # type: ignore
                 if inspect.isawaitable(result):
                     awaitables.append(result)
             except Exception as e:
