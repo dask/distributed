3ac439c91a104508ece1becbce4400af1e342184
Hendrik Makait <hendrik@coiled.io>
2023-07-20 11:33:01 +0200

    Remove __eq__

--- distributed/shuffle/_scheduler_plugin.py
+++ distributed/shuffle/_scheduler_plugin.py
@@ -51,9 +51,6 @@ class ShuffleState(abc.ABC):
     def __str__(self) -> str:
         return f"{self.__class__.__name__}<{self.id}[{self.run_id}]>"
 
-    def __eq__(self, other: Any) -> bool:
-        return type(other) == type(self) and other.run_id == self.run_id
-
     def __hash__(self) -> int:
         return hash(self.run_id)
 
