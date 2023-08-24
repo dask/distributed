aa2711f599565d750479cc3f580e71146744bed5
Hendrik Makait <hendrik@coiled.io>
2023-08-24 14:11:12 +0200

    Experiment

--- distributed/shuffle/_worker_plugin.py
+++ distributed/shuffle/_worker_plugin.py
@@ -104,10 +104,11 @@ class ShuffleWorkerPlugin(WorkerPlugin):
             await shuffle.inputs_done()
 
     async def _close_shuffle_run(self, shuffle: ShuffleRun) -> None:
-        await shuffle.close()
-        async with self._runs_cleanup_condition:
-            self._runs.remove(shuffle)
-            self._runs_cleanup_condition.notify_all()
+        pass
+        # await shuffle.close()
+        # async with self._runs_cleanup_condition:
+        #     self._runs.remove(shuffle)
+        #     self._runs_cleanup_condition.notify_all()
 
     def shuffle_fail(self, shuffle_id: ShuffleId, run_id: int, message: str) -> None:
         """Fails the shuffle run with the message as exception and triggers cleanup.
