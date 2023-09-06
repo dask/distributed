2606b18814d36d34c5a885515382cd4a21200a03
Hendrik Makait <hendrik.makait@gmail.com>
2023-09-06 08:50:20 +0200

    Update distributed/diagnostics/tests/test_worker_plugin.py
    Co-authored-by: crusaderky <crusaderky@gmail.com>

--- distributed/diagnostics/tests/test_worker_plugin.py
+++ distributed/diagnostics/tests/test_worker_plugin.py
@@ -280,6 +280,6 @@ async def test_duck_typed_worker_plugin_is_deprecated(c, s, a):
             pass
 
     n_existing_plugins = len(a.plugins)
-    with pytest.warns(DeprecationWarning, match="duck-typed"):
+    with pytest.warns(DeprecationWarning, match="duck-typed.*WorkerPlugin"):
         await c.register_worker_plugin(DuckPlugin())
     assert len(a.plugins) == n_existing_plugins + 1
