12394686e46f37c65bc4093d353cec49eef77fc7
Hendrik Makait <hendrik.makait@gmail.com>
2023-09-06 08:50:10 +0200

    Update distributed/diagnostics/tests/test_nanny_plugin.py
    Co-authored-by: crusaderky <crusaderky@gmail.com>

--- distributed/diagnostics/tests/test_nanny_plugin.py
+++ distributed/diagnostics/tests/test_nanny_plugin.py
@@ -16,6 +16,6 @@ async def test_duck_typed_nanny_plugin_is_deprecated(c, s, a):
             pass
 
     n_existing_plugins = len(a.plugins)
-    with pytest.warns(DeprecationWarning, match="duck-typed"):
+    with pytest.warns(DeprecationWarning, match="duck-typed.*NannyPlugin"):
         await c.register_worker_plugin(DuckPlugin(), nanny=True)
     assert len(a.plugins) == n_existing_plugins + 1
