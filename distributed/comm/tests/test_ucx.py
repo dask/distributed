0aa0aa25a82b80783c9c5e6c6952e05b33d3560c
Peter Andreas Entschev <peter@entschev.com>
2023-11-13 16:05:20 +0100

    Update cuDF test with explicit `dtype=object` (#8339)

--- distributed/comm/tests/test_ucx.py
+++ distributed/comm/tests/test_ucx.py
@@ -181,7 +181,7 @@ async def test_ucx_deserialize(ucx_loop):
     [
         lambda cudf: cudf.Series([1, 2, 3]),
         lambda cudf: cudf.Series([], dtype=object),
-        lambda cudf: cudf.DataFrame([]),
+        lambda cudf: cudf.DataFrame([], dtype=object),
         lambda cudf: cudf.DataFrame([1]).head(0),
         lambda cudf: cudf.DataFrame([1.0]).head(0),
         lambda cudf: cudf.DataFrame({"a": []}),
