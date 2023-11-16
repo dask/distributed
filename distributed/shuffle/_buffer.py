05a2b227eef3adb2ef46c9f33507da52abb35753
Hendrik Makait <hendrik@coiled.io>
2023-11-16 19:31:40 +0100

    WIP

--- distributed/shuffle/_buffer.py
+++ distributed/shuffle/_buffer.py
@@ -5,7 +5,8 @@ import contextlib
 import logging
 from collections import defaultdict
 from collections.abc import Iterator, Sized
-from typing import TYPE_CHECKING, Any, Generic, TypeVar
+from types import TracebackType
+from typing import TYPE_CHECKING, Any, Generic, Literal, NamedTuple, TypeVar
 
 from distributed.metrics import time
 from distributed.shuffle._limiter import ResourceLimiter
@@ -14,13 +15,17 @@ from distributed.sizeof import sizeof
 logger = logging.getLogger("distributed.shuffle")
 if TYPE_CHECKING:
     # TODO import from collections.abc (requires Python >=3.12)
-    from typing_extensions import Buffer
+    # TODO import from typing (requires Python >= 3.10)
+    from typing_extensions import Buffer, TypeAlias
 else:
     Buffer = Sized
 
 ShardType = TypeVar("ShardType", bound=Buffer)
 
 T = TypeVar("T")
+
+
+BufferState: TypeAlias = Literal["open", "flushing", "flushed", "erred", "closed"]
 
 
 class ShardsBuffer(Generic[ShardType]):
@@ -267,3 +272,227 @@ class ShardsBuffer(Generic[ShardType]):
         yield
         stop = time()
         self.diagnostics[name] += stop - start
+
+
+class SizedShard(NamedTuple, Generic[ShardType]):
+    shard: ShardType
+    size: int
+
+
+class BaseBuffer(Generic[ShardType]):
+    #: List of buffered shards per output key with their approximate in-memory size
+    shards: defaultdict[str, list[SizedShard[ShardType]]]
+    #: Total size of in-memory data per flushable key
+    flushable_sizes: defaultdict[str, int]
+    #: Total size of in-memory data per currently flushing key
+    flushing_sizes: dict[str, int]
+    #: Limit of concurrent tasks flushing data
+    concurrency_limit: int
+    #: Plugin-wide resource limiter used to apply back-pressure
+    memory_limiter: ResourceLimiter
+    #: Diagnostic data
+    diagnostics: dict[str, float]
+    #: Maximum size of data per key when flushing
+    max_message_size: int
+
+    bytes_total: int
+    bytes_memory: int
+    bytes_written: int
+    bytes_read: int
+
+    #: State of the buffer
+    _state: BufferState
+    #: Exception that occurred while flushing (if exists)
+    _exception: Exception | None
+    #: Async condition used for coordination
+    _flush_condition: asyncio.Condition
+    #: Background tasks flushing data
+    _tasks: list[asyncio.Task]
+
+    def __init__(
+        self,
+        memory_limiter: ResourceLimiter,
+        concurrency_limit: int = 2,
+        max_message_size: int = -1,
+    ) -> None:
+        self.shards = defaultdict(list[SizedShard])
+        self.flushable_sizes = defaultdict(int)
+        self.flushing_sizes = {}
+        self.memory_limiter = memory_limiter
+        self.diagnostics: dict[str, float] = defaultdict(float)
+        self.max_message_size = max_message_size
+
+        self.bytes_total = 0
+        self.bytes_memory = 0
+        self.bytes_written = 0
+        self.bytes_read = 0
+        self._tasks = [
+            asyncio.create_task(self._background_task())
+            for _ in range(concurrency_limit)
+        ]
+
+    def heartbeat(self) -> dict[str, Any]:
+        return {
+            "memory": self.bytes_memory,
+            "total": self.bytes_total,
+            "buckets": len(self.shards),
+            "written": self.bytes_written,
+            "read": self.bytes_read,
+            "diagnostics": self.diagnostics,
+            "memory_limit": self.memory_limiter.limit,
+        }
+
+    async def write(self, data: dict[str, Any]) -> None:
+        self._raise_if_erred()
+
+        if self._state != "open":
+            raise RuntimeError(
+                f"{self} is no longer open for new data, it is {self._state}."
+            )
+
+        if not data:
+            return
+
+        async with self._flush_condition:
+            for worker, shard in data.items():
+                size = sizeof(shard)
+                self.shards[worker].append(SizedShard(shard=shard, size=size))
+                if worker in self.flushing_sizes:
+                    self.flushing_sizes[worker] += size
+                else:
+                    self.flushable_sizes[worker] += size
+                self.bytes_memory += size
+                self.bytes_total += size
+                self.memory_limiter.increase(size)
+            del data
+            self._flush_condition.notify_all()
+        await self.memory_limiter.wait_for_available()
+
+    async def _background_task(self) -> None:
+        def _continue() -> bool:
+            return bool(self.flushable_sizes or self._state != "open")
+
+        while True:
+            async with self._flush_condition:
+                await self._flush_condition.wait_for(_continue)
+                if not self.flushable_sizes and not self._state != "open":
+                    break
+            await self.flush_largest()
+
+    async def flush_largest(self) -> None:
+        if not self.flushable_sizes or self._state not in {"open", "flushing"}:
+            return
+        largest_partition = max(
+            self.flushable_sizes, key=self.flushable_sizes.__getitem__
+        )
+
+        partition_size = self.flushable_sizes.pop(largest_partition)
+        shards: list[ShardType]
+
+        if self.max_message_size > 0:
+            shards = []
+            message_size = 0
+            while message_size < self.max_message_size:
+                try:
+                    shard, size = self.shards[largest_partition].pop()
+                    message_size += size
+                    shards.append(shard)
+                except IndexError:
+                    break
+            if message_size == partition_size:
+                assert not self.shards[largest_partition]
+                del self.shards[largest_partition]
+        else:
+            shards = [shard for shard, _ in self.shards.pop(largest_partition)]
+            message_size = partition_size
+
+        self.flushing_sizes[largest_partition] = partition_size - message_size
+
+        bytes_written = 0
+        start = time()
+        try:
+            bytes_written = await self._flush(largest_partition, shards)
+        except Exception as e:
+            if not self._state == "erred":
+                self._exception = e
+                self._state = "erred"
+                self.shards.clear()
+                flushable_size = sum(self.flushable_sizes.values())
+                self.flushable_sizes.clear()
+                flushing_size = sum(self.flushable_sizes.values())
+                self.flushing_sizes.clear()
+                await self.memory_limiter.decrease(flushable_size + flushing_size)
+        finally:
+            stop = time()
+            self.diagnostics["avg_size"] = (
+                0.98 * self.diagnostics["avg_size"] + 0.02 * message_size
+            )
+            self.diagnostics["avg_duration"] = 0.98 * self.diagnostics[
+                "avg_duration"
+            ] + 0.02 * (stop - start)
+
+            async with self._flush_condition:
+                self.bytes_memory -= message_size
+                self.bytes_written += bytes_written
+                if self._state in {"open", "flushing"}:
+                    if (
+                        size_since_flush := self.flushing_sizes.pop(largest_partition)
+                    ) > 0:
+                        self.flushable_sizes[largest_partition] = size_since_flush
+                else:
+                    assert self._state == "erred"
+                await self.memory_limiter.decrease(message_size)
+                self._flush_condition.notify_all()
+
+    async def flush(self) -> None:
+        if self._state in {"flushed", "closed"}:
+            return
+
+        if self._state == "flushing":
+            async with self._flush_condition:
+                await self._flush_condition.wait_for(
+                    lambda: self._state in {"erred", "flushed", "closed"}
+                )
+            self._raise_if_erred()
+            return
+        self._raise_if_erred()
+        assert self._state == "open", self._state
+
+        async with self._flush_condition:
+            await asyncio.gather(*self._tasks)
+        self._raise_if_erred()
+
+    async def close(self) -> None:
+        if self._state == "closed":
+            return
+
+        try:
+            await self.flush()
+        except Exception:
+            assert self._state == "erred"
+        await asyncio.gather(*self._tasks, return_exceptions=True)
+        self._state = "closed"
+
+        assert not self.shards
+        assert not self.flushable_sizes
+        assert not self.flushing_sizes
+        assert not self.bytes_memory
+
+    async def __aenter__(self) -> BaseBuffer:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        await self.close()
+
+    def _raise_if_erred(self) -> None:
+        if self._state == "erred":
+            assert self._exception
+            raise self._exception
+
+    async def _flush(self, id: str, shards: list[ShardType]) -> int:
+        raise NotImplementedError()
