c5f1379200c4802b80470e24a2c1b2ac49b8e891
fjetter <fjetter@users.noreply.github.com>
2024-01-09 10:50:05 +0100

    rewrite the thing

--- distributed/protocol/pickle.py
+++ distributed/protocol/pickle.py
@@ -67,14 +67,16 @@ def dumps(x, *, buffer_callback=None, protocol=HIGHEST_PROTOCOL):
             buffers.clear()
             pickler.dump(x)
             result = f.getvalue()
-        if (
+
+        if not _always_use_pickle_for(x) and (
             CLOUDPICKLE_GE_20
             and getattr(inspect.getmodule(x), "__name__", None)
             in cloudpickle.list_registry_pickle_by_value()
-        ) or (
-            (len(result) < 1000 and not _always_use_pickle_for(x))
-            # this is pretty expensive so check very last
-            and b"__main__" in result
+            or (
+                len(result) < 1000
+                # Do this very last since it's expensive
+                and b"__main__" in result
+            )
         ):
             buffers.clear()
             result = cloudpickle.dumps(x, **dump_kwargs)
