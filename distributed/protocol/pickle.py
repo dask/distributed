f4d3c119a7ace7a142d0f4a92dabd0dbca3e25c0
fjetter <fjetter@users.noreply.github.com>
2024-01-09 10:15:07 +0100

    Check if main is in pickled stream very last

--- distributed/protocol/pickle.py
+++ distributed/protocol/pickle.py
@@ -67,14 +67,17 @@ def dumps(x, *, buffer_callback=None, protocol=HIGHEST_PROTOCOL):
             buffers.clear()
             pickler.dump(x)
             result = f.getvalue()
-        if b"__main__" in result or (
+        if (
             CLOUDPICKLE_GE_20
             and getattr(inspect.getmodule(x), "__name__", None)
             in cloudpickle.list_registry_pickle_by_value()
+        ) or (
+            (len(result) < 1000 or not _always_use_pickle_for(x))
+            # this is pretty expensive so check very last
+            and b"__main__" in result
         ):
-            if len(result) < 1000 or not _always_use_pickle_for(x):
-                buffers.clear()
-                result = cloudpickle.dumps(x, **dump_kwargs)
+            buffers.clear()
+            result = cloudpickle.dumps(x, **dump_kwargs)
     except Exception:
         try:
             buffers.clear()
