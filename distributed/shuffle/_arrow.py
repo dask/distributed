151e679320f7a5a13d78a3e9920bf30ffd7b0220
Hendrik Makait <hendrik@makait.com>
2024-01-12 13:46:44 +0100

    pyarrow=7 support

--- distributed/shuffle/_arrow.py
+++ distributed/shuffle/_arrow.py
@@ -70,7 +70,7 @@ def convert_shards(shards: list[pa.Table], meta: pd.DataFrame) -> pd.DataFrame:
 
     table = concat_tables(shards)
     table = table.sort_by(_INPUT_PARTITION_ID_COLUMN)
-    table = table.drop(_INPUT_PARTITION_ID_COLUMN)
+    table = table.drop([_INPUT_PARTITION_ID_COLUMN])
 
     df = from_pyarrow_table_dispatch(meta, table, self_destruct=True)
     reconciled_dtypes = {}
