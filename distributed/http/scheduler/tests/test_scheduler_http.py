bcc262c0e8f6ac0a18a3530da9a95b9b1d14aecf
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-04-11 08:12:40 -0500

    Expose paused and retired workers separately in prometheus

--- distributed/http/scheduler/tests/test_scheduler_http.py
+++ distributed/http/scheduler/tests/test_scheduler_http.py
@@ -282,6 +282,8 @@ async def test_prometheus_collect_worker_states(c, s, a, b):
         "partially_saturated": 0,
         "saturated": 0,
         "paused_or_retiring": 0,
+        "paused": 0,
+        "retiring": 0,
     }
 
     ev = Event()
@@ -292,6 +294,8 @@ async def test_prometheus_collect_worker_states(c, s, a, b):
         "partially_saturated": 1,
         "saturated": 0,
         "paused_or_retiring": 0,
+        "paused": 0,
+        "retiring": 0,
     }
 
     y = c.submit(lambda ev: ev.wait(), ev, key="y", workers=[a.address])
@@ -304,6 +308,8 @@ async def test_prometheus_collect_worker_states(c, s, a, b):
         "partially_saturated": 0,
         "saturated": 1,
         "paused_or_retiring": 0,
+        "paused": 0,
+        "retiring": 0,
     }
 
     a.monitor.get_process_memory = lambda: 2**40
@@ -314,8 +320,22 @@ async def test_prometheus_collect_worker_states(c, s, a, b):
         "partially_saturated": 0,
         "saturated": 0,
         "paused_or_retiring": 1,
+        "paused": 1,
+        "retiring": 0,
     }
 
+    sa.status = Status.stopping
+    while sa.status != Status.stopping:
+        await asyncio.sleep(0.01)
+
+    assert await fetch_metrics() == {
+        "idle": 1,
+        "partially_saturated": 0,
+        "saturated": 0,
+        "paused_or_retiring": 1,
+        "paused": 0,
+        "retiring": 1,
+    }
     await ev.set()
 
 
