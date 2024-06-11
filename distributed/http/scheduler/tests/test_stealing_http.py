f08f04286f20d5cb1a77474d08fcc5d6f956dd86
Hendrik Makait <hendrik@makait.com>
2024-06-11 22:06:21 +0200

    wrong place

--- distributed/http/scheduler/tests/test_stealing_http.py
+++ distributed/http/scheduler/tests/test_stealing_http.py
@@ -55,7 +55,7 @@ async def test_prometheus_collect_count_total_by_cost_multipliers(c, s, a, b):
     expected_count = sum(
         len(event[1]) for _, event in s.events["stealing"] if event[0] == "request"
     )
-    assert count == pytest.approx(expected_count)
+    assert count == expected_count
 
 
 @gen_cluster(client=True)
@@ -91,7 +91,7 @@ async def test_prometheus_collect_cost_total_by_cost_multipliers(c, s, a, b):
         for request in event[1]
         if event[0] == "request"
     )
-    assert count == expected_cost
+    assert count == pytest.approx(expected_cost)
 
 
 @gen_cluster(
