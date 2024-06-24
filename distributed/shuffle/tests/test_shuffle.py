533ad9f6f86582d738872b9dc70ce807215a1f65
Hendrik Makait <hendrik@makait.com>
2024-06-24 15:16:30 +0200

    Fix imports

--- distributed/shuffle/tests/test_shuffle.py
+++ distributed/shuffle/tests/test_shuffle.py
@@ -24,11 +24,11 @@ from dask.utils import key_split
 from distributed.shuffle._core import ShuffleId, ShuffleRun, barrier_key
 from distributed.worker import Status
 
-dd = pytest.importorskip("dask.dataframe")
+pd = pytest.importorskip("pandas")
 
 import numpy as np
-import pandas as pd
 
+import dask.dataframe as dd
 from dask.dataframe._compat import PANDAS_GE_150, PANDAS_GE_200
 from dask.typing import Key
 
