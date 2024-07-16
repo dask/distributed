dask/distributed

commit 03035dafb939166d241fb0db09433c2bb3bc369c

Fix deadlock (#8703)

distributed/tests/test_scheduler.py
@@ -4650,6 +4650,43 @@ async def test_deadlock_resubmit_queued_tasks_fast(c, s, a, rootish):
     await c.gather(fut3)
 
 
+@gen_cluster(client=True, nthreads=[("", 1)])
+async def test_deadlock_dependency_of_queued_released(c, s, a):
+    @delayed
+    def inc(input):
+        return input + 1
+
+    @delayed
+    def block_on_event(input, block, executing):
+        executing.set()
+        block.wait()
+        return input
+
+    block = Event()
+    executing = Event()
+
+    dep = inc(0)
+    futs = [
+        block_on_event(dep, block, executing, dask_key_name=("rootish", i))
+        for i in range(s.total_nthreads * 2 + 1)
+    ]
+    del dep
+    futs = c.compute(futs)
+    await executing.wait()
+    assert s.queued
+    await s.remove_worker(address=a.address, stimulus_id="test")
+
+    s.validate_state()
+
+    await block.set()
+    await executing.clear()
+
+    async with Worker(s.address) as b:
+        s.validate_state()
+        await c.gather(*futs)
+        s.validate_state()
+
+
 @gen_cluster(client=True)
 async def test_submit_dependency_of_erred_task(c, s, a, b):
     x = c.submit(lambda: 1 / 0, key="x")

distributed/scheduler.py
@@ -2540,7 +2540,7 @@ class SchedulerState:
             recommendations[key] = "waiting"
 
         for dts in ts.waiters or ():
-            if dts.state in ("no-worker", "processing"):
+            if dts.state in ("no-worker", "processing", "queued"):
                 recommendations[dts.key] = "waiting"
             elif dts.state == "waiting":
                 if not dts.waiting_on:

commit d68a5d9c57628732f0d020e8d46d424d5c67397c

Add another test for a possible deadlock scenario caused by #8703 (#8769)

distributed/tests/test_scheduler.py
@@ -4644,8 +4644,11 @@ async def test_deadlock_resubmit_queued_tasks_fast(c, s, a, rootish):
     not QUEUING_ON_BY_DEFAULT,
     reason="The situation handled in this test requires queueing.",
 )
+@pytest.mark.parametrize("validate", [True, False])
 @gen_cluster(client=True, nthreads=[("", 1)])
-async def test_deadlock_dependency_of_queued_released(c, s, a):
+async def test_deadlock_dependency_of_queued_released_when_worker_replaced(
+    c, s, a, validate
+):
     @delayed
     def inc(input):
         return input + 1
@@ -4670,14 +4673,66 @@ async def test_deadlock_dependency_of_queued_released(c, s, a):
     assert s.queued
     await s.remove_worker(address=a.address, stimulus_id="test")
 
-    s.validate_state()
+    if validate:
+        s.validate_state()
 
     await block.set()
     await executing.clear()
 
     async with Worker(s.address) as b:
+        if validate:
+            s.validate_state()
+        await c.gather(futs)
+        if validate:
+            s.validate_state()
+
+
+@pytest.mark.skipif(
+    not QUEUING_ON_BY_DEFAULT,
+    reason="The situation handled in this test requires queueing.",
+)
+@pytest.mark.parametrize("validate", [True, False])
+@gen_cluster(client=True)
+async def test_deadlock_dependency_of_queued_released_when_worker_removed(
+    c, s, a, b, validate
+):
+    @delayed
+    def inc(input):
+        return input + 1
+
+    @delayed
+    def block_on_event(input, block):
+        block.wait()
+        return input
+
+    block = Event()
+
+    with dask.annotate(workers=a.address, allow_other_workers=True):
+        dep = inc(0)
+    futs = [
+        block_on_event(dep, block, dask_key_name=("rootish", i))
+        for i in range(s.total_nthreads * 2 + 1)
+    ]
+    dep.release()
+    futs = c.compute(futs)
+    with freeze_batched_send(b.batched_stream):
+        await async_poll_for(
+            lambda: b.state.tasks.get(dep.key) is not None
+            and b.state.tasks.get(dep.key).state == "memory",
+            timeout=5,
+        )
+        assert s.queued
+        await s.remove_worker(address=a.address, stimulus_id="test")
+
+    if validate:
         s.validate_state()
-        await c.gather(*futs)
+
+    await block.set()
+
+    if validate:
+        s.validate_state()
+    await c.gather(futs)
+    if validate:
         s.validate_state()
 
 