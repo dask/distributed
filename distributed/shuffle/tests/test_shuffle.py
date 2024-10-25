72f87f9dc68ff081b675fc30c30bb2a1f8b789a7
fjetter <fjetter@users.noreply.github.com>
2024-10-25 11:14:20 +0200

    do it the right way

--- distributed/shuffle/tests/test_shuffle.py
+++ distributed/shuffle/tests/test_shuffle.py
@@ -253,7 +253,7 @@ async def test_shuffle_with_array_conversion(c, s, a, b, npartitions):
         out = df.shuffle("x", npartitions=npartitions).values
 
     # See distributed#7816. TaskSpec is currently blocking linear fusion. If that was implemented, this may raise a P2PConsistencyErrro
-    if not dd._dask_expr_enabled() and npartitions == 1:
+    if dd._dask_expr_enabled() and npartitions == 1:
         with pytest.raises(P2PConsistencyError, match="Barrier task"):
             await c.compute(out)
     else:
