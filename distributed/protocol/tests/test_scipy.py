aa3554471ee4c5d66a74e9092bb246e815d1daf8
Hendrik Makait <hendrik@makait.com>
2025-01-06 15:53:16 +0100

    Skip big-endian floats if using scipy>=1.15.0

--- distributed/protocol/tests/test_scipy.py
+++ distributed/protocol/tests/test_scipy.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from packaging.version import Version
 
 import pytest
 
@@ -8,6 +9,9 @@ numpy = pytest.importorskip("numpy")
 scipy = pytest.importorskip("scipy")
 scipy_sparse = pytest.importorskip("scipy.sparse")
 
+
+SCIPY_VERSION = Version(scipy.__version__)
+SCIPY_GE_1_15_0 = SCIPY_VERSION.release >= (1, 15, 0)
 
 @pytest.mark.parametrize(
     "sparse_type",
@@ -23,7 +27,12 @@ scipy_sparse = pytest.importorskip("scipy.sparse")
 )
 @pytest.mark.parametrize(
     "dtype",
-    [numpy.dtype("<f4"), numpy.dtype(">f4"), numpy.dtype("<f8"), numpy.dtype(">f8")],
+    [
+        numpy.dtype("<f4"), 
+        pytest.param(numpy.dtype(">f4"), marks=pytest.mark.skipif(SCIPY_GE_1_15_0, reason="https://github.com/scipy/scipy/issues/22258")), 
+        numpy.dtype("<f8"),
+        pytest.param(numpy.dtype(">f8"), marks=pytest.mark.skipif(SCIPY_GE_1_15_0, reason="https://github.com/scipy/scipy/issues/22258")), 
+    ],
 )
 def test_serialize_scipy_sparse(sparse_type, dtype):
     a = numpy.array([[0, 1, 0], [2, 0, 3], [0, 4, 0]], dtype=dtype)
