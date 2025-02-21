54eec97efc1419da67f2d5cc1a93f8235a07c4df
Isaac Cheruiyot <icheruiyot@palantir.com>
2025-02-21 15:07:08 -0500

    Add redirect from prefix root to status

--- distributed/dashboard/scheduler.py
+++ distributed/dashboard/scheduler.py
@@ -172,15 +172,22 @@ def connect(application, http_server, scheduler, prefix=""):
     application.add_application(bokeh_app)
     bokeh_app.initialize(IOLoop.current())
 
-    bokeh_app.add_handlers(
-        r".*",
-        [
-            (
-                r"/",
-                web.RedirectHandler,
-                {"url": urljoin((prefix or "").strip("/") + "/", r"status")},
-            )
-        ],
-    )
+    routes = [
+        (
+            r"/",
+            web.RedirectHandler,
+            {"url": urljoin((prefix or "").strip("/") + "/", "status")},
+        )
+    ]
 
+    if prefix:
+        routes.append(
+            (
+                rf"/{prefix.strip("/")}/?",
+                web.RedirectHandler,
+                {"url": urljoin((prefix).strip("/") + "/", "status")},
+            )
+        )
+
+    bokeh_app.add_handlers(r".*", routes)
     bokeh_app.start()
