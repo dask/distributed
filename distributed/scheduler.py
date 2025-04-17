3f17ddbab2499cfe6d4142e4caf8a62fc9077b96
Florian Jetter <fjetter@users.noreply.github.com>
2025-04-17 14:29:43 +0200

    Avoid deepcopy when submitting graph (#8633)

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -4854,8 +4854,7 @@ class Scheduler(SchedulerState, ServerNode):
     async def update_graph(
         self,
         client: str,
-        expr_header: dict,
-        expr_frames: list[bytes],
+        expr_ser: Serialized,
         keys: set[Key],
         span_metadata: SpanMetadata,
         internal_priority: dict[Key, int] | None,
@@ -4873,8 +4872,8 @@ class Scheduler(SchedulerState, ServerNode):
         try:
             logger.debug("Received new graph. Deserializing...")
             try:
-                expr = deserialize(expr_header, expr_frames).data
-                del expr_header, expr_frames
+                expr = deserialize(expr_ser.header, expr_ser.frames)
+                del expr_ser
             except Exception as e:
                 msg = """\
                     Error during deserialization of the task graph. This frequently
