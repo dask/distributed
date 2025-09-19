52e258fa8a0da7c74b9419d7afb34f0b95164cd1
Jacob Tomlinson <jtomlinson@nvidia.com>
2025-09-19 16:20:30 +0100

    Fix linting issues

--- distributed/deploy/tests/test_old_ssh.py
+++ distributed/deploy/tests/test_old_ssh.py
@@ -11,9 +11,11 @@ from distributed import Client
 from distributed.deploy.old_ssh import SSHCluster
 from distributed.metrics import time
 
-
 # https://github.com/dask/distributed/issues/9114
-pytestmark = pytest.mark.skipif(sys.platform == "win32", reason="Hangs on Windows in CI")
+pytestmark = pytest.mark.skipif(
+    sys.platform == "win32", reason="Hangs on Windows in CI"
+)
+
 
 @pytest.mark.avoid_ci
 def test_cluster(loop):
